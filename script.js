3ef1e44a9b8453a97a3d9362a8d4d04e2b114443
William <williamjog@hotmail.com>
2020-07-25 17:46:46 -0300

    5th requirement fixing codeClimate  adding addTaskButton

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-window.onload = function() {
-function createNewTask() {
-  const newTask = document.createElement('li');
-  newTask.innerHTML = document.querySelector('#texto-tarefa').value;
-  document.querySelector('#lista-tarefas').appendChild(newTask);
-  document.querySelector('#texto-tarefa').value = '';
-}
+window.onload = function () {
+  function createNewTask() {
+    const newTask = document.createElement('li');
+    newTask.innerHTML = document.querySelector('#texto-tarefa').value;
+    document.querySelector('#lista-tarefas').appendChild(newTask);
+    document.querySelector('#texto-tarefa').value = '';
+  }
 
-const createTaskButton = document.querySelector('#criar-tarefa');
-createTaskButton.addEventListener('click', createNewTask);
-
-}
+  const createTaskButton = document.querySelector('#criar-tarefa');
+  createTaskButton.addEventListener('click', createNewTask);
+};
