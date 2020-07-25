b6ed9763bef1ef3a9a28cba3c31afd893d54bd42
Pablo Santos <pablo.trybe@gmail.com>
2020-07-25 01:57:37 -0300

    Corrigindo issues do codeclimate

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-/*Função para adicionar uma nova tarefa*/
+/* Função para adicionar uma nova tarefa */
 const task = document.getElementById('texto-tarefa');
 const btnCreateTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 function createTask() {
-  if (task.value.length !== 0) { /*Verifica se o input não está vazio*/
-    const li = document.createElement('li'); /*Acessa a lista do html*/
-    taskList.appendChild(li); /*Cria um filho na lista passando o conteúdo recebido no input*/
-    li.innerText = task.value; /*Adiciona tarefa na lista*/
-    task.value = ''; /*Limpa o input*/
+  if (task.value.length !== 0) { /* Verifica se o input não está vazio */
+    const li = document.createElement('li'); /* Acessa a lista do html */
+    taskList.appendChild(li); /* Cria um filho na lista passando o conteúdo recebido no input */
+    li.innerText = task.value; /* Adiciona tarefa na lista */
+    task.value = ''; /* Limpa o input */
   }
 }
 btnCreateTask.addEventListener('click', createTask);
 
-/*Função para apagar tudo*/
+/* Função para apagar tudo */
 const btnDelete = document.getElementById('apaga-tudo');
 function clear() {
-  taskList.innerText = "";
+  taskList.innerText = '';
 }
 btnDelete.addEventListener('click', clear);
