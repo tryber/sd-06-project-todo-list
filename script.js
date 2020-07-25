dad2f8fcbcd491dfbddb428da5e1652ab9935e82
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-25 16:13:43 -0300

    updating line through

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-const buttonCreateTask = document.getElementById('criar-tarefa');
-const taskList = document.getElementById('lista-tarefas');
-let taskInput = '';
+window.onload = function () {
+  const buttonCreateTask = document.getElementById('criar-tarefa');
+  const taskList = document.getElementById('lista-tarefas');
+  let taskInput = '';
+  let deco = false;
 
-buttonCreateTask.addEventListener('click', function () {
-  taskInput = document.getElementById('texto-tarefa').value;
-  if (taskInput !== '') {
-    const newLine = document.createElement('li');
-    newLine.innerHTML = taskInput;
-    taskList.appendChild(newLine);
-    document.getElementById('texto-tarefa').value = '';
-  } else {
-    alert('Invalid entry!');
-  }
-});
+  buttonCreateTask.addEventListener('click', function () {
+    taskInput = document.getElementById('texto-tarefa').value;
+    if (taskInput !== '') {
+      const newLine = document.createElement('li');
+      newLine.innerHTML = taskInput;
+      taskList.appendChild(newLine);
+      document.getElementById('texto-tarefa').value = '';
+    } else {
+      alert('Invalid entry!');
+    }
+  });
 
-taskList.addEventListener('click', function (event) {
-  if (event.target && event.target.matches('li')) {
+  taskList.addEventListener('click', function (event) {
+    let listItem = document.getElementsByTagName('li');
     event.target.style.backgroundColor = 'rgb(128,128,128';
-    for (let i = 0; i < document.getElementsByTagName('li').length; i += 1) {
-      if (document.getElementsByTagName('li')[i] !== event.target) {
-        document.getElementsByTagName('li')[i].style.backgroundColor = 'transparent';
+    for (let i = 0; i < listItem.length; i += 1) {
+      if (listItem[i] !== event.target) {
+        listItem[i].style.backgroundColor = 'transparent';
       }
     }
-  }
-});
+  });
+
+  taskList.addEventListener('dblclick', function (event) {
+    if (!deco) {
+      event.target.style.textDecoration = 'line-through';
+      deco = true;
+    } else if (deco) {
+      event.target.style.textDecoration = 'none';
+      deco = false;
+    }
+  });
+}
