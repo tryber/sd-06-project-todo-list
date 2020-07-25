56832dbb691ae95a4b9d17ce0892ba6afdfb31ba
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-07-25 20:58:34 -0300

    add selectListItem function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function createListItem () {
-  let item_text = document.getElementById('texto-tarefa').value;
-  let item = document.createElement('li');
-  item.innerText = item_text;
-  document.getElementById('lista-tarefas').appendChild(item)
+window.onload = function () {
+  function createListItem () {
+    let item_text = document.getElementById('texto-tarefa').value;
+    let item = document.createElement('li');
+    item.innerText = item_text;
+    document.getElementById('lista-tarefas').appendChild(item)
+  }
+  
+  
+  function selectListItem (event) {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  }
+
+  document.getElementById('criar-tarefa').addEventListener('click', createListItem);
+
+  document.getElementById('lista-tarefas').addEventListener('click', selectListItem)
+
 }
