942683858eb53f10977b36cdf0646d047b308e3d
Debora Polesel <deborahpolesel@hotmail.com>
2020-07-25 15:45:32 -0300

    "Quinto Requisito"

---
+++ script.js
@@ -0,0 +1,9 @@
+function adicionar() {
+    let inputTarefa = document.getElementById("texto-tarefa").value;
+    let listaTarefas  = document.getElementById("lista-tarefas").innerHTML;
+    listaTarefas = listaTarefas +"<li>"+inputTarefa+"</li>";
+    
+    document.getElementById("lista-tarefas").innerHTML = listaTarefas;
+    document.getElementById("texto-tarefa").value = "";
+  }
+adicionar()
