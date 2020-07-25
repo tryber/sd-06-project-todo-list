3b37cdb65733a6febbffeefc26c25be91c01f8d6
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-25 10:51:17 -0300

    Task 11: removing completed tasks | Task 12: Saving task on localStorage

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
+//Addind a task
 let buttonAddElement = document.getElementById('criar-tarefa');
-buttonAddElement.addEventListener('click', (event) => addListItem());
+buttonAddElement.addEventListener('click', (event) => addListItem(0, null, null));
 
-function addListItem() {
+function addListItem(newOrSavedIndicator, taskName, taskClass) {
     let listElement = document.querySelector('#lista-tarefas');
     let listItem = document.createElement('li');
     let inputElement = document.querySelector('#texto-tarefa');
 
-    listItem.innerHTML = inputElement.value;
+    if (newOrSavedIndicator == 0) {
+        listItem.innerHTML = inputElement.value;
+    } else {  
+        listItem.innerHTML = taskName;
+        listItem.className = taskClass; 
+    }
+
     listElement.appendChild(listItem);
 
     clearInputField();
@@ -16,11 +23,13 @@ function addListItem() {
     listItem.addEventListener('dblclick', (event) => scratchItem(listItem));
 }
 
+//Clearing the input field
 function clearInputField() {
     let inputElement = document.querySelector('#texto-tarefa');
     inputElement.value = '';
 }
 
+//Changing task background color 
 function changeItemColor(listItemToChange) {
     listItemToChange.style.backgroundColor = 'rgb(128,128,128)'; 
 
@@ -32,10 +41,12 @@ function changeItemColor(listItemToChange) {
     }
 }
 
+//Scratching task
 function scratchItem(listItemToScratch) {
     listItemToScratch.className = (listItemToScratch.className == 'completed') ? '' : 'completed';
 }
 
+//Deleting all tasks
 let buttonDeleteElement = document.getElementById('apaga-tudo');
 buttonDeleteElement.addEventListener('click', (event) => deleteAllListItem());
 
@@ -46,3 +57,39 @@ function deleteAllListItem() {
         listElement.removeChild(allListItem[item])
     }
 }
+
+//Deleting completed tasks
+let buttonDeleteCompletedElement = document.getElementById('remover-finalizados');
+buttonDeleteCompletedElement.addEventListener('click', (event) => deleteCompletedListItem());
+
+function deleteCompletedListItem() {
+    let allCompletedListItem = document.querySelectorAll('.completed');
+    let listElement = document.querySelector('#lista-tarefas');
+    for (let item = 0; item < allCompletedListItem.length; item += 1) {
+        listElement.removeChild(allCompletedListItem[item])
+    }
+}
+
+
+//Saving tasks on localStorage
+let buttonSaveElement = document.getElementById('salvar-tarefas');
+buttonSaveElement.addEventListener('click', (event) => saveListItem());
+
+function saveListItem() {
+    let allListItem = document.querySelectorAll('li');
+    for (let item = 0; item < allListItem.length; item += 1) {
+      let taskName = 'task' + item;
+      let taskClass = 'taskClass' + item;
+      localStorage[taskName] = allListItem[item].innerHTML;
+      localStorage[taskClass] = allListItem[item].className;
+      localStorage.numerOfTasks = allListItem.length;
+    }
+}
+
+//Recovering tasks from localStorage
+for (let item = 0; item < localStorage.numerOfTasks; item += 1) {
+    let taskName = 'task' + item;
+    let taskClass = 'taskClass' + item;
+    addListItem(1, localStorage[taskName], localStorage[taskClass]);
+}
+
