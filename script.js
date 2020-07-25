ad38695b7811ef9537a711c47aaf9bbe130f9278
theCoyote <flaviolunaferreira@gmail.com>
2020-07-25 19:13:21 -0300

    btn adicionar (corrigir erros code climate)

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
-var lista = [];
-var btn = document.getElementById("criar-tarefa");
-btn.addEventListener("click", function() {
-  clickBtnCriarTarefa();
-});
-
-/*************************************************/
+/* *********************************************** */
+function pegaTexto() {
+  let texto = document.getElementById('texto-tarefa');
+  return texto.value;
+}
+/* *********************************************** */
+function criarArray() {
+  let texto = pegaTexto();
+  if (texto !== ''){
+    lista.push(texto);
+    console.log(lista);
+  } else {
+    alert('Campo tarefa precisa ser preenchido!!!')
+  }
+}
+/* *********************************************** */
+function criarTabela() {
+  let itens = document.querySelectorAll('li');
+  for (let i = 0; i < itens.length; i += 1) {
+    document.getElementById('lista-tarefas').removeChild(itens[i]);
+  }
+  for (let i = 0; i < lista.length; i += 1) {
+    let tarefa = document.createElement('li')
+    tarefa.innerHTML = lista[i];
+    document.getElementById('lista-tarefas').appendChild(tarefa);
+  }
+}
+/* *********************************************** */
+function limpaTexto() {
+  let tarefa = document.getElementById('texto-tarefa');
+  tarefa.value = '';
+}
+/* *********************************************** */
 function clickBtnCriarTarefa() {
   pegaTexto();
   criarArray()
   criarTabela();
   limpaTexto()
 }
-/*************************************************/
-function pegaTexto() {
-  let texto = document.getElementById("texto-tarefa");
-  return texto.value;
-}
-/*************************************************/
-function criarArray() {
-  let texto = pegaTexto();
-  if (texto !== ""){
-    lista.push(texto);
-    console.log(lista);
-  } else {
-    alert("Campo tarefa precisa ser preenchido!!!")
-  } 
-}
-/*************************************************/
-function criarTabela() {
-  for (let i = 0; i < lista.length; i +=1) {
-    let tarefa = document.createElement("li")
-    tarefa.innerHTML = lista[i];
-    document.getElementById("lista-tarefas").appendChild(tarefa);
-  }
-}  
-/*************************************************/
-function limpaTexto() {
-  let tarefa = document.getElementById("texto-tarefa");
-  tarefa.value = "";
-}
-/*************************************************/
+/* *********************************************** */
+let lista = [];
+let btn = document.getElementById('criar-tarefa');
+btn.addEventListener('click', function () {
+  clickBtnCriarTarefa();
+});
 
