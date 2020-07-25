8f0108019aacda580787b2f2344f107192d01c7e
Debora Polesel <deborahpolesel@hotmail.com>
2020-07-25 15:52:37 -0300

    "Revisão pontuação"

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function adicionar() {
+function adicionarTarefa() {
     let inputTarefa = document.getElementById("texto-tarefa").value;
     let listaTarefas  = document.getElementById("lista-tarefas").innerHTML;
     listaTarefas = listaTarefas +"<li>"+inputTarefa+"</li>";
-    
     document.getElementById("lista-tarefas").innerHTML = listaTarefas;
     document.getElementById("texto-tarefa").value = "";
-  }
-adicionar()
+}
+adicionarTarefa()
