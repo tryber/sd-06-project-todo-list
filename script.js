32f3060e31884f85fb1db8948333e20c58005c01
William <williamjog@hotmail.com>
2020-07-25 18:08:23 -0300

    6th and 7th requirements adding backGroundColor on clicked itens

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ window.onload = function () {
     newTask.innerHTML = document.querySelector('#texto-tarefa').value;
     document.querySelector('#lista-tarefas').appendChild(newTask);
     document.querySelector('#texto-tarefa').value = '';
+    newTask.addEventListener('click', function () {
+      newTask.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
   }
 
   const createTaskButton = document.querySelector('#criar-tarefa');
