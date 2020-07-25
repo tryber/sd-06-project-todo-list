917c01c0ec7f49010ee7252476cb2718849efe6a
rafMewtwo <ponciano.raf@gmail.com>
2020-07-25 00:28:32 -0300

    limpando caixa de texto

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ window.onload = function () {
 let buttonAdd = document.querySelector("#criar-tarefa")
 let campoTexto = document.getElementById("texto-tarefa")
 
+
+//Add elementos na lista
 buttonAdd.addEventListener("click", function(){
     let linha = document.createElement("li")
     document.getElementById("lista-tarefas").appendChild(linha)
     linha.innerHTML = campoTexto.value
+    campoTexto.value = ""
     
 })
+
+
 
 }
