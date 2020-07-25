bf115eecbd1224b6befe5ee87f1c870be7f2da9e
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-25 01:08:59 -0300

    add lista

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector("#criar-tarefa");
+const inputText = ducument.querySelector("#texto-tarefa");
+const addList = ducument.querySelector("#lista-tarefas");
+
+function newList() {
+    alert("djjdjsn");
+}
+
+
+
+inputText.addEventListener("click", newList);
