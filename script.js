ce89d530a2c662c7af0368eec65f3e25b0e6846d
William <williamjog@hotmail.com>
2020-07-25 17:25:57 -0300

    5th requirement adding addTaskButton

---
+++ script.js
@@ -0,0 +1,9 @@
+function createNewTask() {
+  const newTask = document.createElement('li');
+  newTask.innerHTML = document.querySelector('#texto-tarefa').value;
+  document.querySelector('#lista-tarefas').appendChild(newTask);
+  document.querySelector('#texto-tarefa').value = '';
+}
+
+const createTaskButton = document.querySelector('#criar-tarefa');
+createTaskButton.addEventListener('click', createNewTask);
