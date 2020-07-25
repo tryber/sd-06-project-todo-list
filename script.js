73a056f5218996e56e7e5417ccbff9d379127b8b
William <williamjog@hotmail.com>
2020-07-25 18:30:03 -0300

    Adding line-through function

--- script.js
+++ script.js
@@ -7,8 +7,17 @@ window.onload = function () {
     newTask.addEventListener('click', function () {
       newTask.style.backgroundColor = 'rgb(128, 128, 128)';
     });
+    newTask.addEventListener('dblclick', function () {
+      newTask.className = 'completed';
+      newTask.style.textDecoration = 'line-through';
+      newTask.addEventListener('dblclick', function () {
+        if (newTask.className === 'completed') {
+          newTask.classList.remove('completed');
+          newTask.style.textDecoration = 'none';
+        }
+      });
+    });
   }
-
   const createTaskButton = document.querySelector('#criar-tarefa');
   createTaskButton.addEventListener('click', createNewTask);
 };
