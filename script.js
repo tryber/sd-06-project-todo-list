8e4351ca97524873a47904b1583cee1d0ce0ff93
Debora Polesel <deborahpolesel@hotmail.com>
2020-07-25 19:13:56 -0300

    "Revisão"

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function adicionarTarefa() {
-let inputTarefa = document.getElementById("texto-tarefa").value;
-let listaTarefas  = document.getElementById("lista-tarefas").innerHTML;
-listaTarefas = listaTarefas +"<li>"+inputTarefa+"</li>";
-document.getElementById("lista-tarefas").innerHTML = listaTarefas;
-document.getElementById("texto-tarefa").value = "";
+    let inputTarefa = document.getElementById("texto-tarefa").value;
+    let listaTarefas  = document.getElementById("lista-tarefas").innerHTML;
+    listaTarefas = listaTarefas +"<li>"+inputTarefa+"</li>";
+    document.getElementById("lista-tarefas").innerHTML = listaTarefas;
+    document.getElementById("texto-tarefa").value = "";
 }
 adicionarTarefa();
