6e114a2895c4648aebaaeac66acb6ecb7d011c94
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-25 14:26:50 -0300

    updating

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const buttonCreateTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 let taskInput = '';
 
-buttonCreateTask.addEventListener('click', function() {
+buttonCreateTask.addEventListener('click', function () {
   taskInput = document.getElementById('texto-tarefa').value;
   if (taskInput !== '') {
-    let newLine = document.createElement('li');
+    const newLine = document.createElement('li');
     newLine.innerHTML = taskInput;
-      taskList.appendChild(newLine);
+    taskList.appendChild(newLine);
   } else {
-    alert("Invalid entry!")
+    alert('Invalid entry!');
   }
 });
