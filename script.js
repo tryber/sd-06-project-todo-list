f8b308195dfca2573ee3233650033a0c78a94483
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-07-26 13:09:07 -0300

    Verificando requisitos 4 e 5 - adicionando a lista e o botao

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.onload = function() {
-  
+  let tarefa = document.querySelector("#texto-tarefa")
+  let tarefaBtn = document.querySelector("#criar-tarefa");
+  let lista = document.querySelector("#lista-tarefas");
+
+  tarefaBtn.addEventListener("click", function(){
+    lista.innerHTML += '<li>' + tarefa.value + '</li>';
+    tarefa.value = '';
+  });
 }
