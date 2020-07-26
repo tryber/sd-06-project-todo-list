c09905d7839b9216a79bb632d48e75252860cfde
William <williamjog@hotmail.com>
2020-07-26 20:08:22 -0300

    9th requirement - line through

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 // declarando as variáveis e os botões
 const createTaskButton = document.querySelector('#criar-tarefa');
+const textInput = document.querySelector('#texto-tarefa');
+const taskList = document.querySelector('#lista-tarefas');
+
 
 function createNewTask() {
-  const textInput = document.querySelector('#texto-tarefa');
-  const list = document.querySelector('#lista-tarefas');
   const newTask = document.createElement('li');
   newTask.innerHTML = textInput.value;
-  list.appendChild(newTask);
+  taskList.appendChild(newTask);
   textInput.value = '';
-  newTask.addEventListener('click', function (event) {
-    const allTasks = document.querySelectorAll('li');
-    for (let i = 0; i < allTasks.length; i += 1) {
-      if (allTasks[i].classList.contains('selectedTask')) {
-        allTasks[i].classList.remove('selectedTask');
-      }
-    }
-    event.target.classList.add('selectedTask');
-  });
 }
-
 createTaskButton.addEventListener('click', createNewTask);
+
+function selectedTask() {
+  const taskSelected = event.target;
+  const allTasks = document.querySelectorAll('li');
+  for (let i = 0; i < allTasks.length; i += 1) {
+    if (allTasks[i].classList.contains('selectedTask')) {
+      allTasks[i].classList.remove('selectedTask');
+    }
+  }
+  taskSelected.classList.add('selectedTask');
+}
+taskList.addEventListener('click', selectedTask);
+
+function scrachTask() {
+  const doubleClickedTask = event.target;
+  if (doubleClickedTask.classList.contains('completed')) {
+    doubleClickedTask.classList.remove('completed');
+  } else {
+    doubleClickedTask.classList.add('completed');
+  }
+}
+taskList.addEventListener('dblclick', scrachTask);
