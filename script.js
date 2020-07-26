0b6f2d5cadbc3511df9a54b892ae0314c9382b54
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-07-26 16:48:04 -0300

    Verificando o 7

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 window.onload = function() {
   let tarefa = document.querySelector("#texto-tarefa")
   let tarefaBtn = document.querySelector("#criar-tarefa");
-  let lista = document.querySelector("#lista-tarefas");
+  let lista = document.querySelector("#lista-tarefas");  
 
   tarefaBtn.addEventListener("click", function(){
     lista.innerHTML += '<li>' + tarefa.value + '</li>';
     tarefa.value = '';
+  });
+
+  lista.addEventListener("click", function(e) {
+    let cont = 0
+    let range = document.elementFromPoint(e.clientX, e.clientY);
+    range.style.backgroundColor = "rgb(128, 128, 128)";
   });
 }
