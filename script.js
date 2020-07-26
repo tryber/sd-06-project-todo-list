4e410ca0e100196bd85fbcf2bd9f36ad4e0c9a93
William <williamjog@hotmail.com>
2020-07-26 18:25:48 -0300

    7th and 8th requirements - backGroundColor

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // declarando as variáveis e os botões
 const createTaskButton = document.querySelector('#criar-tarefa');
-const textInput = document.querySelector('#texto-tarefa');
-const list = document.querySelector('#lista-tarefas');
 
 function createNewTask() {
+  const textInput = document.querySelector('#texto-tarefa');
+  const list = document.querySelector('#lista-tarefas');
   const newTask = document.createElement('li');
   newTask.innerHTML = textInput.value;
   list.appendChild(newTask);
   textInput.value = '';
+  newTask.addEventListener('click', function (event) {
+    const allTasks = document.querySelectorAll('li');
+    for (let i = 0; i < allTasks.length; i += 1) {
+      if (allTasks[i].classList.contains('selectedTask')) {
+        allTasks[i].classList.remove('selectedTask');
+      }
+    }
+    event.target.classList.add('selectedTask');
+  });
 }
 
 createTaskButton.addEventListener('click', createNewTask);
