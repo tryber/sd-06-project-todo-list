5c3c83089b20e3eda4dd1014ea766727d37c4e8a
Havyner Caetano <hrcgeo@gmail.com>
2020-07-26 15:22:39 -0300

    Add grey to background color of selected li

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const addTaskButton = document.querySelector('button:nth-of-type(1)');
 const taskList = document.querySelector('ol');
 const taskText = document.querySelector('input');
+let tasks = [];
 
 addTaskButton.addEventListener('click', function () {
   const li = document.createElement('li');
-  
+
   li.innerText = taskText.value;
+
+  li.addEventListener('click', function (event) {
+    event.path[0].className = 'selected';
+  })
+
   taskText.value = '';
   taskList.appendChild(li);
+  tasks.push(li);
 })
 
