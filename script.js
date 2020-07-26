74934b8c0565d2acc79ec60106c477efe4cac919
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-26 20:13:00 -0300

    Desafio 7

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// imput do texto
-let textoTarefa = document.getElementById('texto-tarefa');
-// lista ordenada 
-let listaTarefas = document.getElementById('lista-tarefas');
-// botao ao lado do imput
-let criarTarefa = document.getElementById('criar-tarefa');
+const textoTarefa = document.getElementById('texto-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const criarTarefa = document.getElementById('criar-tarefa');
+
 criarTarefa.addEventListener('click', function () {
-  let li = document.createElement('li');
+  const li = document.createElement('li');
   li.innerHTML = textoTarefa.value;
   listaTarefas.appendChild(li);
+  li.addEventListener('click', function () {
+    const liSelecionado = document.getElementsByClassName('itemSelecionado');
+    if (liSelecionado.length > 0) {
+      liSelecionado[0].classList.remove('itemSelecionado');
+    }
+    li.classList.add('itemSelecionado');
+  });
   textoTarefa.value = '';
 });
+
+
+// 1) Será verificado que, ao se clicar em um item da lista, 
+// ele passa a ter o estilo CSS `background-color: rgb(128, 128, 128)`
