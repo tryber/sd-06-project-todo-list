c071bd514235bf356f93197af3eb8191eeae7a99
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-07-25 22:03:25 -0300

    fix createListItem - clear input

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 window.onload = function () {
   function createListItem () {
-    let item_text = document.getElementById('texto-tarefa').value;
     let item = document.createElement('li');
-    item.innerText = item_text;
+    item.innerText = document.getElementById('texto-tarefa').value;
     document.getElementById('lista-tarefas').appendChild(item)
+    document.getElementById('texto-tarefa').value = '';
   }
-  
   
   function selectListItem (event) {
     for (item of document.getElementsByTagName('li')) {
@@ -16,14 +15,12 @@ window.onload = function () {
     event.target.style.backgroundColor = 'rgb(128,128,128)';
   }
 
-
   function doubleSelectListItem (event) {
     if (event.target.className === '') {
       event.target.className = 'completed';
     } else {
       event.target.className = '';
     }
-    
   }
 
   document.getElementById('criar-tarefa').addEventListener('click', createListItem);
