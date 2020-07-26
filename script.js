8c58b27b343a4ee822104e9a5103664931b4f889
MCoutinho <matheuscavalcante88@hotmail.com>
2020-07-26 13:31:35 -0300

    delete all tasks button event listener

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputElement = document.querySelector('#texto-tarefa');
 const createButton = document.querySelector('#criar-tarefa');
+const deleteAllButton = document.querySelector('#apaga-tudo');
 const elementOl = document.querySelector('#lista-tarefas');
 const tasks = [];
 
@@ -14,5 +15,12 @@ createButton.addEventListener('click', () => {
   if (inputElement.value) {
     tasks.push(createElementLiAndAppendToElementOl(elementOl, inputElement.value));
     inputElement.value = '';
+  }
+});
+
+deleteAllButton.addEventListener('click', () => {
+  while (elementOl.firstChild) {
+    elementOl.removeChild(elementOl.lastChild);
+    tasks.pop();
   }
 });
