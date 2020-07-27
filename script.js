e2085c150743619bb7f1c434f891b2415834f83d
theCoyote <flaviolunaferreira@gmail.com>
2020-07-27 18:27:52 -0300

    verificando o avaliador

--- script.js
+++ script.js
@@ -1,73 +1,105 @@
-let lista = [];
-const btnIncluir = document.getElementById('criar-tarefa');
-const btnApagarTudo = document.getElementById('apaga-tudo');
-/* *********************************************** */
-function pegaTexto() {
-  const texto = document.getElementById('texto-tarefa');
-  return texto.value;
+const inputElement = document.querySelector('#texto-tarefa');
+const elementOl = document.querySelector('#lista-tarefas');
+const createButton = document.querySelector('#criar-tarefa');
+const moveUpButton = document.querySelector('#mover-cima');
+const moveDownButton = document.querySelector('#mover-baixo');
+const deleteAllButton = document.querySelector('#apaga-tudo');
+const deleteCompletedButton = document.querySelector('#remover-finalizados');
+const deleteSelectedButton = document.querySelector('#remover-selecionado');
+const saveTasksButton = document.querySelector('#salvar-tarefas');
+
+function createElementLiAndAppendToElementOl(orderedList, value, completed = false) {
+  const elementLi = document.createElement('li');
+  elementLi.innerText = value;
+  orderedList.appendChild(elementLi);
+  if (completed) {
+    elementLi.className = 'completed';
+  }
 }
-/* *********************************************** */
-function criarArray() {
-  const texto = pegaTexto();
-  if (texto !== '') {
-    lista.push(texto);
+function updateElements(taskList) {
+  for (let item = 0; item < taskList.length; item += 1) {
+    if (taskList[item].includes('.completed')) {
+      taskList[item] = taskList[item].replace('.completed', '');
+      createElementLiAndAppendToElementOl(elementOl, taskList[item], true);
+    } else {
+      createElementLiAndAppendToElementOl(elementOl, taskList[item]);
+    }
+  }
+}
+function loadFromStorage() {
+  if (localStorage.taskList !== undefined) {
+    const taskList = JSON.parse(localStorage.getItem('taskList'));
+    updateElements(taskList);
+  }
+}
+loadFromStorage();
+createButton.addEventListener('click', () => {
+  if (inputElement.value) {
+    createElementLiAndAppendToElementOl(elementOl, inputElement.value);
+    inputElement.value = '';
+  }
+});
+deleteAllButton.addEventListener('click', () => {
+  while (elementOl.firstChild) {
+    elementOl.removeChild(elementOl.lastChild);
+  }
+});
+elementOl.addEventListener('click', (event) => {
+  event.preventDefault();
+  const selectedTask = event.target;
+  const tasks = document.getElementsByTagName('li');
+  for (let item = 0; item < tasks.length; item += 1) {
+    if (tasks[item].classList.contains('selected')) {
+      tasks[item].classList.remove('selected');
+    }
+  }
+  selectedTask.classList.add('selected');
+});
+elementOl.addEventListener('dblclick', (event) => {
+  const selectedTask = event.target;
+  if (selectedTask.classList.contains('completed')) {
+    selectedTask.classList.remove('completed');
   } else {
-    alert('Campo tarefa precisa ser preenchido!!!');
+    selectedTask.classList.add('completed');
   }
-}
-/* *********************************************** */
-function limparTabela() {
-  const itens = document.querySelectorAll('li');
-  for (let i = 0; i < itens.length; i += 1) {
-    document.getElementById('lista-tarefas').removeChild(itens[i]);
-  }
-}
-/* *********************************************** */
-function criarTabela() {
-  for (let i = 0; i < lista.length; i += 1) {
-    const tarefa = document.createElement('li');
-    tarefa.innerHTML = lista[i];
-    document.getElementById('lista-tarefas').appendChild(tarefa);
-  }
-}
-/* *********************************************** */
-function limpaTexto() {
-  const tarefa = document.getElementById('texto-tarefa');
-  tarefa.value = '';
-  tarefa.focus();
-}
-/* *********************************************** */
-function atribuirFuncaoClick() {
-  var item = document.querySelectorAll('li')
-  for (let i = 0; i < item.length; i += 1) {
-    item[i].addEventListener('click', function(e) {
-      if (item[i].style.backgroundColor === 'rgb(128, 128, 128)') { 
-        item[i].style.backgroundColor = 'transparent';
-      } else {
-        item[i].style.backgroundColor = 'rgb(128,128,128)';
-      }  
-    });
-  }
-}
-/* *********************************************** */
-function clickBtnCriarTarefa() {
-  pegaTexto();
-  criarArray();
-  limparTabela();
-  criarTabela();
-  limpaTexto();
-  atribuirFuncaoClick();
-}
-function clickBtnApagarTudo() {
-  limparTabela();
-  lista = [];
-}
-/* *********************************************** */
-btnIncluir.addEventListener('click', function () {
-  clickBtnCriarTarefa();
 });
-/* *********************************************** */
-btnApagarTudo.addEventListener('click', function () {
-  clickBtnApagarTudo();
+moveUpButton.addEventListener('click', () => {
+  const tasks = Array.from(document.getElementsByTagName('li'));
+  for (let item = 0; item < tasks.length; item += 1) {
+    if (tasks[item].classList.contains('selected') && (item !== 0)) {
+      tasks[item].parentNode.insertBefore(tasks[item], tasks[item - 1]);
+    }
+  }
 });
-/* *********************************************** */
+
+moveDownButton.addEventListener('click', () => {
+  const tasks = Array.from(document.getElementsByTagName('li'));
+  for (let item = 0; item < tasks.length; item += 1) {
+    if (tasks[item].classList.contains('selected') && (item !== tasks.length - 1)) {
+      tasks[item].parentNode.insertBefore(tasks[item + 1], tasks[item]);
+    }
+  }
+});
+
+deleteCompletedButton.addEventListener('click', () => {
+  const tasksCompleted = Array.from(document.querySelectorAll('.completed'));
+  for (let item = 0; item < tasksCompleted.length; item += 1) {
+    elementOl.removeChild(tasksCompleted[item]);
+  }
+});
+deleteSelectedButton.addEventListener('click', () => {
+  const tasksSelected = Array.from(document.querySelectorAll('.selected'));
+  for (let item = 0; item < tasksSelected.length; item += 1) {
+    elementOl.removeChild(tasksSelected[item]);
+  }
+});
+saveTasksButton.addEventListener('click', () => {
+  const tasks = Array.from(document.getElementsByTagName('li'));
+  const taskList = tasks.map((task) => {
+    if (task.classList.contains('completed')) {
+      return `${task.innerText}.completed`;
+    }
+    return task.innerText;
+  });
+  localStorage.setItem('taskList', JSON.stringify(taskList));
+});
