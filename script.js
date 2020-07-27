3ea26ce827d1ae763c78ab7ee843e6eda37c600b
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-27 19:13:27 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ botao.addEventListener("click", function() {
 })
 
 window.onclick = function(){
+    if (document.querySelector(".selected") != null) {
+        document.querySelector(".selected").style.backgroundColor = "white";
+        document.querySelector(".selected").classList.remove("selected");
+    }
     if (event.target.className === "item") {
         event.target.style.backgroundColor = "rgb(128,128,128)";
+        event.target.classList.add("selected");        
     }
 }
