bf5bf203ad106d6b117ec4ee2dd04830241901f0
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-07-27 00:40:33 -0300

    Botao apagar tudo.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const createTaskButton = document.getElementById('criar-tarefa');
-createTaskButton.addEventListener('click', createTask);
-
 function createTask() {
   const olElement = document.getElementById('lista-tarefas');
   const liElement = document.createElement('li');
@@ -9,3 +6,19 @@ function createTask() {
   olElement.appendChild(liElement);
   document.getElementById('texto-tarefa').value = '';
 }
+
+const createTaskButton = document.getElementById('criar-tarefa');
+createTaskButton.addEventListener('click', createTask);
+
+function removeAllTasks() {
+  const liElements = document.getElementsByTagName('li');
+
+  for (let i = liElements.length - 1; i >= 0; i -= 1) {
+    liElements[i].remove();
+  }
+}
+
+const removeAllTasksButton = document.getElementById('apaga-tudo');
+removeAllTasksButton.addEventListener('click', removeAllTasks);
+
+
