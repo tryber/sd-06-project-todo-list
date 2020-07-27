2a1f64e23c8b2b9ec6b6bd0d533830cb25342d20
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-07-27 16:30:20 -0300

    Eighth Task

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const btn = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const text = document.getElementById('texto-tarefa');
 
-btn.addEventListener('click', function() {
-  let li = document.createElement('li');
+btn.addEventListener('click', function () {
+  const li = document.createElement('li');
   li.innerHTML = text.value;
   list.appendChild(li);
   text.value = '';
-  li.addEventListener('click', function() {
+  li.addEventListener('click', function () {
+    for (let i = 0; i < document.querySelectorAll('li').length; i += 1) {
+      document.querySelectorAll('li')[i].style.backgroundColor = 'white';
+    }
     li.style.backgroundColor = 'rgb(128, 128, 128)';
   });
 });
