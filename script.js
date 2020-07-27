5afab4ab4fffb8b4e69d421d541e30b3a93e4a90
Alvaro Neto <alvaroshn@gmail.com>
2020-07-27 13:05:46 -0300

    passo 04

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+function initialLoad () {
+    console.log("load");
+}
+
+window.onload = initialLoad;
+
 let addButton = document.querySelector("#criar-tarefa");
 addButton.addEventListener('click',function () {
     let myList = document.querySelector("#lista-tarefa");
     let myItem = document.createElement('li');
+    myItem.className = "itemList";
     myItem.innerText = document.getElementById('texto-tarefa').value;
-    myList.appendChild(myItem);
-    clearInput();
-})
+    myList.appendChild(myItem);     
+    clearInput();   
+
+    let myListItem = document.getElementsByClassName('itemList');
+    for (let i = 0 ; i < myListItem.length ; i += 1){
+        myListItem[i].addEventListener('click',function(){
+            myListItem[i].style.backgroundColor = "rgb(128, 128, 128)";
+        });
+    }
+});
+
+
+   
+
+
 
 function clearInput () {
     document.getElementById('texto-tarefa').value = null;
