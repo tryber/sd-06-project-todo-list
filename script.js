f9a0684c8f3b377b92e783460b3a1df762de1649
theCoyote <flaviolunaferreira@gmail.com>
2020-07-27 18:39:08 -0300

    retornando alterações indesejadas

--- script.js
+++ script.js
@@ -1,105 +1,69 @@
-const inputElement = document.querySelector('#texto-tarefa');
-const elementOl = document.querySelector('#lista-tarefas');
-const createButton = document.querySelector('#criar-tarefa');
-const moveUpButton = document.querySelector('#mover-cima');
-const moveDownButton = document.querySelector('#mover-baixo');
-const deleteAllButton = document.querySelector('#apaga-tudo');
-const deleteCompletedButton = document.querySelector('#remover-finalizados');
-const deleteSelectedButton = document.querySelector('#remover-selecionado');
-const saveTasksButton = document.querySelector('#salvar-tarefas');
-
-function createElementLiAndAppendToElementOl(orderedList, value, completed = false) {
-  const elementLi = document.createElement('li');
-  elementLi.innerText = value;
-  orderedList.appendChild(elementLi);
-  if (completed) {
-    elementLi.className = 'completed';
+window.onload = function () {
+  const btnIncluir = document.getElementById('criar-tarefa');
+  const btnApagarTudo = document.getElementById('apaga-tudo');
+  btnIncluir.addEventListener('click', clickBtnCriarTarefa);
+  btnApagarTudo.addEventListener('click', clickBtnApagarTudo);
+}
+/* *********************************************** */
+function pegaTexto() {
+  const texto = document.getElementById('texto-tarefa');
+  return texto.value;
+}
+/* *********************************************** */
+function limparTabela() {
+  const itens = document.querySelectorAll('li');
+  for (let i = 0; i < itens.length; i += 1) {
+    document.getElementById('lista-tarefas').removeChild(itens[i]);
   }
 }
-function updateElements(taskList) {
-  for (let item = 0; item < taskList.length; item += 1) {
-    if (taskList[item].includes('.completed')) {
-      taskList[item] = taskList[item].replace('.completed', '');
-      createElementLiAndAppendToElementOl(elementOl, taskList[item], true);
-    } else {
-      createElementLiAndAppendToElementOl(elementOl, taskList[item]);
-    }
-  }
-}
-function loadFromStorage() {
-  if (localStorage.taskList !== undefined) {
-    const taskList = JSON.parse(localStorage.getItem('taskList'));
-    updateElements(taskList);
-  }
-}
-loadFromStorage();
-createButton.addEventListener('click', () => {
-  if (inputElement.value) {
-    createElementLiAndAppendToElementOl(elementOl, inputElement.value);
-    inputElement.value = '';
-  }
-});
-deleteAllButton.addEventListener('click', () => {
-  while (elementOl.firstChild) {
-    elementOl.removeChild(elementOl.lastChild);
-  }
-});
-elementOl.addEventListener('click', (event) => {
-  event.preventDefault();
-  const selectedTask = event.target;
-  const tasks = document.getElementsByTagName('li');
-  for (let item = 0; item < tasks.length; item += 1) {
-    if (tasks[item].classList.contains('selected')) {
-      tasks[item].classList.remove('selected');
-    }
-  }
-  selectedTask.classList.add('selected');
-});
-elementOl.addEventListener('dblclick', (event) => {
-  const selectedTask = event.target;
-  if (selectedTask.classList.contains('completed')) {
-    selectedTask.classList.remove('completed');
+/* *********************************************** */
+function criarTarefa() {
+  const texto = pegaTexto();
+  if (texto !== '') {
+    criarTabela();
   } else {
-    selectedTask.classList.add('completed');
+    alert('Campo tarefa precisa ser preenchido!!!');
   }
-});
-moveUpButton.addEventListener('click', () => {
-  const tasks = Array.from(document.getElementsByTagName('li'));
-  for (let item = 0; item < tasks.length; item += 1) {
-    if (tasks[item].classList.contains('selected') && (item !== 0)) {
-      tasks[item].parentNode.insertBefore(tasks[item], tasks[item - 1]);
-    }
+}
+/* *********************************************** */
+function criarTabela() {
+  const item = document.createElement('li');
+  item.innerText = document.getElementById('texto-tarefa').value;
+  document.getElementById('lista-tarefas').appendChild(item);
+  item.style.cursor = 'pointer';
+}
+/* *********************************************** */
+function limpaTexto() {
+  const tarefa = document.getElementById('texto-tarefa');
+  tarefa.value = '';
+  tarefa.focus();
+}
+/* *********************************************** */
+function desmarcaSelecionado() {
+  let item = document.querySelectorAll('li')
+  for (let i = 0; i < item.length; i += 1) {
+    item[i].style.backgroundColor = 'transparent';
   }
-});
-
-moveDownButton.addEventListener('click', () => {
-  const tasks = Array.from(document.getElementsByTagName('li'));
-  for (let item = 0; item < tasks.length; item += 1) {
-    if (tasks[item].classList.contains('selected') && (item !== tasks.length - 1)) {
-      tasks[item].parentNode.insertBefore(tasks[item + 1], tasks[item]);
-    }
+}
+/* *********************************************** */
+function atribuirFuncaoDbClick() {
+  var item = document.querySelectorAll('li')
+  for (let i = 0; i < item.length; i += 1) {
+    item[i].addEventListener('click', function (e) {
+      if (item[i].style.backgroundColor === 'rgb(128, 128, 128)') {
+        item[i].style.backgroundColor = 'transparent';
+      } else {
+        item[i].style.backgroundColor = 'rgb(128,128,128)';
+      }
+    });
   }
-});
-
-deleteCompletedButton.addEventListener('click', () => {
-  const tasksCompleted = Array.from(document.querySelectorAll('.completed'));
-  for (let item = 0; item < tasksCompleted.length; item += 1) {
-    elementOl.removeChild(tasksCompleted[item]);
-  }
-});
-deleteSelectedButton.addEventListener('click', () => {
-  const tasksSelected = Array.from(document.querySelectorAll('.selected'));
-  for (let item = 0; item < tasksSelected.length; item += 1) {
-    elementOl.removeChild(tasksSelected[item]);
-  }
-});
-saveTasksButton.addEventListener('click', () => {
-  const tasks = Array.from(document.getElementsByTagName('li'));
-  const taskList = tasks.map((task) => {
-    if (task.classList.contains('completed')) {
-      return `${task.innerText}.completed`;
-    }
-    return task.innerText;
-  });
-  localStorage.setItem('taskList', JSON.stringify(taskList));
-});
+}
+/* *********************************************** */
+function clickBtnCriarTarefa() {
+  criarTarefa();
+  limpaTexto();
+  atribuirFuncaoDbClick();
+}
+function clickBtnApagarTudo() {
+  limparTabela();
+}
