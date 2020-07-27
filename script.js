44e1f47e60e3f9a6724eab4546ffd39cda70e1e0
Alvaro Neto <alvaroshn@gmail.com>
2020-07-27 15:12:39 -0300

    passo 05

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 function initialLoad () {
-    console.log("load");
+    //localStorage.clear();
+    //localStorage.isColored = true;
 }
 
-window.onload = initialLoad;
+//window.onload = initialLoad;
 
 let addButton = document.querySelector("#criar-tarefa");
 addButton.addEventListener('click',function () {
     let myList = document.querySelector("#lista-tarefa");
     let myItem = document.createElement('li');
-    myItem.className = "itemList";
+    myItem.className = 'itemList';
     myItem.innerText = document.getElementById('texto-tarefa').value;
     myList.appendChild(myItem);     
     clearInput();   
-
-    let myListItem = document.getElementsByClassName('itemList');
-    for (let i = 0 ; i < myListItem.length ; i += 1){
-        myListItem[i].addEventListener('click',function(){
-            myListItem[i].style.backgroundColor = "rgb(128, 128, 128)";
-        });
-    }
+    color();
 });
-
-
-   
-
-
 
 function clearInput () {
     document.getElementById('texto-tarefa').value = null;
 }
+
+function color () {
+    let myListItem = document.getElementsByClassName('itemList');
+    for (let i = 0 ; i < myListItem.length ; i += 1){
+        myListItem[i].addEventListener('click',function(){
+            if(localStorage.isColored == "true"){
+            myListItem[i].style.backgroundColor = 'rgb(128, 128, 128)';
+            for (let x in myListItem) {
+                if (x != i) {
+                    myListItem[x].style.backgroundColor = "white";
+                }
+           }
+           localStorage.isColored = "false";
+        }
+        });
+    }
+}
+
