76f79c36839a9fd5ad5c70ed1689ce57918435a1
thalesgr <thalesgr@gmail.com>
2020-07-27 20:03:04 -0300

    mudanças no script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const tarefa = document.getElementById('texto-tarefa');
 const adicionar = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
-   
+
 adicionar.addEventListener('click', () => {
     let elemento = document.createElement('li');
     elemento.innerHTML = tarefa.value;
     elemento.className += 'item';
     lista.appendChild(elemento);
     tarefa.value = '';
-});
-
-lista.addEventListener('click', ()  => {
     let itemLista = document.querySelectorAll('.item');
-    itemLista.forEach(item =>  {
+    itemLista.forEach(item => {
         item.addEventListener('click', () => {
             itemLista.forEach(item => {
                 item.classList.remove('selected');
@@ -20,15 +17,9 @@ lista.addEventListener('click', ()  => {
             item.classList.add('selected');
         })
     })
-});
-
-lista.addEventListener('dblclick', () => {
-    let itemLista = document.querySelectorAll('.item');
     itemLista.forEach(item =>  {
         item.addEventListener('dblclick', () => {
             item.classList.toggle('completed');
         })
     })
 });
-
-
