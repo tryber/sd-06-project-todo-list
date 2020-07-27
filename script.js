d34cbf60fbb41836d03d74fd6c6192b8dff029a4
Vagner Pereira <sousa.vagner82@gmail.com>
2020-07-26 21:30:04 -0300

    Add JavaScript

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function addLista () {
     var textoTarefa = document.getElementById('texto-tarefa').value;
     let list = document.getElementById('lista-tarefas')
-    let li = document.createElement('li');
+    var li = document.createElement('li');
     li.innerHTML = textoTarefa;
     list.appendChild(li);
     document.getElementById('texto-tarefa').value = '';
-
+    li.addEventListener('click', function () {
+        if (li.style.backgroundColor !== 'rgb(128, 128, 128)') {
+            li.style.backgroundColor = 'rgb(128, 128, 128)';
+        } else {
+            li.style.backgroundColor = 'white';
+        }
+        
+    });
 }
+
 
 
 
 let criarTarefa = document.getElementById('criar-tarefa');
 criarTarefa.addEventListener('click', addLista);
+
+
 
 
