e068b1d6773070b1b56ccfabfb7fb21f35f5b8d8
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-27 20:40:26 -0300

    Adding requirements check one more time ; )

--- script.js
+++ script.js
@@ -6,7 +6,31 @@ window.onload = function () {
   addButton.addEventListener('click', function () {
     let listItem = document.createElement('li');
     listItem.innerText = taskInput.value;
+    listItem.className = 'task-iten'
     taskList.appendChild(listItem);
     taskInput.value = '';
   });
+
+  let selected = document.getElementsByClassName('selected');
+
+  console.log(selected.length)
+
+  taskList.addEventListener('click', function (event) {
+    if (event.target.classList.contains('task-iten') && selected.length === 0) {
+      event.target.classList.add('selected');
+    } else if (event.target.classList.contains('task-iten') && selected.length === 1) {
+      selected[0].className = 'task-iten';
+      event.target.classList.add('selected');
+    }
+  });
+
+  taskList.addEventListener('dblclick', function(event) {
+    if (event.target.classList.contains('task-iten')) {
+      event.target.className = "completed"
+    } else if (event.target.classList.contains('completed')) {
+      event.target.className = 'task-iten'
+    }
+    
+
+  })
 };
