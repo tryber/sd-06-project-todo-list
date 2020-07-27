d4de8c37c608bc8803cf40682aa3610cfa845986
Havyner Caetano <hrcgeo@gmail.com>
2020-07-27 10:49:14 -0300

    Validate that only one li can have the selected css class at a time

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const addTaskButton = document.querySelector('button:nth-of-type(1)');
 const taskList = document.querySelector('ol');
 const taskText = document.querySelector('input');
-let tasks = [];
+let currentLi;
 
 addTaskButton.addEventListener('click', function () {
   const li = document.createElement('li');
@@ -9,11 +9,17 @@ addTaskButton.addEventListener('click', function () {
   li.innerText = taskText.value;
 
   li.addEventListener('click', function (event) {
-    event.path[0].className = 'selected';
+    if (!currentLi) {
+      event.path[0].className = 'selected';
+      currentLi = li;
+    } else {
+      currentLi.classList.remove('selected');
+      currentLi = event.path[0];
+      currentLi.className = 'selected';
+    }
   })
 
   taskText.value = '';
   taskList.appendChild(li);
-  tasks.push(li);
 })
 
