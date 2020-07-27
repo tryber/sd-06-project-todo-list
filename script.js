d534447867e47b7684c7440140bbeadb67828c56
Vagner Pereira <sousa.vagner82@gmail.com>
2020-07-27 19:00:08 -0300

    Add JavaScript

--- script.js
+++ script.js
@@ -1,52 +1,61 @@
-function addLista () {
+const newTask = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+const rmFinished = document.getElementById('remover-finalizados');
+const rmSelected = document.getElementById('remover-selecionado');
+const rmAll = document.getElementById('apaga-tudo');
+
+newTask.addEventListener('click', function () {
     var textoTarefa = document.getElementById('texto-tarefa').value;
     var list = document.getElementById('lista-tarefas')
     var li = document.createElement('li');
     li.innerHTML = textoTarefa;
     list.appendChild(li);
     document.getElementById('texto-tarefa').value = '';
-    li.addEventListener('click', function () {
-        if (li.style.backgroundColor !== 'rgb(128, 128, 128)') {
-            li.style.backgroundColor = 'rgb(128, 128, 128)';
+});
+
+taskList.addEventListener('click', function(event) {
+    const li = document.getElementsByTagName('li');
+    for (let i = 0; i < li.length; i += 1) {
+        if (li[i].style.backgroundColor === 'rgb(128, 128, 128)') {
+            li[i].style.backgroundColor = 'transparent';
         } 
-        else {
-            li.style.backgroundColor = 'white';
-        }   
-    });
-    li.addEventListener('dblclick', function () {
-        if (li.className !== 'completed') {
-            li.className = 'completed';
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+})
+
+taskList.addEventListener('dblclick', function(event) {
+    if (event.target.className !== 'completed') {
+        event.target.className = 'completed';
         } else {
-            li.className = 'incompleted';
-        }
-    });
-    var rmFinalizado = document.getElementById('remover-finalizados');
-    rmFinalizado.addEventListener('click', function () {
-        let aux = document.getElementsByClassName('completed');
-        for (let i = 0; i < aux.length; i += 1) {
+        event.target.className = 'incompleted';
+    }
+});
+
+rmFinished.addEventListener('click', function() {
+    let aux = document.querySelectorAll('.completed');
+    for (let i = 0; i < aux.length; i += 1) {
+    document.getElementById('lista-tarefas').removeChild(aux[i]);
+    }
+});
+
+rmSelected.addEventListener('click', function() {
+    let aux = document.getElementsByTagName('li');
+    for (let i = 0; i < aux.length; i += 1) {
+        if (aux[i].style.backgroundColor === 'rgb(128, 128, 128)') {
             document.getElementById('lista-tarefas').removeChild(aux[i]);
         }
+    }
+});
 
-    });
-    var rmSelecionado = document.getElementById('remover-selecionado');
-    rmSelecionado.addEventListener('click', function() {
-        let aux = document.getElementsByTagName('li');
-        for(let i = 0; i < aux.length; i += 1) {
-            if (aux[i].style.backgroundColor === 'rgb(128, 128, 128)') {
-                document.getElementById('lista-tarefas').removeChild(aux[i]);
-            }
-        }
-        
-
-    });
-
-}
+rmAll.addEventListener('click', function() {
+    const aux = document.getElementById('lista-tarefas');
+    while (aux.firstChild) {
+        aux.firstChild.remove();
+    }
+   
+});
 
 
-
-
-var criarTarefa = document.getElementById('criar-tarefa');
-criarTarefa.addEventListener('click', addLista);
 
 
 
