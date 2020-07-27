551f2aa714e16aaec3b4f981226b8123667d3e2c
Alvaro Neto <alvaroshn@gmail.com>
2020-07-27 16:39:54 -0300

    passo 08

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function initialLoad () {
     localStorage.clear();
-    
+    createTask();
+   
     //localStorage.isColored = true;
 }
 
 window.onload = initialLoad;
 
 
-
+function createTask(){
 let addButton = document.querySelector("#criar-tarefa");
 addButton.addEventListener('click',function () {
     let myList = document.querySelector("#lista-tarefas");
@@ -17,8 +18,8 @@ addButton.addEventListener('click',function () {
     myList.appendChild(myItem);     
     clearInput();   
     color();
-    
 });
+}
 
 function clearInput () {
     document.getElementById('texto-tarefa').value = null;
@@ -27,10 +28,8 @@ function clearInput () {
 function color () {
     let myListItem = document.getElementsByClassName('itemList');
     for (let i = 0 ; i < myListItem.length ; i += 1){
-        
         myListItem[i].addEventListener('click',function(){
             myListItem[i].style.backgroundColor = 'rgb(128, 128, 128)';
-        
             for (let x = 0 ; x < myListItem.length ; x += 1) {
                 if (x != i) {
                     myListItem[x].style.backgroundColor = 'transparent';
@@ -38,11 +37,21 @@ function color () {
            }
         });
     }
-    
+   
 }
 
-
-
-
+// function doubleClick(event){
+//     let doubleCheck = document.getElementsByClassName('itemList');
     
+//     for (let i = 0 ; i < doubleCheck.length ; i += 1){
+       
+//         if (doubleCheck[i].style.backgroundColor == 'rgb(128, 128, 128)'){
+//             console.log(event.target);
+//             doubleCheck[i].addEventListener('dblclick',function(){
+//             doubleCheck[i].style.className = 'completed';
+            
+//             });
+//         }
+//     }               
+// }
 
