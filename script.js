167615a4086d6f1d7065f901abe4a7c8b01026ac
fefarina <wfelipefarina@gmail.com>
2020-07-27 16:07:17 -0300

    update js file

--- script.js
+++ script.js
@@ -2,22 +2,38 @@ let buttonAdd = document.querySelector('#criar-tarefa');
 let list = document.querySelector('#lista-tarefas');
 let inputTarefa = document.querySelector('#texto-tarefa');
 let eraseButton = document.querySelector('#apaga-tudo')
+let allListTasks = [];
 
 function insertTask() {
     buttonAdd.addEventListener('click', function (e) {
+        e.preventDefault();
         createTask();
+        inputTarefa.value = ''; 
+        
+        for (let i = 0; i < allListTasks.length; i++) { 
+            allListTasks[i].addEventListener('dblclick', function() {
+                allListTasks[i].classList.add('completed')
+            })           
+            
+        }
     })
 
 }
+insertTask();
 
 function createTask() {
     let itensList = document.createElement('li');
-    list.appendChild(itensList);
     itensList.innerText = inputTarefa.value;
+    list.appendChild(itensList);
+    allListTasks.push(itensList);
+}
+createTask();
 
-    inputTarefa.value = '';
+function selectedTask() {
+    
 }
 
+selectedTask();
 
 function deleteAllTasks() {
     eraseButton.addEventListener('click', function () {
@@ -25,6 +41,5 @@ function deleteAllTasks() {
     })
 }
 
-createTask();
-insertTask();
+
 deleteAllTasks();
