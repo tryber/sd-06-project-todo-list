115fa9c9f3c20a757a0182d06bfd89c24f6bfd63
Alvaro Neto <alvaroshn@gmail.com>
2020-07-27 19:11:51 -0300

    passo 10

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function initialLoad () {
     localStorage.clear();
     createTask();
-   
+    
     //localStorage.isColored = true;
 }
 
 window.onload = initialLoad;
+
+
+
 
 
 function createTask(){
@@ -13,12 +16,22 @@ let addButton = document.querySelector("#criar-tarefa");
 addButton.addEventListener('click',function () {
     let myList = document.querySelector("#lista-tarefas");
     let myItem = document.createElement('li');
+    myItem.id = "itemListID"
     myItem.className = 'itemList'
+        myItem.addEventListener('dblclick',function(){
+            switch (myItem.className){
+                case 'completed':
+                    myItem.className='itemList';
+                break;
+                case 'itemList':
+                    myItem.className='completed';
+                break;
+            }
+        });     
     myItem.innerText = document.getElementById('texto-tarefa').value;
     myList.appendChild(myItem);     
     clearInput();   
     color();
-    doubleClick();
 });
 }
 
@@ -27,12 +40,10 @@ function clearInput () {
 }
 
 function color () {
-    let saveValue=0;
     let myListItem = document.getElementsByTagName('li');
     for (let i = 0 ; i < myListItem.length ; i += 1){
         myListItem[i].addEventListener('click',function(){
             myListItem[i].style.backgroundColor = 'rgb(128, 128, 128)';
-            saveValue=i;
             for (let x = 0 ; x < myListItem.length ; x += 1) {
                 if (x != i) {
                     myListItem[x].style.backgroundColor = 'transparent';
@@ -42,18 +53,11 @@ function color () {
     }
 }
 
-function doubleClick(){
-    let contador1 = 0;
-    let contador2 = 0;
-    let myItem = document.getElementsByTagName('li');
-    for (let i = 0 ; i < myItem.length ; i += 1){
-        myItem[i].addEventListener('dblclick',function(){
-            if ( myItem[i].className == 'itemList') {
-                myItem[i].className = 'completed';   
-            } else if ( myItem[i].className == 'completed'){
-                myItem[i].className = 'itemList';
-            } 
-        });
+let deleteButton = document.getElementById('apaga-tudo');
+let listToDelete = document.getElementsByTagName('li');
+console.log(listToDelete)
+deleteButton.addEventListener('click',function(){
+    while (listToDelete.length > 0){
+        document.getElementsByTagName('li')[0].remove();
     }
-    
-}
+});
