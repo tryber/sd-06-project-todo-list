4e8041f2b65b7bc065c08878b966e6427d089f31
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-26 22:54:05 -0300

    botao apagar tudo

---
+++ script.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+  //Boton Adicionar que cria elemnetos na lista ordenada, e os mostra. Apaga o conteudo da caixa de texto ao adicionar.
+
+  let botaoAdicionar = document.querySelector('#criar-tarefa');
+  botaoAdicionar.addEventListener('click', function() {
+    textoInput = document.querySelector('#texto-tarefa').value;
+    document.querySelector('#texto-tarefa').value = "";
+    let listaElemento = document.createElement ('li');
+    listaElemento.innerText = textoInput;
+    document.querySelector('#lista-tarefas').appendChild(listaElemento);
+  })
+
+  //Ao fazer click  num elemento da lista ele coloca fundo cinzas
+  let listaColor = document.querySelector('#lista-tarefas');
+  listaColor.addEventListener('click', function(event) {
+    let lista = event.target;
+    lista.className = "grey"; 
+  })
+
+
+  //Botao para apagar tudo o conteudo da lista 
+  let botaoApagar = document.querySelector('#apaga-tudo');
+  botaoApagar.addEventListener('click', function() {
+    let lista = document.querySelector('#lista-tarefas');
+    let iten = document.querySelectorAll('li');
+    for (let i = 0; i < iten.length; i += 1){
+      lista.removeChild(lista.children[0]);
+    }
+  })
+
+}
