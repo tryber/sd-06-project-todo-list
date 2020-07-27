ef02222a594ba477eca93b70f6a63e31be7930fb
fefarina <wfelipefarina@gmail.com>
2020-07-27 14:25:16 -0300

    Updte js files

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 let buttonAdd = document.querySelector('#criar-tarefa');
 let list = document.querySelector('#lista-tarefas');
 let inputTarefa = document.querySelector('#texto-tarefa');
+let eraseButton = document.querySelector('#apaga-tudo')
 
 function insertTask() {
-    buttonAdd.addEventListener('click', function(e) {
+    buttonAdd.addEventListener('click', function (e) {
         createTask();
     })
-    
+
 }
 
 function createTask() {
     let itensList = document.createElement('li');
     list.appendChild(itensList);
     itensList.innerText = inputTarefa.value;
+
     inputTarefa.value = '';
 }
 
 
-
-
+function deleteAllTasks() {
+    eraseButton.addEventListener('click', function () {
+        list.innerHTML = ''
+    })
+}
 
 createTask();
 insertTask();
+deleteAllTasks();
