bbee59a8bac0cbcb053e06bbde3d522a5aec9f86
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-27 18:39:03 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const botao = document.querySelector("#criar-tarefa");
 botao.addEventListener("click", function() {
     let item = document.createElement("li");
+    item.className = "item";
     let texto = document.querySelector("#texto-tarefa");
     item.innerHTML = texto.value;
     document.querySelector("#lista-tarefas").appendChild(item);
     texto.value = "";
 })
+
+window.onclick = function(){
+    if (event.target.className === "item") {
+        event.target.style.backgroundColor = "rgb(128,128,128)";
+    }
+}
