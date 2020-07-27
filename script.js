e6ab16eb4656fa3e2a4dc83516a71d5fc4fb47d0
us19861229c <rcks.santos@gmail.com>
2020-07-27 12:09:11 -0300

    Modifica nome de variáveis para torná-las mais compreensíveis, implementa a seleção da tarefa (atribuindo e removendo classe - que confere um background cinza ao texto).

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const enviarTexto = document.querySelector('#criar-tarefa');
-const listarOrdenadamente = document.querySelector('#lista-tarefas');
+const listaTarefa = document.querySelector('#lista-tarefas');
+const textoTarefa = document.querySelector('#texto-tarefa');
 
 enviarTexto.addEventListener('click', function (event) {
-  event.preventDefault()
-  const tarefaLi = document.createElement('li');
-  const caixaTexto = document.querySelector('#texto-tarefa');
-  tarefaLi.innerHTML = caixaTexto.value;
-  listarOrdenadamente.appendChild(tarefaLi);
+  event.preventDefault();
+
+  const novaTarefa = document.createElement('li');
+  novaTarefa.innerHTML = textoTarefa.value;
+  listaTarefa.appendChild(novaTarefa);
+  novaTarefa.addEventListener('click', function () {
+    novaTarefa.classList.toggle('tarefa-selecionada');
+  });
+
+  textoTarefa.value = '';
 });
+
