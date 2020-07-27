eb68e4e77a6af84d21f5f9da05cf87af6b617d38
Marxneves <marxkhristie@gmail.com>
2020-07-26 21:48:06 -0300

    Terminei o requisito de limpar todos e de deixar riscado.

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const inputText = document.getElementById("texto-tarefa");
 const list = document.getElementById("lista-tarefas");
 const btnCriar = document.getElementById("criar-tarefa");
+const btnApagar = document.getElementById("apaga-tudo");
 
-btnCriar.addEventListener('click', function(){
+btnCriar.addEventListener('click', function () {
   let elementoLista = document.createElement("li");
   list.appendChild(elementoLista);
   elementoLista.innerText = inputText.value;
-  console.log(elementoLista);
   inputText.value = "";
 })
+
+list.addEventListener('click', function(event) {
+  let li = event.target;
+  if (li.style.backgroundColor === 'rgb(128, 128, 128)'){
+    li.style.backgroundColor = 'white';
+  } else {
+    li.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+});
+
+list.addEventListener('dblclick', function (event) {
+  let li = event.target;
+  if (li.style.textDecoration === 'line-through'){
+    li.style.textDecoration = 'none';
+  } else {
+    li.style.textDecoration = 'line-through';
+  }
+});
+
+btnApagar.addEventListener('click', function () {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+});
+
