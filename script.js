c58d20e1c8320eb413c81783d8a9e907641722e0
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-07-28 13:26:19 -0300

    adicionando java

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ window.onload = function () {
             event.target.classList.add('selected');
         }
     });
+    taskList.addEventListener('dblclick', function (event) {
+        if (event.target.classList.contains('task-iten')) {
+            event.target.className = "completed"
+        } else if (event.target.classList.contains('completed')) {
+            event.target.className = 'task-iten';
+        }
+    });
+
+    let clearAll = document.querySelector('#apaga-tudo')
+    let numberOfTasks = document.getElementById('lista-tarefas')
+    clearAll.addEventListener('click', function () {
+        numberOfTasks.innerHTML = '';
+    });
+
+    let clearCompleted = document.querySelector('#remover-finalizados');
+
+    clearCompleted.addEventListener('click', function () {
+
+    });
+};
