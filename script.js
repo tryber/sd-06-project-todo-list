62fbf5b8d16f5e420b12c21620a5f50e3ca03487
luiz <phellipe501@gmail.com>
2020-07-28 17:19:19 -0300

    fazendo os ultimos obrigatorios

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.onload = function(){
     var inptTarefa = document.querySelector("#texto-tarefa");
     var btnTarefa = document.querySelector("#criar-tarefa");
     var lista = document.querySelector("#lista-tarefas");
-
-
+    var apagaTudo = document.querySelector("#apaga-tudo");
+    var finalizados  = document.querySelector("#remover-finalizados");
 
     btnTarefa.addEventListener("click", function(){
        var insideList = document.createElement("li");
@@ -11,4 +11,33 @@ window.onload = function(){
        insideList.innerText = inptTarefa.value;
        inptTarefa.value = "";
     })
+    
+    lista.addEventListener("click", function(event){
+        for(let i = 1; i < lista.childNodes.length; i += 1){
+            lista.childNodes[i].style.backgroundColor = "";
+        } 
+        event.target.style.backgroundColor = "rgb(128,128,128)";
+    })
+
+    lista.addEventListener("dblclick", function(event){
+        if(event.target.className == "completed"){
+            event.target.className = " ";
+        event.target.style.textDecoration = "none";
+        }else{
+            event.target.className = "completed";
+        event.target.style.textDecoration = "line-through solid rgb(0, 0, 0)";
+        }
+
+    })
+    apagaTudo.addEventListener("click", function(){
+        lista.innerHTML = "";
+    })
+
+    finalizados.addEventListener("click", function(){
+        
+    })
+
+
+
+    
 }
