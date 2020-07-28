563dceb2379e3fc8a5bc11445ac7d24c5433453b
Ana Flávia <anaflvia@gmail.com>
2020-07-28 15:53:41 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let botaoAdicionar = document.querySelector('#criar-tarefa')
-botaoAdicionar.addEventListener('click', function() {
+let botaoAdicionar = document.querySelector('#criar-tarefa');
+botaoAdicionar.addEventListener('click', function () {
   let tarefaAdicionada = document.querySelector('#texto-tarefa');
   let li = document.createElement('li');
   li.innerHTML = tarefaAdicionada.value;
   document.getElementById('lista-tarefas').appendChild(li);
-  tarefaAdicionada.value = ""
-})
+  tarefaAdicionada.value = '';
+});
+let tarefaSelecionada = document.getElementById('lista-tarefas');
+tarefaSelecionada.addEventListener('click', function() {
+  let elementoSelecionado = event.target;
+  elementoSelecionado.classList.add('selected');
+});
