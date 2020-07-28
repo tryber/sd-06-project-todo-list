e1aab78d73b68aec3851b05466c7f0de8991090e
higoramorim <1810042@aluno.univesp.br>
2020-07-28 18:08:31 -0300

    cc II

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-window.onload = function(){
-const criarTarefa = document.querySelector("#criar-tarefa");
-const listaTarefa = document.querySelector("#lista-tarefas");
-const textoTarefa = document.querySelector("#texto-tarefa");
-const removerSelecionado = document.querySelector("#remover-selecionado");
-const moverCima = document.querySelector("#mover-cima");
-const moverBaixo = document.querySelector("#mover-baixo");
-const apagaTudo = document.querySelector("#apaga-tudo");
-const removerFinalizados = document.querySelector("#remover-finalizados");
-const salvarTarefas = document.querySelector("#salvar-tarefas");
+window.onload = function() {
+  const criarTarefa = document.querySelector('#criar-tarefa');
+  const listaTarefa = document.querySelector('#lista-tarefas');
+  const textoTarefa = document.querySelector('#texto-tarefa');
+  const removerSelecionado = document.querySelector('#remover-selecionado');
+  const moverCima = document.querySelector('#mover-cima');
+  const moverBaixo = document.querySelector('#mover-baixo');
+  const apagaTudo = document.querySelector('#apaga-tudo');
+  const removerFinalizados = document.querySelector('#remover-finalizados');
+  const salvarTarefas = document.querySelector('#salvar-tarefas');
 
-criarTarefa.addEventListener('click', criarElementLi);
-function criarElementLi(){
-  let li = document.createElement('li');
-  li.innerText = textoTarefa.value;
-  listaTarefa.appendChild(li);
-  li.className = 'tarefa';         
-};
+  criarTarefa.addEventListener('click', criarElementLi);
+  function criarElementLi(){
+    let li = document.createElement('li');
+    li.innerText = textoTarefa.value;
+    listaTarefa.appendChild(li);
+    li.className = 'tarefa';         
+  };
 
-const classeSelecionada = document.getElementsByClassName('selected');
-console.log(classeSelecionada.length);
+  const classeSelecionada = document.getElementsByClassName('selected');
+  console.log(classeSelecionada.length);
 
-listaTarefa.addEventListener('click' , function(event){
-  if (event.target.classList.contains('tarefa') && classeSelecionada.length === 0){
-    event.target.classList.add('classeSelecionada');
-  }else{
-    event.target.classList.contains('tarefa') && classeSelecionada.length === 1
-  }
-    classeSelecionada[0].className = 'tarefa';
-    event.target.classList.add('classeSelecionada');
-});
+  listaTarefa.addEventListener('click' , function(event){
+    if (event.target.classList.contains('tarefa') && classeSelecionada.length === 0){
+      event.target.classList.add('classeSelecionada');
+    }else{
+      event.target.classList.contains('tarefa') && classeSelecionada.length === 1
+    }
+      classeSelecionada[0].className = 'tarefa';
+      event.target.classList.add('classeSelecionada');
+  });
 
-listaTarefa.addEventListener('dbclick', function(event){
-  if (event.target.classList.contains('tarefa')){
-    event.target.className = 'completed';
-  }else{
-    event.target.className = 'tarefa';
-  }
-});
+  listaTarefa.addEventListener('dbclick', function(event){
+    if (event.target.classList.contains('tarefa')){
+      event.target.className = 'completed';
+    }else{
+      event.target.className = 'tarefa';
+    }
+  });
 
-apagaTudo.addEventListener('click', function(){
-  numberOfTasks.innerHTML = '';
-});
+  apagaTudo.addEventListener('click', function(){
+    numberOfTasks.innerHTML = '';
+  });
 
-removerFinalizados.addEventListener('click', function(){
+  removerFinalizados.addEventListener('click', function(){
 
-});
+  });
 }
