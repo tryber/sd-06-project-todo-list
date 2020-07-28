5f4b23f16ee5a0691a0eaeb6f46b110907c77883
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-07-28 13:16:25 -0300

    mais java

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ window.onload = function () {
         taskList.appendChild(listItem);
         taskInput.value = '';
     });
-
     let selected = document.getElementsByClassName('selected');
 
     console.log(selected.length)
@@ -23,24 +22,3 @@ window.onload = function () {
             event.target.classList.add('selected');
         }
     });
-
-    taskList.addEventListener('dblclick', function (event) {
-        if (event.target.classList.contains('task-iten')) {
-            event.target.className = "completed"
-        } else if (event.target.classList.contains('completed')) {
-            event.target.className = 'task-iten';
-        }
-    });
-
-    let clearAll = document.querySelector('#apaga-tudo')
-    let numberOfTasks = document.getElementById('lista-tarefas')
-    clearAll.addEventListener('click', function () {
-        numberOfTasks.innerHTML = '';
-    });
-
-    let clearCompleted = document.querySelector('#remover-finalizados');
-
-    clearCompleted.addEventListener('click', function () {
-
-    });
-};
