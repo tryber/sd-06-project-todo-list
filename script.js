2d1ada224ee720a88f6376e9dd90a28a33b99201
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-28 18:11:48 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-addEventListener('click', function insertText() {
-    const text = document.getElementById('texto-tarefa');
-    document.getElementById('lista-tarefas').innerHTML = text.value;
-  });
+const button = document.getElementById('criar-tarefa');
+
+button.addEventListener('click', createTask)
+
+function createTask(){
+const listOl = document.getElementById('lista-tarefas');
+const input = document.getElementById('texto-tarefa');
+const newItem = document.createElement('li');
+newItem.innerHTML = input.value
+listOl.appendChild(newItem)
+input.value = ""
+newItem.addEventListener('click', changeColorItem())
+newItem.addEventListener('dblclick', scratchItem())
+}
