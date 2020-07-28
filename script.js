e25896087b0e846c2d3122043d7706b18783dc9a
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-07-28 18:54:43 -0300

    Implementado funcionalidade dos itens  7 e 8

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 
-const caixaTexto = document.querySelector('#texto-tarefa');
+const boxText = document.querySelector('#texto-tarefa');
+const liSelected = document.querySelector("#lista-tarefas");
+const buttonNewTask = document.querySelector("#criar-tarefa");
+const buttonClear = document.querySelector("#apaga-tudo");
+const listItem = document.getElementById('lista-tarefas');
 
 function newTask() {
   const novoItem = document.createElement("li");
-  novoItem.innerHTML = caixaTexto.value;
+  novoItem.innerHTML = boxText.value;
   document.getElementById("lista-tarefas").appendChild(novoItem);
   document.getElementById('texto-tarefa').value = "";
 }
-buttonNewTask = document.querySelector("#criar-tarefa");
-buttonNewTask.addEventListener("click", newTask)
+
+buttonNewTask.addEventListener("click", newTask);
 
 function clearAll() {
   console.log("apagando");
+  document.getElementById('lista-tarefas').innerHTML = ''
 }
-buttonClear = document.querySelector("#apaga-tudo");
-buttonClear.addEventListener("click", clearAll)
 
+buttonClear.addEventListener("click", clearAll);
+
+// function selectLi(event) {
+//    console.log("chamou?")
+//    document.querySelector('selected').classList.remove('selected');
+//    event.target.classList.add('selected');
+// }
+
+// liSelected.addEventListener('click', selectLi)
+
+function selecionaItem(event) {
+  const selectedItem = event.target;
+  const taskList = document.querySelectorAll('#lista-tarefas li');
+  for (let i = 0; i < taskList.length; i += 1) {
+    if (taskList[i].classList.contains('selected')) {
+      taskList[i].classList.remove('selected');
+    }
+  }
+  selectedItem.classList.add('selected');
+}
+listItem.addEventListener('click', selecionaItem)
 
