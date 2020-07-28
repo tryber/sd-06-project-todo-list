a39691099a13ecbe975f5025cc77f6d4cc4fa759
Daniele Perse <danieleperse3@gmail.com>
2020-07-28 18:34:24 -0300

    adicionado funçao ref. o item 5 - listar itens em ordem e apagar o texto do inpput para receber outro

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-window.onload = function () {
+function listarItens() {
+  const textoTarefa = document.getElementById('texto-tarefa');
+  const listaTarefas = document.getElementById('lista-tarefas');
+  
+  var itemLista = document.createElement('li');
+  listaTarefas.appendChild(itemLista);
+  itemLista.innerHTML = textoTarefa.value;
+  textoTarefa.value = '';
+}
 
+window.onload = function () {
+  const criarTarefa = document.querySelector('#criar-tarefa');
+  criarTarefa.addEventListener('click', listarItens);
 };
