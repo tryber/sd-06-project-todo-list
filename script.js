feedd15f3bd982102ac7df86e0e752fe642223ab
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-28 18:33:43 -0300

    Se seleciona so um item e fica no cor cinza

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.onload = function () {
   //Boton Adicionar que cria elemnetos na lista ordenada, e os mostra. Apaga o conteudo da caixa de texto ao adicionar.
-
   let botaoAdicionar = document.querySelector('#criar-tarefa');
   botaoAdicionar.addEventListener('click', function() {
     textoInput = document.querySelector('#texto-tarefa').value;
@@ -8,20 +7,27 @@ window.onload = function () {
     let listaElemento = document.createElement ('li');
     listaElemento.innerText = textoInput;
     document.querySelector('#lista-tarefas').appendChild(listaElemento);
-  })
+  });
   //Ao fazer click  num elemento da lista ele coloca fundo cinzas
   let listaColor = document.querySelector('#lista-tarefas');
   listaColor.addEventListener('click', function(event) {
-    let lista = event.target;
-    lista.className = "grey"; 
-  })
-  //Botao para apagar tudo o conteudo da lista 
+    let itemSelecionadoAntiguio = document.querySelector('.grey');
+    let itemSelecionado = event.target;
+    if (itemSelecionadoAntiguio) {
+      itemSelecionadoAntiguio.classList.remove('grey');
+      itemSelecionado.classList.add('grey');
+    }
+    else {
+      itemSelecionado.classList.add('grey');
+    };
+  });
+  //Botao para apagar tudo o conteudo da lista
   let botaoApagar = document.querySelector('#apaga-tudo');
   botaoApagar.addEventListener('click', function() {
     let lista = document.querySelector('#lista-tarefas');
     let iten = document.querySelectorAll('li');
-    for (let i = 0; i < iten.length; i += 1){
+    for (let i = 0; i < iten.length; i += 1) {
       lista.removeChild(lista.children[0]);
-    }
-  })
-}
+    };
+  });
+};
