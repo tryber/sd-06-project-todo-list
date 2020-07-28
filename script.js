2f4332a61e8ba316e3a96104e0d690011e8ddd61
DehSartoreto <fixsartoreto@gmail.com>
2020-07-28 19:01:36 -0300

    colorindo a lista

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ window.onload = function() {
 
     btnTask.addEventListener("click", function() {
         let newLI = document.createElement("li");
+        newLI.setAttribute("class", "listTask")
         newLI.innerHTML = listContent.value;
-        orderList.insertBefore(newLI, orderList.firstChild);
+        orderList.appendChild(newLI);
         listContent.value = "";
     })
+
+    let colorTask = "white";
+
+    orderList.addEventListener("click", function(event) {
+        let clickTask = event.target;
+        if (colorTask === "white") {
+            clickTask.style.backgroundColor = "rgb(128, 128, 128)";
+            colorTask = "grey";
+        } else {
+            clickTask.style.backgroundColor = "white";
+            colorTask = "white";
+        }
+    })
+
+
+
+
+
+
 }
