3ec76ac144fbafe94bfb05999eb135c02b552723
Ana Flávia <anaflvia@gmail.com>
2020-07-28 16:12:49 -0300

    Requisito 8 e parte do 9

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let botaoAdicionar = document.querySelector('#criar-tarefa');
+
 botaoAdicionar.addEventListener('click', function () {
   let tarefaAdicionada = document.querySelector('#texto-tarefa');
   let li = document.createElement('li');
@@ -6,8 +7,26 @@ botaoAdicionar.addEventListener('click', function () {
   document.getElementById('lista-tarefas').appendChild(li);
   tarefaAdicionada.value = '';
 });
+
 let tarefaSelecionada = document.getElementById('lista-tarefas');
+
 tarefaSelecionada.addEventListener('click', function() {
+  let elementoSelecionadoAnteriormente = document.querySelector('.selected');
+  if (elementoSelecionadoAnteriormente) {
+    elementoSelecionadoAnteriormente.classList.remove('selected');
+  }
   let elementoSelecionado = event.target;
   elementoSelecionado.classList.add('selected');
 });
+
+tarefaSelecionada.addEventListener('dblclick', function () {
+  let elementoSelecionado = event.target;
+  elementoSelecionado.classList.add('completed');
+})
+
+tarefaSelecionada.addEventListener('dblclick', function () {
+  elementoRiscado = document.querySelector('.completed');
+  if (event.target == elementoRiscado) {
+    elementoRiscado.classList.remove('.completed')
+  }
+})
