09c06f4ee4ae2e6f4181692f91bd28f26361337f
Matheus Dias <matheuscara2000@gmail.com>
2020-07-29 19:26:37 -0300

    Adicionando os quesito 7

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-//variaveis
+// variaveis
+
 const input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
 
 
-//funcões
+// funcões
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
     let text = input.value;
     let tarefa = document.createElement('li');
-    tarefa.innerText=text;
-    tarefa.classList.add="work";
-    if(text === ""){
+    tarefa.innerText = text;
+    tarefa.classList.add = 'work';
+    if (text === '') {
         alert("Por favor, Digite algo no input");
-    } else{
+    } else {
         list.appendChild(tarefa);
-        input.value="";
+        input.value = '';
     };
 });
+
+
+list.addEventListener ('dblclick', function(e) {
+    e.target.style.backgroundColor = 'rgb(128,128,128)'
+})
