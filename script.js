d0fb0bb0277e61a0069b3ac0d462515cdc4f9ed9
Matheus Dias <matheuscara2000@gmail.com>
2020-07-29 18:11:38 -0300

    Adicionando os li ordenados

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//dddd
+//variaveis
+const input = document.querySelector('#texto-tarefa');
+const button = document.querySelector('#criar-tarefa');
+const list = document.querySelector('#lista-tarefas');
+
+
+//funcões
+
+button.addEventListener('click', function(){
+    let text = input.value;
+    let tarefa = document.createElement('li');
+    tarefa.innerText=text;
+    tarefa.classList.add="work";
+    if(text === ""){
+        alert("Por favor, Digite algo no input");
+    } else{
+        list.appendChild(tarefa);
+    };
+});
