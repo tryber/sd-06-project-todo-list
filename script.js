8201d37f7224c7501ab4a7470daaf84d35780092
Marthus Sera <marthus2001@gmail.com>
2020-07-29 16:26:16 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function listListItems() {
   let items = document.getElementsByTagName('li');
   for (let i = 0; i < items.length; i++) {
     items[i].addEventListener('click', function() {changeBackground(items[i])});
+    items[i].addEventListener('dblclick', function() {completed(items[i])});
   }
 }
 
@@ -27,6 +28,26 @@ function changeBackground(item) {
   oldBg.style.backgroundColor = "";
   item.classList.add('cinza');
   item.style.backgroundColor = "rgb(128, 128, 128)";
+}
+
+function completed(item) {
+  let temp = document.getElementsByClassName('completed');
+  if (findItemInElements(item, temp)) {
+    item.classList.remove('completed');
+  } else {
+    item.classList.add('completed');
+  }
+}
+
+function findItemInElements(item, elements) {
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i] == item) {
+      return 1
+    }
+  }
+
+  return 0
+
 }
 
 button.addEventListener('click', function() {addNewItem()});
