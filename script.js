03c8c03e22020e4cf2145cba96c17a9f00578337
isabellavjs <isabellavjs@gmail.com>
2020-07-29 18:56:40 -0300

    Requirement 10

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const orderedList = document.getElementById('lista-tarefas');
 const typedtext = document.getElementById('texto-tarefa');
 const addTaskButton = document.getElementById('criar-tarefa');
+const clearButton = document.getElementById('apaga-tudo');
 
 
 //Create the list itens, and add them in the orderedList when the button is clicked.
@@ -18,11 +19,24 @@ addTaskButton.addEventListener('click', addItens);
 //Order itens
 
 //Change the background color of a selected iten when clicked
-function selectTask(event) {
-  let selectedTask = event.target;
-  if (document.querySelector('.selected-task') !== null) {
-    document.querySelector('.selected-task').classList.remove('selected-task');
-  } else {
-    document.querySelector('.selected-task').classList.add('selected-task');
+orderedList.childNodes.addEventListener('click', function(event) {
+  let liItens = target.event;
+  for (let index = 0; index < orderedList.childNodes.length; index += 1) {
+    if (orderedList.childNodes[index] !== liItens) {
+      orderedList.childNodes[index].style.backgroundColor = 'white';
+    }
   }
-}
+  if (liItens.style.backgroundColor === 'rgb(128, 128, 128)') {
+    liItens.style.backgroundColor = 'white';
+  } else {
+    liItens.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+  })
+
+  //Clear the list content
+  clearButton.addEventListener('click', function() {
+    let displayList = document.querySelectorAll('li');
+    for (let index in displayList) {
+      orderedList.removeChild(displayList[index]);
+    }
+  })
