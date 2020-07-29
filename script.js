9ee154368459fdba1654cb361723f4646f5d1d90
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-29 15:18:15 -0300

    5. Add tarefas na lista

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ function criaLista() {
   lista.id = 'lista-tarefas';
   document.body.appendChild(lista);
 }
-
 criaLista();
+
+function addTarefa() {
+  const textItem = document.querySelector('#texto-tarefa');
+  const lista = document.querySelector('#lista-tarefas');
+  const item = document.createElement('li');
+  item.innerHTML = textItem.value;
+  textItem.value = '';
+  lista.appendChild(item);
+}
+const btnAdd = document.querySelector('#criar-tarefa');
+btnAdd.addEventListener('click', addTarefa);
