d4d4584059d5fec4924cf75f1e39f57d703b8307
Alessandra <itsalematos@hotmail.com>
2020-07-29 17:08:10 -0400

    5º e 6º Add botão que adiciona item e limpando input

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// iniciando
+var button = document.querySelector('#criar-tarefa');
+var list = document.querySelector('#lista-tarefas')
+
+button.addEventListener('click', insertTask);
+
+function insertTask() {
+    let task =  document.querySelector('#texto-tarefa').value;
+    let btn = document.createElement('li');
+    btn.innerHTML = task;
+    list.appendChild(btn);
+
+    let cleanInput = document.getElementById("texto-tarefa").value = "";
+}
