ec8e22603ed3adedb304032a1e3b8679e988089b
cyranowebdev <cyrano@cyranowebdev.com>
2020-07-29 17:47:27 -0300

    adicionando até requisito 11.

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-
 let tasksList = document.querySelector('#tasks ol');
-let itemTxt = '';
-let createItemBtn = document.querySelector('#criar-tarefa');
-createItemBtn.addEventListener('click', createItem);
 
 function itemClickBg(event) {
   let listItems = document.querySelectorAll('#tasks ol li');
@@ -24,10 +20,41 @@ function itemCompleted(event) {
 
 function createItem() {
   let createItemElement = document.createElement('li');
-  itemTxt = document.querySelector('#texto-tarefa').value;
+  let itemTxt = document.querySelector('#texto-tarefa').value;
   createItemElement.innerText = itemTxt;
   tasksList.appendChild(createItemElement);
   createItemElement.addEventListener('click', itemClickBg);
   createItemElement.addEventListener('dblclick', itemCompleted);
   document.querySelector('#texto-tarefa').value = '';
 }
+
+let createItemBtn = document.querySelector('#criar-tarefa');
+createItemBtn.addEventListener('click', createItem);
+
+function eraseAll(event) {
+  let tasks = document.querySelector('#tasks ol').children;
+  if (tasks.length) {
+    alert('Certeza que quer apagar tudo?');
+    for (let task = tasks.length - 1; task >= 0; task -= 1) {
+      tasksList.removeChild(tasks[task]);
+    }
+  }
+}
+
+let eraseAllBtn = document.querySelector('#apaga-tudo');
+eraseAllBtn.addEventListener('click', eraseAll);
+
+function eraseCompleted(event) {
+  let tasks = document.querySelector('#tasks ol').children;
+  if (tasks.length) {
+    alert('Certeza que quer apagar as tarefas completadas?');
+    for (let task = tasks.length - 1; task >= 0; task -= 1) {
+      if(tasks[task].classList.contains('completed')) {
+        tasksList.removeChild(tasks[task]);
+      }
+    }
+  }
+}
+
+let eraseCompletedBtn = document.querySelector('#remover-finalizados');
+eraseCompletedBtn.addEventListener('click', eraseCompleted);
