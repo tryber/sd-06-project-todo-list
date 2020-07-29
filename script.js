79a18a0596b2715c8662bb02f861d274199874aa
Daniel Moura <darthur.moura@gmail.com>
2020-07-29 17:58:31 -0300

    Correção de id do botão criar-tarefa

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+// Configuração de variáveis
 let textInput = document.getElementById("texto-tarefa");
-let addBtn = document.getElementById("adicionar-tarefa");
-addBtn.addEventListener('click', function(event){
-  let novoItem = document.createElement("li");
-  document.getElementById("lista-tarefas").appendChild(novoItem);
-  novoItem.innerText = textInput.value;
-  textInput.value = "";
-})
+let addBtn = document.getElementById("criar-tarefa");
+let listaTarefas = document.getElementById("lista-tarefas");
+let itemSelecionado = document.getElementsByClassName("selecionado");
+
+// Funções e eventos para adicionar novo item à lista de tarefas
+addBtn.addEventListener('click', adicionarTarefa);
+
+function adicionarTarefa() {
+  if (textInput.value === "") {
+    textInput.value = "";
+  } else {
+    let novoItem = document.createElement("li");
+    listaTarefas.appendChild(novoItem);
+    novoItem.innerText = textInput.value;
+    textInput.value = "";
+  }
+}
+
+textInput.addEventListener("keyup", function(event){
+  if (event.keyCode === 13) {
+    adicionarTarefa();
+  }
+});
+
+// Função e evento para selecionar item da lista
+listaTarefas.addEventListener("click", selecionarItem);
