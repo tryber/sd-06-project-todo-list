d4ced304813f77eb24859aad7a525d4623fe5028
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-29 18:43:49 -0300

    Req 13: bonus

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let currentSelectedTask = "";
 let deleteTasksButton = document.querySelector("#apaga-tudo");
 let removeDoneTasksButton = document.querySelector("#remover-finalizados");
 let removeSelectedTaskButton = document.querySelector("#remover-selecionado");
+let moveUpButton = document.querySelector("#mover-cima")
+let moveDownButton = document.querySelector("#mover-baixo")
 
 addTaskButton.addEventListener("click", addTask);
 taskList.addEventListener("click", highlightTask);
@@ -13,6 +15,8 @@ taskList.addEventListener("dblclick", tickTask);
 deleteTasksButton.addEventListener("click", deleteTasks);
 removeDoneTasksButton.addEventListener("click", removeDoneTasks);
 removeSelectedTaskButton.addEventListener("click", removeSelectedTask);
+moveUpButton.addEventListener("click", moveTaskUp);
+moveDownButton.addEventListener("click", moveTaskDown);
 
 
 function addTask(event){
@@ -28,7 +32,7 @@ function highlightTask(event){
         currentSelectedTask.classList.add("selected");
         previousSelectedTask = currentSelectedTask;
     } else if (event.target.tagName.toLowerCase() == "li"){
-        previousSelectedTask = currentSelectedTask;
+        previousSelectedTask = document.querySelector(".selected");
         currentSelectedTask = event.target;
         currentSelectedTask.classList.add("selected");
         previousSelectedTask.classList.remove("selected");
@@ -61,4 +65,55 @@ function removeDoneTasks(){
 function removeSelectedTask(){
     let selectedTask = document.querySelector(".selected");
     taskList.removeChild(selectedTask);
+}
+
+function moveTaskUp(){
+    let selectedTask = document.querySelector(".selected");
+    if (selectedTask == taskList.firstElementChild){
+    }else{
+        let selectedTask = document.querySelector(".selected");
+        let selectedTaskText = selectedTask.innerText;
+        let aboveTask = selectedTask.previousSibling;
+        let aboveTaskText = aboveTask.innerText;
+
+        aboveTask.innerText = selectedTaskText;
+        aboveTask.classList.add("selected");
+
+        selectedTask.innerText = aboveTaskText;
+        selectedTask.classList.remove("selected");    
+
+        if(selectedTask.classList.contains("completed") && aboveTask.classList.contains("completed")){
+        } else if(selectedTask.classList.contains("completed")){
+            aboveTask.classList.add("completed");
+            selectedTask.classList.remove("completed");
+        } else if(aboveTask.classList.contains("completed")){
+            aboveTask.classList.remove("completed");
+            selectedTask.classList.add("completed");
+        }
+    }
+}
+
+function moveTaskDown(){
+    let selectedTask = document.querySelector(".selected");
+    if(selectedTask == taskList.lastElementChild){
+    }else{
+        let selectedTaskText = selectedTask.innerText;
+        let bellowTask = selectedTask.nextElementSibling;
+        let bellowTaskText = bellowTask.innerText;
+
+        bellowTask.innerText = selectedTaskText;
+        selectedTask.innerText = bellowTaskText;
+
+        selectedTask.classList.remove("selected");
+        bellowTask.classList.add("selected");
+
+        if(selectedTask.classList.contains("completed") && bellowTask.classList.contains("completed")){
+        } else if(selectedTask.classList.contains("completed")){
+            bellowTask.classList.add("completed");
+            selectedTask.classList.remove("completed");
+        } else if(bellowTask.classList.contains("completed")){
+            bellowTask.classList.remove("completed");
+            selectedTask.classList.add("completed");
+        }
+    }
 }
