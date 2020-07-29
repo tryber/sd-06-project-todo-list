a7b098a24046a0e37a98669030773dfc3de9e52a
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-29 16:18:44 -0300

    Criada função que seleciona um item da lista conforme solicitado nas etapas 7 e 8 do projeto.

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ criarTarefa.addEventListener('click', function () {
   ondeCriar.appendChild(tarefaCriada);
   document.querySelector('#texto-tarefa').value = '';
 });
+
+const listaTarefas = document.querySelector('#lista-tarefas');
+let itemSelecionado = '';
+
+function marcadorDeTarefas () {
+  let tarefaSelecionada = event.target;
+
+  if (itemSelecionado) {
+    itemSelecionado.classList.remove('selecionado');
+  }
+
+  tarefaSelecionada.classList.add('selecionado');
+  itemSelecionado = event.target;
+}
+
+listaTarefas.addEventListener('click', marcadorDeTarefas);
