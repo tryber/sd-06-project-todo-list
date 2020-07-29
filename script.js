f2374918fe4c073bed39c4fd07e850915f1d6d33
kissyla <kissylamottapereira@gmail.com>
2020-07-29 19:59:08 -0300

    botão de input, com id certo agora

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 window.onload = function(){
+//5 - Deve haver um botão com id="criar-tarefa" e ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo.
+//Será verificada a existência de um elemento do tipo button com o id criar-tarefa
+//No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
+//A adição de elementos na lista será feita algumas vezes, e será checado se todos os itens criados permanecem na lista na medida em que novos são adicionados.
 
+function createToDo (){
+    
+}
     
 }
