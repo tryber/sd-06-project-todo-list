ac32ee2f40e03a1796a610e7dedacba96d92f78d
Anderson Alves <contatodeanderson@gmail.com>
2020-07-29 14:50:03 -0300

    add del list

--- script.js
+++ script.js
@@ -8,17 +8,23 @@ let tarefas = {
   3: "scooter"
 };
 
+let list = document.getElementById("lista-tarefas");
+
 document.querySelector('#criar-tarefa').addEventListener('click', addTask);
+document.querySelector('#apaga-tudo').addEventListener('click', delList);
+
 
 function loadList() {
   let tamanhoObjeto = Object.keys(tarefas).length;
   let orderedList = document.getElementById("lista-tarefas");
 
+  delList();
   for (let index = 1; index <= tamanhoObjeto; index += 1) {
     let listItem = document.createElement('li');
     listItem.appendChild(document.createTextNode(tarefas[index]));
     orderedList.appendChild(listItem);
     listItem.addEventListener("click", changeBgColor);
+    listItem.addEventListener("dblclick", taskCompleted);
   }
 }
 
@@ -37,4 +43,14 @@ function changeBgColor() {
 
 function teste() {
   console.log('clicou');
+}
+
+function taskCompleted() {
+  this.setAttribute("style", 'text-decoration: line-through');
+}
+
+function delList() {
+  while (list.firstChild){
+    list.removeChild(list.firstChild);
+  }
 }
