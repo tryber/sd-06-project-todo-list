6e38303c1c938364f023ab4a61ca8b42533239c8
FlavioSugano <flavio.sugano@gmail.com>
2020-07-29 18:23:22 -0300

    alteração na função criarTarefa, para ela criar o elemento li já c/ eventListener

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 window.onload = function () {
   const botaoCriarTarefa = document.getElementById('criar-tarefa');
+  let listaDeTarefas = document.getElementsByTagName('li');
 
   botaoCriarTarefa.addEventListener('click', criarTarefa);
+
+  // adiciona fundo cinza ao clicar na tarefa
+
+  
+
 
 }
 
 function criarTarefa () {
   let textoTarefa = document.getElementById('texto-tarefa').value;
   let tarefa = document.createElement('li');
-  let textNode = document.createTextNode(textoTarefa);
-
-  tarefa.appendChild(textNode);
+  let textoNode = document.createTextNode(textoTarefa);
+  
+  tarefa.appendChild(textoNode);
   document.getElementById('lista-tarefas').appendChild(tarefa);
   document.getElementById('texto-tarefa').value = null;
+  listaDeTarefas = document.getElementsByTagName('li');
+  
+  tarefa.addEventListener('click', function () { 
+    console.log(listaDeTarefas.className);
+    tarefa.classList.add('selecionada');
+  })
 }
