8626bbc506b6c0113fa1fae1e2780dfe2d5ae3a6
Marthus Sera <marthus2001@gmail.com>
2020-07-29 15:54:50 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -11,9 +11,22 @@ function addNewItem() {
   newItem.appendChild(document.createTextNode(input.value));
   list.appendChild(newItem);
   input.value = "";
+  listListItems();
+}
+
+function listListItems() {
+  let items = document.getElementsByTagName('li');
+  for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', function() {changeBackground(items[i])});
+  }
+}
+
+function changeBackground(item) {
+  let oldBg = document.getElementsByClassName('cinza')[0];
+  oldBg.classList.remove('cinza');
+  oldBg.style.backgroundColor = "";
+  item.classList.add('cinza');
+  item.style.backgroundColor = "rgb(128, 128, 128)";
 }
 
 button.addEventListener('click', function() {addNewItem()});
-
-
-
