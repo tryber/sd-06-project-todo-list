b1f69892f781f1e9fbf37e5c2f285a1af730235e
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2020-07-29 20:20:34 -0300

    CodeClimate fixes

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 // Add task
 
+const addTaskInput = document.getElementById('texto-tarefa');
+const addTaskButton = document.getElementById('criar-tarefa');
+
 function taskCreation() {
-  if (addTaskInput.value == "") {
-    return alert("Name a task before adding it!");
+  if (addTaskInput.value === '') {
+    return alert('Name a task before adding it!');
   }
   const li = addTask();
   addText(li);
+  return
 }
 
 function addTask() {
-  const ol = document.getElementById("lista-tarefas");
-  const li = document.createElement("li");
+  const ol = document.getElementById('lista-tarefas');
+  const li = document.createElement('li');
   ol.appendChild(li);
   return li;
 }
 
 function addText(li) {
   li.innerText = addTaskInput.value;
-  addTaskInput.value = "";
+  addTaskInput.value = '';
 }
 
-const addTaskInput = document.getElementById("texto-tarefa");
-const addTaskButton = document.getElementById("criar-tarefa");
 addTaskButton.onclick = taskCreation;
