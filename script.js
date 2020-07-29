0716f69294420ca8e29eba9a0526929aa173f613
Fabio Camargos <fabiocmmcm@gmail.com>
2020-07-29 16:08:46 -0300

    parte funcoes dos botoes Js. e Html pronto?

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+    let btnCreate = document.querySelector("#criar-tarefa")
+    let btnClearCompleted = document.querySelector("#clear-completed")
+    let btnClearList = document.querySelector("#clear-list")
+    let btnSave = document.querySelector("#save")
+    let btnRemove = document.querySelector("#remove")
+    let inputTextoTarefa = document.querySelector("#texto-tarefa")
+    let list = document.querySelector("#lista-tarefas")
+    let btnRemSelec = document.querySelector("#remover-selecionado")
+}
