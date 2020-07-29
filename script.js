be20e6cb8c7ee485fe0e2f5f3d6cdb8627633485
Anderson Alves <contatodeanderson@gmail.com>
2020-07-29 13:50:16 -0300

    add selected bg color

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let tarefas = {
   3: "scooter"
 };
 
+document.querySelector('#criar-tarefa').addEventListener('click', addTask);
+
 function loadList() {
   let tamanhoObjeto = Object.keys(tarefas).length;
   let orderedList = document.getElementById("lista-tarefas");
@@ -16,8 +18,21 @@ function loadList() {
     let listItem = document.createElement('li');
     listItem.appendChild(document.createTextNode(tarefas[index]));
     orderedList.appendChild(listItem);
-    listItem.addEventListener("click", teste);
+    listItem.addEventListener("click", changeBgColor);
   }
+}
+
+function addTask() {
+  let newItem = Object.keys(tarefas).length + 1;
+  tarefas[newItem] = document.querySelector('#texto-tarefa').value;
+  loadList();
+}
+
+function changeBgColor() {
+  document.querySelectorAll('li').forEach(function(li) {
+    li.style.backgroundColor = '#fff';
+  })
+ this.setAttribute("style", 'background-color: rgb(128, 128, 128)');
 }
 
 function teste() {
