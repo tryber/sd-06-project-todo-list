dcc2d682db172535bd5c0eeeedb7fbd00441175a
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-07-29 16:33:01 -0300

    duvida

--- script.js
+++ script.js
@@ -9,4 +9,20 @@ window.onload = function() {
         listaTarefas.appendChild(tarefa);
         texto.value = "";   
     });
+
+    let click1 = 0;
+    listaTarefas.addEventListener("click", function(event) {
+        if (click1 === 0){
+            // primeiro click declaro a variavel
+            let ultimoSelecionado = event.target;
+            event.target.style.backgroundColor = "rgb(128,128,128)";
+            click1 += 1;
+        } else {
+            // no segundo click a variavel parece que não guardou valor nenhum e o codigo da erro porque
+            ultimoSelecionado.style.backgroundColor = "";
+            event.target.style.backgroundColor = "rgb(128,128,128)";
+            ultimoSelecionado = event.target;
+        }        
+    });
+
 }
