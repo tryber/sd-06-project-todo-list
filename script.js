f7e0434cb5b29f70da04f3b124b030cc240b6188
Kesley Lahoz <namielahoz@gmail.com>
2020-07-29 15:12:27 -0300

    teste

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 window.onload = function () {
+    //button to input text 
     let textInput = document.querySelector("#texto-tarefa");
     let addBtn = document.querySelector("#criar-tarefa");
-    let takeText = document.querySelector("#listing");
+    let takeText = document.querySelector("#lista-tarefas");
 
     addBtn.addEventListener('click', function () {
-        takeText.innerHTML = textInput.value
+        for (let index = 0; index < textInput.length; index += 1) {
+            let liElement = document.createElement("li");
+            takeText[i] = elementoLi + textInput.value;
+        }
     });
 
 
