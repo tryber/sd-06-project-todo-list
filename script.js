22f2fe923f6a2a8d30d0051b298b3b9a1a1dd6d9
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-29 01:06:10 -0300

    Requisito 7 - teste

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('criar-tarefa');
-
 button.addEventListener('click', createTask)
 
+//Criar novos itens e zerar o input
 function createTask(){
   const listOl = document.getElementById('lista-tarefas');
   const input = document.getElementById('texto-tarefa');
@@ -9,17 +9,43 @@ function createTask(){
   newItem.innerHTML = input.value
   listOl.appendChild(newItem)
   input.value = ""
+  listOl.addEventListener('click', function turnGrey() {
+    newItem.style.backgroundColor = 'grey';
+  })
 }
 
-  // const createdItem = document.getElementByTagName('li')
-  // createdItem.addEventListener('click', changeColorItem())
-  // createdItem.addEventListener('dblclick', scratchItem())
+// const buttonCreate = document.getElementById('criar-tarefa');
+// buttonCreate.addEventListener('click', createTask)
+
+// let listOl = document.getElementById('lista-tarefas');
+// let input = document.getElementById('texto-tarefa');
 
 
-// function changeColorItem() {
-//   console.log('click simples')
+// //Cria novos itens e zera o input
+// function createTask(){
+//   const listOl = document.getElementById('lista-tarefas');
+//   const input = document.getElementById('texto-tarefa');
+//   const newItem = document.createElement('li');
+//   newItem.innerHTML = input.value
+//   listOl.appendChild(newItem)
+//   input.value = ""
 // }
 
-// function scratchItem() {
-//   console.log('click duplo')
+// //Torna cinza itens com um click
+// listOl.addEventListener('click', turnGrey)
+// function turnGrey() {
+//   listOl.style.backgroundColor = 'grey';
+// }
+
+// //Torna riscado itens com double click
+// listOl.addEventListener('dblclick', scratchItem)
+// function scratchItem(){
+//   console.log('risca ça merda');
+// }
+
+// // Botão apaga tudo
+//   const buttonErase = document.getElementById('apaga-tudo');
+//   buttonErase.addEventListener('click', eraseAll)
+//   function eraseAll() {  
+//   console.log('apaga ça merda');
 // }
