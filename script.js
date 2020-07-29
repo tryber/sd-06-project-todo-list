6052a426178042703f3f35864aa46419291a7742
Alessandra <itsalematos@hotmail.com>
2020-07-29 18:51:03 -0400

    7º Add propriedade para mudar a cor do item

--- script.js
+++ script.js
@@ -6,8 +6,40 @@ button.addEventListener('click', insertTask);
 function insertTask() {
     let task =  document.querySelector('#texto-tarefa').value;
     let btn = document.createElement('li');
+    btn.className = 'item';
     btn.innerHTML = task;
     list.appendChild(btn);
 
     let cleanInput = document.getElementById("texto-tarefa").value = "";
 }
+
+function changeSelected(select){
+
+}
+
+list.addEventListener('click', function(event) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    console.log(event.target);
+
+    if (event.target.className === 'item'){
+      event.target.classList.add('selected')
+    } else if(event.target.item.className === 'item selected'){
+
+    }
+    
+
+});
+
+
+
+// function handlePalletItemEvent(event){
+//     let oldSelectedDiv = document.querySelector('.selected');
+//     let currentSelectedDiv = event.target;
+//     let backgroundColor = currentSelectedDiv.style.backgroundColor;
+//     console.log(backgroundColor);
+
+//     oldSelectedDiv.classList.remove('selected');
+//     currentSelectedDiv.classList.add('selected');
+    
+//     selectedColor = backgroundColor;     
+// }
