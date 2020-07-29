5a2cdff62a3a397d44919e2e33b88a9163d155ec
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-28 23:29:49 -0300

    Requisito 5 - ajustes

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', createTask)
 
 function createTask(){
-const listOl = document.getElementById('lista-tarefas');
-const input = document.getElementById('texto-tarefa');
-const newItem = document.createElement('li');
-newItem.innerHTML = input.value
-listOl.appendChild(newItem)
-input.value = ""
-newItem.addEventListener('click', changeColorItem())
-newItem.addEventListener('dblclick', scratchItem())
+  const listOl = document.getElementById('lista-tarefas');
+  const input = document.getElementById('texto-tarefa');
+  const newItem = document.createElement('li');
+  newItem.innerHTML = input.value
+  listOl.appendChild(newItem)
+  input.value = ""
 }
+
+  // const createdItem = document.getElementByTagName('li')
+  // createdItem.addEventListener('click', changeColorItem())
+  // createdItem.addEventListener('dblclick', scratchItem())
+
+
+// function changeColorItem() {
+//   console.log('click simples')
+// }
+
+// function scratchItem() {
+//   console.log('click duplo')
+// }
