bd471e75d453624327e32f85e72b7bd08ddf5793
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-29 14:25:40 -0300

    Req 5 e 6

---
+++ script.js
@@ -0,0 +1,11 @@
+let addTaskButton = document.querySelector("#criar-tarefa");
+let taskInput = document.querySelector("#texto-tarefa")
+
+addTaskButton.addEventListener("click", addTask);
+
+function addTask(event){
+    let taskList = document.querySelector("#lista-tarefas");
+    let newLi = document.createElement("li");
+    newLi.innerText = taskInput.value;
+    taskList.appendChild(newLi);
+}
