b9e846ab2fe052f7eabdda92655d6231f6184f1e
Isadora Koga <isadorakoga710@gmail.com>
2020-07-29 19:22:18 -0300

    Remove selected

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const inputButton = document.getElementById('criar-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
-inputButton.addEventListener('click', addTarefa);
 
 // Criar tarefa na lista
 function addTarefa() {
-  let inputText = document.getElementById('texto-tarefa').value;
+  const inputText = document.getElementById('texto-tarefa').value;
   const createListItem = document.createElement('li');
   createListItem.innerHTML = inputText;
   createListItem.className = 'list-item';
@@ -12,25 +11,27 @@ function addTarefa() {
   document.getElementById('texto-tarefa').value = '';
 }
 
+inputButton.addEventListener('click', addTarefa);
+
 // Add backgroundColor
-listaTarefas.addEventListener('click', function(event) {
-  let removeClass = document.querySelector('.selected');
+listaTarefas.addEventListener('click', function (event) {
+  const removeClass = document.querySelector('.selected');
   if (removeClass !== null) {
     removeClass.classList.remove('selected');
   }
-  let selectClass = event.target;
+  const selectClass = event.target;
   selectClass.className += ' selected';
 });
 
 // Add linha riscada
-listaTarefas.addEventListener('dblclick', function(event) {
-  let completedClass = event.target;
+listaTarefas.addEventListener('dblclick', function (event) {
+  const completedClass = event.target;
   completedClass.classList.toggle('completed');
 });
 
 // Remover tudo
 const clearAllButton = document.getElementById('apaga-tudo');
-clearAllButton.addEventListener('click', function() {
+clearAllButton.addEventListener('click', function () {
   while (listaTarefas.firstChild) {
     listaTarefas.removeChild(listaTarefas.firstChild);
   }
@@ -38,8 +39,8 @@ clearAllButton.addEventListener('click', function() {
 
 // Remover itens finalizados
 const removeDoneButton = document.getElementById('remover-finalizados');
-removeDoneButton.addEventListener('click', function() {
-  let listaLength = listaTarefas.childNodes.length-1;
+removeDoneButton.addEventListener('click', function () {
+  let listaLength = listaTarefas.childNodes.length - 1;
   for (listaLength; listaLength >= 0; listaLength -= 1) {
     if (listaTarefas.childNodes[listaLength].classList.contains('completed')) {
       listaTarefas.removeChild(listaTarefas.childNodes[listaLength]);
@@ -47,5 +48,9 @@ removeDoneButton.addEventListener('click', function() {
   }
 });
 
-
-
+// Remover itens selecionados
+const removeSelectedButton = document.getElementById('remover-selecionado');
+removeSelectedButton.addEventListener('click', function () {
+  const selectedClass = document.querySelector('.selected');
+  listaTarefas.removeChild(selectedClass);
+});
