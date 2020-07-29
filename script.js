0f3ae76649c1c0ee3f4d490aa7a7c16a72fba1be
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-29 14:38:37 -0300

    Req 7

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let addTaskButton = document.querySelector("#criar-tarefa");
-let taskInput = document.querySelector("#texto-tarefa")
+let taskInput = document.querySelector("#texto-tarefa");
+let taskList = document.querySelector("#lista-tarefas");
+
 
 addTaskButton.addEventListener("click", addTask);
+taskList.addEventListener("click", highlightTask);
 
 function addTask(event){
-    let taskList = document.querySelector("#lista-tarefas");
     let newLi = document.createElement("li");
     newLi.innerText = taskInput.value;
     taskList.appendChild(newLi);
     taskInput.value = "";
+}
+
+function highlightTask(event){
+    event.target.style.backgroundColor = "rgb(128, 128, 128)";
 }
