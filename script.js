330e7b72f5b8f7a57a6a07276b8db72be7756588
isabellavjs <isabellavjs@gmail.com>
2020-07-29 15:25:37 -0300

    Requirements 5

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ let cointainerList = document.querySelector('.list-container');
 orderedList.id = 'lista-tarefas';
 cointainerList.appendChild(orderedList);
 
-//Create the list itens, and add them in the orderedList
-const listItens = document.createElement('li');
+//Create the list itens, and add them in the orderedList when the button is clicked.
 let typedtext = document.getElementById('texto-tarefa');
-const addButton = document.getElementById('criar-tarefa');
+function addItens() {
+    if (typedtext.value !== '') {
+        const listItens = document.createElement('li');
+        listItens.innerHTML = typedtext.value;
+        orderedList.appendChild(listItens);
+        typedtext.value = '';
+    }
+}
 
-addButton.addEventListener('click', function() {
-    listItens.innerHTML = typedtext.value;
-    orderedList.appendChild(listItens);
-})
+const addButton = document.getElementById('criar-tarefa');
+addButton.addEventListener('click', addItens)
 
