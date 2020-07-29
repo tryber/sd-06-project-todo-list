206f94ab713213d8db34d420ddbdef2adb591cb5
Isadora Koga <isadorakoga710@gmail.com>
2020-07-29 16:55:59 -0300

    Add botoes

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const inputButton = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
 inputButton.addEventListener('click', addTarefa);
 
 function addTarefa() {
   let inputText = document.getElementById('texto-tarefa').value;
-  let listaTarefas = document.getElementById('lista-tarefas');
-  let createListItem = document.createElement('li');
+  const createListItem = document.createElement('li');
   createListItem.innerHTML = inputText;
+  createListItem.className = 'list-item';
   listaTarefas.appendChild(createListItem);
   document.getElementById('texto-tarefa').value = '';
 }
+
+const listItems = document.querySelectorAll('.list-item');
+listItems.addEventListener('click', addNewBackgroundColor);
+
+function addNewBackgroundColor() {
+  for (item in listItems) {
+    item.className = 'selected';
+  }
+}
+
+
