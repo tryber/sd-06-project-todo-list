ffaa2fc0bb82af2f82d674410cc282a5b37f7e13
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-29 02:21:36 -0300

    Requisito 10 - teste

--- script.js
+++ script.js
@@ -1,51 +1,38 @@
 const button = document.getElementById('criar-tarefa');
+const input = document.getElementById('texto-tarefa');
+const listOl = document.getElementById('lista-tarefas');
+const buttonErase = document.getElementById('apaga-tudo');
+
 button.addEventListener('click', createTask)
 
 //Criar novos itens e zerar o input
 function createTask(){
-  const listOl = document.getElementById('lista-tarefas');
-  const input = document.getElementById('texto-tarefa');
-  const newItem = document.createElement('li');
-  newItem.innerHTML = input.value
-  listOl.appendChild(newItem)
-  input.value = ""
-  listOl.addEventListener('click', function turnGrey() {
-    newItem.style.backgroundColor = 'grey';
-  })
+  let newItem = document.createElement('li');
+  listOl.appendChild(newItem);
+  newItem.innerHTML = input.value;
+  newItem.className = "tarefa";
+  newItem.addEventListener('click', turnGrey)
+  newItem.addEventListener('dblclick', scratchItem)
+  input.value = "";
 }
 
-// const buttonCreate = document.getElementById('criar-tarefa');
-// buttonCreate.addEventListener('click', createTask)
+//Torna cinza itens com um click
+function turnGrey() {
+  let selectedItem = event.target;
+  selectedItem.style.backgroundColor = 'grey'
+}
 
-// let listOl = document.getElementById('lista-tarefas');
-// let input = document.getElementById('texto-tarefa');
-
-
-// //Cria novos itens e zera o input
-// function createTask(){
-//   const listOl = document.getElementById('lista-tarefas');
-//   const input = document.getElementById('texto-tarefa');
-//   const newItem = document.createElement('li');
-//   newItem.innerHTML = input.value
-//   listOl.appendChild(newItem)
-//   input.value = ""
-// }
-
-// //Torna cinza itens com um click
-// listOl.addEventListener('click', turnGrey)
-// function turnGrey() {
-//   listOl.style.backgroundColor = 'grey';
-// }
-
-// //Torna riscado itens com double click
-// listOl.addEventListener('dblclick', scratchItem)
-// function scratchItem(){
-//   console.log('risca ça merda');
-// }
+//Torna riscado itens com double click
+function scratchItem(){
+  let selectedItem = event.target;
+  selectedItem.style.textDecoration = 'line-through'
+}
 
 // // Botão apaga tudo
-//   const buttonErase = document.getElementById('apaga-tudo');
-//   buttonErase.addEventListener('click', eraseAll)
-//   function eraseAll() {  
-//   console.log('apaga ça merda');
-// }
+buttonErase.addEventListener('click', eraseAll)
+function eraseAll() {
+const task = document.getElementsByClassName('tarefa')
+while (task.length > 0) task[0].remove();
+}
+
+
