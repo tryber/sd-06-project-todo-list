2fb06f2c364e1d3bd4ecada11e05d9ee889d592b
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-29 15:28:11 -0300

    Added selection of item with grey background

--- script.js
+++ script.js
@@ -6,9 +6,28 @@ window.onload = function () {
   function newTask() {
     const newListItem = document.createElement('li');
     newListItem.innerHTML = userInput.value;
+    newListItem.className = 'list-item';
     tasksList.appendChild(newListItem);
     userInput.value = '';
   }
-    
+
+  function selectedTask(event) {
+    const oldSelectedItem = document.querySelector('.selected');
+    if (oldSelectedItem == null) {
+      event.target.classList.add('selected');
+    } else {
+      oldSelectedItem.classList.remove('selected');
+      event.target.classList.add('selected');
+    }
+  }
+
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('list-item')) {
+      selectedTask(event);
+    }
+  })
+
   newTaskButton.addEventListener('click', newTask);
+
+
 };
