602b5d78e18282ed1db594ff7af078710dca6a60
Marthus Sera <marthus2001@gmail.com>
2020-07-29 17:32:33 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const header = document.getElementsByClassName('header')[0];
 const button = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
+const removeAllButton = document.getElementById('apaga-tudo');
+let items;
+
+button.addEventListener('click', function() {addNewItem()});
+removeAllButton.addEventListener('click', function() {removeAll()});
 
 function addNewItem() {
   let input = document.getElementById('texto-tarefa');
@@ -10,44 +14,41 @@ function addNewItem() {
   let newItem = document.createElement('li');
   newItem.appendChild(document.createTextNode(input.value));
   list.appendChild(newItem);
+  newItem.addEventListener('click', function() {changeBackground(newItem)});
+  newItem.addEventListener('dblclick', function() {completed(newItem)});
   input.value = "";
-  listListItems();
-}
-
-function listListItems() {
-  let items = document.getElementsByTagName('li');
-  for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener('click', function() {changeBackground(items[i])});
-    items[i].addEventListener('dblclick', function() {completed(items[i])});
-  }
 }
 
 function changeBackground(item) {
   let oldBg = document.getElementsByClassName('cinza')[0];
   oldBg.classList.remove('cinza');
-  oldBg.style.backgroundColor = "";
   item.classList.add('cinza');
-  item.style.backgroundColor = "rgb(128, 128, 128)";
 }
 
 function completed(item) {
   let temp = document.getElementsByClassName('completed');
   if (findItemInElements(item, temp)) {
     item.classList.remove('completed');
+    return 0;
   } else {
     item.classList.add('completed');
+    return 1;
   }
 }
 
 function findItemInElements(item, elements) {
   for (let i = 0; i < elements.length; i++) {
     if (elements[i] == item) {
-      return 1
+      return 1;
     }
   }
 
-  return 0
+  return 0;
 
 }
 
-button.addEventListener('click', function() {addNewItem()});
+function removeAll() {
+  while(list.firstChild){
+    list.removeChild(list.firstChild);
+  }
+}
