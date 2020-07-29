dc6ef288730446cbc179895d55470125f288a24f
Micaeli Teixeira <luime6@gmail.com>
2020-07-29 17:24:06 -0300

    resolvendo o requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  
+
  /* fazendo a parte da adição de texto pelo botão */
   const botao = document.getElementById('criar-tarefa');
   
@@ -9,13 +9,30 @@ window.onload = function () {
     const listaOl = document.createElement('li');
     listaOl.innerHTML = texto;
     lista.appendChild(listaOl);
+  
+    /*o que for realicionado ao li tem que ser chamado aqui*/
+    listaOl.style.backgroundColor = 'white';
+    listaOl.addEventListener('click',fundoDeCor);
+
   });
  
   /* fazendo a parte do reset da imput*/
   function resetTexto () {
-   document.querySelector('#texto-tarefa').value = '';
- }
+    document.querySelector('#texto-tarefa').value = '';
+  }
  const text = document.querySelector('#criar-tarefa');
  text.addEventListener('click', resetTexto);
+
+ /*alterando a cor de fundo de um lista da lista*/
+  //const bgCor = document.querySelector('li');
+  //bgCor.addEventListener('click',fundoDeCor);
+
+  function fundoDeCor(){
+   let item = event.target;
+   item.style.backgroundColor = "rgb(128, 128, 128)";
+  }
+ 
+
+  
 
 }
