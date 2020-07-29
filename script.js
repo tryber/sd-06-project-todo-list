f6b3e3250d2799935c307835e0d07d806732c2cc
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-29 17:25:36 -0300

    8. Mudando cor de somente 1 elemento

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function addTarefa() {
   const lista = document.querySelector('#lista-tarefas');
   const textItem = document.querySelector('#texto-tarefa');
   const item = document.createElement('li');
+  item.className = 'item';
   item.innerHTML = textItem.value;
   textItem.value = '';
   lista.appendChild(item);
@@ -17,10 +18,14 @@ const btnAdd = document.querySelector('#criar-tarefa');
 btnAdd.addEventListener('click', addTarefa);
 
 function changingCollor(event) {
+  const lista = document.querySelectorAll('.item');
   const item = event.target;
-  const oldStyle = document.querySelector('.selected');
-  item.classList.add('selected');
-  oldStyle.classList.remove('selected');
+  item.style.background = 'rgb(128 , 128 , 128)';
+  for (let index = 0; index < lista.length; index += 1){
+    if (lista[index].style.background === 'rgb(128 , 128 , 128)') {
+      lista[index].style.remove(background);
+    }
+  }
 }
 const lista = document.querySelector('#lista-tarefas');
 lista.addEventListener('click', changingCollor);
