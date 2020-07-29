bb55240ca665db6c3d67119d1b3e4fcf7a778253
DehSartoreto <fixsartoreto@gmail.com>
2020-07-29 15:57:25 -0300

    add move up move down buttons

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ window.onload = function() {
     let btnTask = document.querySelector("#criar-tarefa");
     let orderList = document.querySelector("#lista-tarefas");
 
+    if (JSON.parse(localStorage.savedTasks !== undefined)){
+        let retrieveTask = JSON.parse(localStorage.getItem("tasks"))    ;
+        for (let i = 0; i < retrieveTask.length; i += 1){
+            let newLine = createElement("li");
+            newLine.className = retrieveTask[i].className;
+            newLine.innerHTML = retrieveTask[i].content;
+            newLine.classList.remove("selected");
+            if (newLine.classList.contains("completed")) {
+                newLine.style.textDecoration = "line-through solid rgb(0, 0, 0)";
+            }
+            orderList.appendChild(newLine);
+        }
+    }
+
     btnTask.addEventListener("click", function() {
         let newLI = document.createElement("li");
         newLI.setAttribute("class", "listTask")
@@ -19,7 +33,9 @@ window.onload = function() {
     orderList.addEventListener("click", function(event) {
         let clickTask = event.target;
             ultimo.style.backgroundColor = "white";
+            ultimo.classList.remove("selected");
             clickTask.style.backgroundColor = "rgb(128, 128, 128)";
+            clickTask.classList.add("selected");
             ultimo = clickTask;
     })
 
@@ -52,8 +68,42 @@ window.onload = function() {
         }
     })
 
+    let saveTasks = document.querySelector("#salvar-tarefas");
 
+    saveTasks.addEventListener("click", function() {
+        let pegaLista = document.querySelectorAll("li");
+        let savedTasks = [];
+        let obj = new Object();
+        for (i = 0; i < pegaLista.length; i += 1){
+            obj.className = pegaLista[i].className;
+            obj.content = pegaLista[i].innerHTML;
+            savedTasks.push(obj);
+        }
+        localStorage.setItem("tasks", JSON.stringify(savedTasks));
 
+    })
 
+    let moveUp = document.querySelector("#mover-cima");
+
+    moveUp.addEventListener("click", function() {
+        let listTask2 = document.querySelectorAll("li");
+        for (let i = 0; i < listTask2.length; i += 1){
+            if (listTask2[i].classList.contains("selected") && i !== 0){
+                listTask2[i].parentElement.insertBefore(listTask2[i], listTask2[i - 1]);
+            }
+        }
+        
+    })
+
+    let moveDown = document.querySelector("#mover-baixo");
+    moveDown.addEventListener("click", function() {
+        let listTask2 = document.querySelectorAll("li");
+        for (let i = 0; i < listTask2.length - 1; i += 1){
+            if (listTask2[i].classList.contains("selected")){
+                listTask2[i].parentElement.insertBefore(listTask2[i + 1], listTask2[i]);
+            }
+        }
+        
+    })
 
 }
