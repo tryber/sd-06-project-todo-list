c73ab5605b0a9572fed514eb3ae3290fef3fd27c
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-07-29 17:42:09 -0300

    tirando o botão da tabela

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-window.onload = function() {
+window.onload = function () {
     let texto = document.querySelector("#texto-tarefa");
     let bAdicionar = document.querySelector("#criar-tarefa");
     let listaTarefas = document.querySelector("#lista-tarefas");
 
-    bAdicionar.addEventListener("click", function(event) {
+    bAdicionar.addEventListener("click", function (event) {
         let tarefa = document.createElement("li");
         tarefa.innerText = texto.value;
         listaTarefas.appendChild(tarefa);
-        texto.value = "";   
+        texto.value = "";
     });
 
     let click1 = 0;
-    listaTarefas.addEventListener("click", function(event) {
-        if (click1 === 0){
+    listaTarefas.addEventListener("click", function (event) {
+        if (click1 === 0) {
             event.target.className = "selecionado";
             click1 = 1;
         } else {
             document.querySelector(".selecionado").className = "";
             event.target.className = "selecionado";
-        }        
+        }
+
     });
 
 }
