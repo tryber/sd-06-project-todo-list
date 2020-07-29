3c286efadc678128fd56f7ba00d8efb914185f5d
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-29 17:44:04 -0300

    7 8 9

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 window.onload = function() {
-    btn.addEventListener('click', adicionaTarefa)
+    botao();
 }
 const btn = document.getElementById('criar-tarefa');
+const btn2 = document.getElementById('apaga-tudo');
 const insert = document.getElementById('texto-tarefa');
+const ordList = document.getElementById('lista-tarefas');
 function adicionaTarefa() {
     let itemList = document.createElement('li');
-    document.getElementById('lista-tarefas').appendChild(itemList);
+    ordList.appendChild(itemList);
     itemList.innerText = insert.value;
     insert.value = '';
-    itemList.addEventListener('click', bgColorItem)
-    function bgColorItem() {
-        itemList.style.background = 'rgb(128, 128, 128)';
-    };
-    
+    itemList.addEventListener('click', clickItem);
+    itemList.addEventListener('dblclick', doubleClick);
 }
+function botao() {
+    btn.addEventListener('click', adicionaTarefa);
+}
+function clickItem(event) {
+    let itemSelected = event.target;
+    itemSelected.style.background = 'rgb(128, 128, 128)';
+};
+function doubleClick(event) {
+    let itemSelected = event.target;
+    itemSelected.classList.add('completed');
+    itemSelected.style.textDecoration = 'line-through';
+}
+function apagaTudo() {
+    ordList.innerHTML = '';
+}
+btn2.addEventListener('click', apagaTudo);
+
