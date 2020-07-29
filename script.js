d98fa5722aa41351e039eec0243bae481f02f0d9
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-29 16:04:05 -0300

    CC

--- script.js
+++ script.js
@@ -4,31 +4,13 @@ window.onload = function () {
   const tasksList = document.querySelector('#lista-tarefas');
 
   function newTask() {
-    const newListItem = document.createElement('li');
+    let newListItem = document.createElement('li');
     newListItem.innerHTML = userInput.value;
     newListItem.className = 'list-item';
     tasksList.appendChild(newListItem);
     userInput.value = '';
   }
-
-  function selectedTask(event) {
-    const oldSelectedItem = document.querySelector('.selected');
-    if (oldSelectedItem == null) {
-      event.target.classList.add('selected');
-    } else {
-      oldSelectedItem.classList.remove('selected');
-      event.target.classList.add('selected');
-    }
-  }
-
-  function completedTask(event) {
-    if (event.target.classList.contains('completed')) {
-      event.target.classList.remove('completed');
-    } else {
-      event.target.classList.add('completed');
-    }
-  }
-
+  
   document.addEventListener('click', function (event) {
     if (event.target.classList.contains('list-item')) {
       selectedTask(event);
@@ -43,3 +25,21 @@ window.onload = function () {
 
   newTaskButton.addEventListener('click', newTask);
 };
+
+function selectedTask(event) {
+  const oldSelectedItem = document.querySelector('.selected');
+  if (oldSelectedItem == null) {
+    event.target.classList.add('selected');
+  } else {
+    oldSelectedItem.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+
+function completedTask(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
