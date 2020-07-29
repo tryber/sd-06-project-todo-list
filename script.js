bf06ff06b80c1c02a40d21c774e967d333abff07
Arthur de Sa <arthurdesa94@gmail.com>
2020-07-29 19:56:02 -0300

    adicionadno os selected

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-window.onload = function(){
-
 
     let createButton = document.querySelector("#criar-tarefa");
     let taskList = document.querySelector("#lista-tarefas");
     let textInput = document.querySelector("#texto-tarefa");
+    let removeTasksDone = document.querySelector("#remover-finalizados")
     let clearList = document.querySelector("#apagar-tudo");
 
-        fuction listingTheTasks('click', function(){
-            let listItem = document.createElement('li');
-            listItem.innerHTML = textInput.value;
-            taskList.appendChild(listItem);
-            textInput.value = "";
-            listItem.addEventListener('click', function(){
-                console.log('testando!!!');
-            })
+    
+    function createTask() {
+        let listItem = document.createElement('li');
+        let task = document.getElementById('texto-tarefa').value;
+        listItem.innerHTML = task;
+        taskList.appendChild(listItem);
+        document.getElementById('texto-tarefa').value = '';
+    }
 
-        })
+    createButton.addEventListener('click', createTask);
+
+    function selectedTask(event) {
+        let listSelectedTask = event.target;
+        if (document.querySelector('.selectedTask') !== null) {
+          document.querySelector('.selectedTask').classList.remove('selectedTask');
+        }
+        listSelectedTask.classList.add('selectedTask');
+    }
+    taskList.addEventListener('click', selectedTask);
+
+
+
+        // function listingTheTasks('click', function(){
+        //     let listItem = document.createElement('li');
+        //     listItem.innerHTML = textInput.value;
+        //     taskList.appendChild(listItem);
+        //     textInput.value = "";
+        //     listItem.addEventListener('click', function(){
+        //         console.log('testando!!!');
+        //     })
+
+        // })
         // listItem.addEventListener('dbclick', function(event){
         //     let doubleClick = event.target;
         //     console.log('testando novamente !!!');
 
         // })
 
-        }
+        
 
-
-        listItem = listingTheTasks();
-        console.log(listItem);
+        // listItem = listingTheTasks();
+        // console.log(listItem);
 
 
 
