c4e3a43af485b6c537ab7fd6d46bef76bc23f408
Daniel Jesus <dev.danieljs@gmail.com>
2020-07-29 20:45:49 -0300

    Fixing function add color background in line when clicked

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ window.onload = function () {
     let inputText = document.querySelector('#texto-tarefa').value;
     let list = document.querySelector('#lista-tarefas');
     let itenLi = document.createElement('li');
-    itenLi.style.backgroundColor = 'white';
+    // itenLi.style.backgroundColor = 'white';
+    itenLi.classList.add('lines');
     itenLi.innerHTML = inputText;
     list.appendChild(itenLi);
 
-    // Add color background in li line when clicked
-    itenLi.addEventListener('click', function() {
-      itenLi = event.target;
-      itenLi.style.backgroundColor = 'rgb(128, 128, 128)';
-    })
+    itenLi.addEventListener('click', colorLineClicked);
 
     // add line through in li when double click
     itenLi.addEventListener('dblclick', function() {
@@ -30,4 +27,15 @@ window.onload = function () {
 
   const text = document.querySelector('#criar-tarefa');
   text.addEventListener('click', cleanTextInput);
+
+  // Add color background in li line when clicked
+  function colorLineClicked () {
+    let listLi = document.querySelectorAll('li');
+    listLi = event.target;
+    let lineSelected = document.querySelector('.line-selected');
+    if (lineSelected) {
+      lineSelected.classList.remove('line-selected');
+    }
+      listLi.classList.add('line-selected');
+  }
 }
