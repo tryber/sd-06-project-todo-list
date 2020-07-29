00182664f3468f7d0632ac967fd3646c026538d9
Marthus Sera <marthus2001@gmail.com>
2020-07-29 15:14:09 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const header = document.getElementsByClassName('header')[0];
+const button = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+
+function addNewItem() {
+  let input = document.getElementById('texto-tarefa');
+  if (input.value === "") {
+    return
+  }
+  let newItem = document.createElement('li');
+  newItem.appendChild(document.createTextNode(input.value));
+  list.appendChild(newItem);
+  input.value = "";
+}
+
+button.addEventListener('click', function() {addNewItem()});
+
+
+
