a9ea3b55dd32aa16a6eb39063f12477c81cd80e5
isabellavjs <isabellavjs@gmail.com>
2020-07-29 11:11:23 -0300

    Requirements 4

---
+++ script.js
@@ -0,0 +1,5 @@
+//Create an ordered list
+const orderedList = document.createElement('ol');
+let cointainerList = document.getElementsByClassName('list-container')[0];
+orderedList.id = 'lista-tarefa';
+cointainerList.appendChild(orderedList);
