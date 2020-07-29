5101f007d6cfc669682d40fa6cf3d471356c08c4
thalesgr <thalesgr@gmail.com>
2020-07-29 15:24:39 -0300

    correçoes codeclimate

--- script.js
+++ script.js
@@ -2,46 +2,55 @@ const tarefa = document.getElementById('texto-tarefa');
 const adicionar = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const deletarTudo = document.getElementById('apaga-tudo');
-const deletarFinalizados = document.getElementById('remover-finalizados')
+const deletarFinalizados = document.getElementById('remover-finalizados');
+const deletarSelecionado =document.getElementById('remover-selecionado');
 
 adicionar.addEventListener('click', () => {
-  let elemento = document.createElement('li');
+  const elemento = document.createElement('li');
   elemento.innerHTML = tarefa.value;
   lista.appendChild(elemento);
   tarefa.value = '';
-  let itemLista = document.querySelectorAll('li');
-  itemLista.forEach(item => {
+  const itemLista = document.querySelectorAll('li');
+  itemLista.forEach((item) => {
     item.addEventListener('click', () => {
-      itemLista.forEach(item => {
+      itemLista.forEach((item) => {
         item.classList.remove('selected');
-      })
+      });
       item.classList.add('selected');
-    })
-  })
+    });
+  });
 });
 
 lista.addEventListener('dblclick', (e) => {
-  let completed = e.target;
-  if(!completed.classList.contains('completed')){
+  const completed = e.target;
+  if (!completed.classList.contains('completed')) {
     completed.className = 'completed';
-  } 
-  else {
+  } else {
     completed.classList -= 'completed';
   }
 });
 
-deletarTudo.addEventListener('click', function ()  {
-  let itemLista = document.querySelectorAll('li');
-    itemLista.forEach(item => {
-      lista.removeChild(item);
-    })
+deletarTudo.addEventListener('click', function () {
+  const itemLista = document.querySelectorAll('li');
+  itemLista.forEach((item) => {
+    lista.removeChild(item);
+  });
 });
 
 deletarFinalizados.addEventListener('click', function () {
-  let itemLista = document.querySelectorAll('li');
-    itemLista.forEach(item => {
-      if(item.classList.contains('completed')) {
-        lista.removeChild(item);
-      }
-    })
-})
+  const itemLista = document.querySelectorAll('li');
+  itemLista.forEach((item) => {
+    if(item.classList.contains('completed')) {
+      lista.removeChild(item);
+    }
+  });
+});
+
+deletarSelecionado.addEventListener('click', function () {
+  const itemLista = document.querySelectorAll('li');
+  itemLista.forEach((item) => {
+    if(item.classList.contains('selected')) {
+      lista.removeChild(item);
+    }
+  });
+});
