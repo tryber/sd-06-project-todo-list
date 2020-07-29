6e187d49aef0792cc6d36e33c67b75b01de2c38f
Micaeli Teixeira <luime6@gmail.com>
2020-07-29 15:55:43 -0300

    fazendo requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-window.onload = function (){
- 
+window.onload = function () {
+  
+ /* fazendo a parte da adição de texto pelo botão */
+  const botao = document.getElementById('criar-tarefa');
+  
+  botao.addEventListener('click', function (){
+    let texto = document.getElementById('texto-tarefa');
+    const lista = document.getElementById('lista-tarefas');
+    const listaOl = document.createElement('li');
+    listaOl.innerHTML = texto.value;
+    lista.appendChild(listaOl);
+  })
 }
