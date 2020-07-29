c455b5c5c83845485e055567531183e9bc4774d3
Micaeli Teixeira <luime6@gmail.com>
2020-07-29 16:22:43 -0300

    limpando a imput

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ window.onload = function () {
   const botao = document.getElementById('criar-tarefa');
   
   botao.addEventListener('click', function (){
-    let texto = document.getElementById('texto-tarefa');
+    let texto = document.getElementById('texto-tarefa').value;
     const lista = document.getElementById('lista-tarefas');
     const listaOl = document.createElement('li');
-    listaOl.innerHTML = texto.value;
+    listaOl.innerHTML = texto;
     lista.appendChild(listaOl);
-  })
+  });
+ 
+  /* fazendo a parte do reset da imput*/
+  function resetTexto () {
+   document.querySelector('#texto-tarefa').value = '';
+ }
+ const text = document.querySelector('#criar-tarefa');
+ text.addEventListener('click', resetTexto);
+
 }
