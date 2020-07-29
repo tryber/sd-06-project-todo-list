718219ccf0494743b2be453dd493892e51988e19
Bruna Franciele <brunafranciele1902@gmail.com>
2020-07-28 23:26:44 -0300

    envio doo requisito 7

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 window.onload = function() {
     let botao = document.querySelector("#criar-tarefa")
     let caixaTexto = document.querySelector("#texto-tarefa")
-    
+    // função para criar as li's da lista
     botao.addEventListener("click", function() {
-    let linhaLista = document.createElement("li")
-    document.getElementById("lista-tarefas").appendChild(linhaLista)
-    linhaLista.innerHTML = caixaTexto.value
-    caixaTexto.value = ""
+      let linhaLista = document.createElement("li")
+      document.getElementById("lista-tarefas").appendChild(linhaLista)
+    // adicionando o conteúdo digitado no input a lista
+      linhaLista.innerHTML = caixaTexto.value
+    // limpando o input depois do click
+      caixaTexto.value = ""
+
+    // adicionando cor de fundo cinza ao li que receber um click
     linhaLista.addEventListener("click", function(){
-            linhaLista.style.backgroundColor = "rgb(128, 128, 128)"
+        let lista = document.getElementsByTagName("li");
+        for(let index = 0; index < lista.length; index ++) {
+            lista[index].classList.remove("corDeFundo")
+        }
+        linhaLista.classList.add("corDeFundo")
         })
+
+    // criando botão que apaga a lista inteira
     let botaoApagaLista = document.querySelector("#apaga-tudo")
     botaoApagaLista.addEventListener("click", function() {
         document.getElementById("lista-tarefas").removeChild(linhaLista)
         }) 
-        linhaLista.addEventListener("dblclick", function() {
-        if(linhaLista.className != "completed") {
-            linhaLista.className = "completed"
-        } else {
-            linhaLista.className = ""
-        }
+        
+    // adicionando a classe de completados aos li's que receberem dobleclick
+    // e que quando receber outro dobleclick deixar de receber a classe completados
+    linhaLista.addEventListener("dblclick", function() {
+      if(linhaLista.className != "completed") {
+        linhaLista.className = "completed"
+      } else {
+        linhaLista.className = ""
+      }
+    // adicionando botão que exclui os itens marcados como completados da lista
         let botaoFinalizados = document.querySelector("#remover-finalizados")
     botaoFinalizados.addEventListener("click", function() {
             document.querySelector(".completed").remove()
