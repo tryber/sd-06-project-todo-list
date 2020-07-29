5bb56b5efc1bdd8f3bdca189a742608d3c3af06e
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-07-28 23:28:52 -0300

    fix all functions to save status

--- script.js
+++ script.js
@@ -1,36 +1,106 @@
-function saveList() {
+function alternativeSaveList() {
   const arrayLi = [];
   for (const item of document.getElementsByTagName('li')) {
-    arrayLi.push(item.innerText);
+    arrayLi.push(item.innerText, item.classList[0], item.classList[1]);
   }
   if (arrayLi.length !== 0){
     localStorage.setItem('ol', arrayLi);
   } 
 }
 
-function createListItem() {
-  const item = document.createElement('li');
-  item.innerText = document.getElementById('texto-tarefa').value;
-  document.getElementById('lista-tarefas').appendChild(item);
-  document.getElementById('texto-tarefa').value = '';
-  saveList();
+function alternativeMountList() {
+  if (localStorage.ol !== undefined) {
+    for (let itens = 0; itens < localStorage.ol.split(',').length; itens += 3) {
+      const listItem = document.createElement('li');
+      listItem.innerText = localStorage.ol.split(',')[itens];
+      if (localStorage.ol.split(',')[itens + 1] !== ''){
+        listItem.classList.add(localStorage.ol.split(',')[itens + 1]);
+      }
+      if (localStorage.ol.split(',')[itens + 2] !== ''){
+        listItem.classList.add(localStorage.ol.split(',')[itens + 2]);
+      }
+      document.getElementById('lista-tarefas').appendChild(listItem);
+    }
+  }
 }
 
-function selectListItem(event) {
+function alternativeSelectListItem(event) {
+  let session = localStorage.ol.split(',');
+  for(let item = 0; item < session.length; item +=3) {
+    if (session[item] === event.target.innerText) {
+      session[item + 1] = 'selected';
+    } else {
+      session[item + 1] = 'not-selected';
+    }
+  }
   for (const item of document.getElementsByTagName('li')) {
     if (item.classList.contains('selected')) {
       item.classList.remove('selected');
     }
   }
+  event.target.classList.remove('not-selected');
   event.target.classList.add('selected');
+  localStorage.setItem('ol', session);
 }
 
-function doubleSelectListItem(event) {
+function alternativeCreateListItem() {
+  const item = document.createElement('li');
+  item.innerText = document.getElementById('texto-tarefa').value;
+  item.classList.add('not-selected', 'not-completed');
+  document.getElementById('lista-tarefas').appendChild(item);
+  document.getElementById('texto-tarefa').value = '';
+  alternativeSaveList();
+}
+
+function alternativeDoubleSelectListItem(event) {
+  let session = localStorage.ol.split(',');
+  for(let item = 0; item < session.length; item +=3) {
+    if (session[item] === event.target.innerText) {
+      if (session[item + 2] === 'completed') {
+        session[item + 2] = 'not-completed';
+      } else {
+        session[item + 2] = 'completed';
+      }
+    }
+  }
   if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
   } else {
     event.target.classList.add('completed');
   }
+  localStorage.setItem('ol', session);
+}
+
+function alternativeClearSelected() {
+  if (localStorage.ol !== undefined && localStorage.ol.split(',').includes(document.querySelector('.selected').innerText)) {
+    let arrayP = localStorage.ol.split(',');
+    const index = localStorage.ol.split(',').indexOf(document.querySelector('.selected').innerText);
+    arrayP.splice(index, 3);
+    localStorage.ol = arrayP;
+  }
+  if (document.getElementsByTagName('li').length !== 0) {
+    document.getElementById('lista-tarefas').removeChild(document.querySelector('.selected'));
+  }
+}
+
+function alternativeClearCompleted() {
+  let clone = [];
+  for (const item of document.querySelectorAll('.completed')) {
+    clone.push(item);
+  }
+  if (localStorage.ol !== undefined) {
+    for (const item of clone) {
+      if (localStorage.ol.split(',').includes(item.innerText)) {
+        let arrayP = localStorage.ol.split(',');
+        const index = localStorage.ol.split(',').indexOf(item.innerText);
+        arrayP.splice(index, 3);
+        localStorage.ol = arrayP;
+      }
+    }
+  }
+  for (const item of clone) {
+    document.getElementById('lista-tarefas').removeChild(item);
+  } 
 }
 
 function clearList() {
@@ -41,51 +111,7 @@ function clearList() {
   for (const item of clone) {
     document.getElementById('lista-tarefas').removeChild(item);
   }
-  localStorage.removeItem('ol');
-}
-
-function clearCompleted() {
-  let clone = [];
-  for (const item of document.querySelectorAll('.completed')) {
-    clone.push(item);
-  }
-  if (localStorage.ol !== undefined) {
-    console.log(clone)
-    for (const item of clone) {
-      if (localStorage.ol.split(',').includes(item.innerText)) {
-        let arrayP = localStorage.ol.split(',');
-        const index = localStorage.ol.split(',').indexOf(item.innerText)
-        arrayP.splice(index, 1);
-        localStorage.ol = arrayP;
-      }
-    }
-  }
-  for (const item of clone) {
-    document.getElementById('lista-tarefas').removeChild(item);
-  } 
-}
-
-function clearSelected() {
-  if (localStorage.ol !== undefined && localStorage.ol.split(',').includes(document.querySelector('.selected').innerText)) {
-    let arrayP = localStorage.ol.split(',');
-    const index = localStorage.ol.split(',').indexOf(document.querySelector('.selected').innerText)
-    arrayP.splice(index, 1);
-    localStorage.ol = arrayP;
-  }
-  if (document.getElementsByTagName('li').length !== 0) {
-    document.getElementById('lista-tarefas').removeChild(document.querySelector('.selected'));
-  }
-  
-}
-
-function mountList() {
-  if (localStorage.ol !== undefined) {
-    for (const item of localStorage.ol.split(',')) {
-      const listItem = document.createElement('li');
-      listItem.innerText = item;
-      document.getElementById('lista-tarefas').appendChild(listItem);
-    }
-  }
+  localStorage.clear();
 }
 
 function moveUp() {
@@ -97,12 +123,12 @@ function moveDown() {
 }
 
 window.onload = function () {
-  document.getElementById('criar-tarefa').addEventListener('click', createListItem);
-  document.getElementById('lista-tarefas').addEventListener('click', selectListItem);
-  document.getElementById('lista-tarefas').addEventListener('dblclick', doubleSelectListItem);
+  document.getElementById('criar-tarefa').addEventListener('click', alternativeCreateListItem);
+  document.getElementById('lista-tarefas').addEventListener('click', alternativeSelectListItem);
+  document.getElementById('lista-tarefas').addEventListener('dblclick', alternativeDoubleSelectListItem);
   document.getElementById('apaga-tudo').addEventListener('click', clearList);
-  document.getElementById('remover-finalizados').addEventListener('click', clearCompleted);
-  document.getElementById('remover-selecionado').addEventListener('click', clearSelected);
-  document.getElementById('salvar-tarefas').addEventListener('click', saveList);
-  mountList();
+  document.getElementById('remover-finalizados').addEventListener('click', alternativeClearCompleted);
+  document.getElementById('remover-selecionado').addEventListener('click', alternativeClearSelected);
+  document.getElementById('salvar-tarefas').addEventListener('click', alternativeSaveList);
+  alternativeMountList();
 }
