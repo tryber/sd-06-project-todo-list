586ed266ccc1847b62c1c2e97d2fe2dabb72f0a3
Kesley Lahoz <namielahoz@gmail.com>
2020-07-29 17:04:18 -0300

    add button works

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 
 window.onload = function () {
     //button to input text 
-    let textInput = document.querySelector("#texto-tarefa");
+    let textValue = document.querySelector("#texto-tarefa");
     let addBtn = document.querySelector("#criar-tarefa");
-    let takeText = document.querySelector("#lista-tarefas");
+    let doList = document.querySelector("#lista-tarefas");
 
     addBtn.addEventListener('click', function () {
-        for (let index = 0; index < textInput.length; index += 1) {
             let liElement = document.createElement("li");
-            takeText[i] = liElement + textInput.value;
-        }
+            liElement.innerHTML = textValue.value;
+            doList.appendChild(liElement);
+            textValue.value = '';
     });
-
-
-
-
-
 
 
 
