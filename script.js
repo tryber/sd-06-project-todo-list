dcf2da7eeeef6e5f3a0eeb0b6fc723504cef95b3
LughWalle <civalfran1@gmail.com>
2020-07-29 16:42:43 -0500

    requisitos 1 ao 5 codeclimate concertado

--- script.js
+++ script.js
@@ -4,21 +4,25 @@ const createText = document.querySelector('#texto-tarefa');
 const createList = document.querySelector('#lista-tarefas');
 
 // functions
- // cria itens da lista
+  // cria itens da lista
 function createLi() {
-  let itemList = createText.value;
-  let item = document.createElement('li');
+  const itemList = createText.value;
+  const item = document.createElement('li');
   item.innerText = itemList;
   item.className = 'work';
   if (itemList === '') {
+
     const span = document.createElement('span');
     span.innerText = 'digite uma tarefa';
     span.className = 'span';
     document.querySelector('#addTarefas').appendChild(span);
 
+
   } else {
+
     createList.appendChild(item);
     createText.value = '';
+    
   }
 }
 
