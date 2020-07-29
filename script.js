20a69fb7e4b150134d7529a95e50e312f5c79777
FPhilipi <fabiphilipi@gmail.com>
2020-07-29 11:41:06 -0300

    Requisitos

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+//criar filho de head com o output da lista
+
+let elementUl = document.createElement('ol');
+  // Reqquisito 4
+  // elementUl.idName = "lista-tarefas";
+  document.body.appendChild(elementUl);  
+  console.log(elementUl);
+
 let button = document.getElementById("criar-tarefa");
       
-      let ordList = document.querySelector("#lista-tarefas");
+button.addEventListener("click", function makeList() {
+  let activity = document.getElementById("texto-tarefa").value;
+  console.log(activity);
+     
+  // let activityArray
+  let elementLi = document.createElement('li');
+    elementLi.innerHTML = `${activity}`;
+    elementUl.appendChild(elementLi);
+    console.log(`${activity}`);
+    activity = "";
+  // Requisito 5  
+  // limpar caixa
+  });  
+      
+  
 
 
-      button.addEventListener("click", function makeList() {
-      let atividade = document.getElementById("texto-tarefa").value;
-      ordList.appendChild = atividade;
-      
-    //   ordList.appendChild(atividade);
-      
-      ordList.innerHTML = `${atividade}`;
-      atividade = "";
-      });
+  
