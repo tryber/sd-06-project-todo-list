7683f69607599345383cc99553c131745d5e6b14
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-07-29 15:32:48 -0300

    Teste de requisitos

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-window.onload = function() {
-  let tarefa = document.querySelector("#texto-tarefa")
-  let tarefaBtn = document.querySelector("#criar-tarefa");
-  let lista = document.querySelector("#lista-tarefas");  
-
-  tarefaBtn.addEventListener("click", function(){
+window.onload = function () {
+  const tarefa = document.querySelector('#texto-tarefa');
+  const tarefaBtn = document.querySelector('#criar-tarefa');
+  const lista = document.querySelector('#lista-tarefas');
+  const listItens = document.getElementsByTagName('li');
+  tarefaBtn.addEventListener('click', function () {
     lista.innerHTML += '<li>' + tarefa.value + '</li>';
     tarefa.value = '';
   });
-
-  lista.addEventListener("click", function(e) {
-    let cont = 0
-    let range = document.elementFromPoint(e.clientX, e.clientY);
-    range.style.backgroundColor = "rgb(128, 128, 128)";
+  lista.addEventListener('click', function () {
+    for (let i = 0; i < listItens.length; i += 1) {
+      listItens[i].addEventListener('click', function () {
+        for (let j = 0; j < listItens.length; j += 1) {
+          if (listItens[j].classList.contains('selected') === true) {
+            listItens[j].classList.remove('selected');
+          }
+        }
+        listItens[i].classList.add('selected');   
+      });
+    }
   });
 }
