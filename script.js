3f586f63d5dd1e43978855a016d59317060bcdde
cyranowebdev <cyrano@cyranowebdev.com>
2020-07-29 18:45:32 -0300

    add requisito button apagar selecionado + ajustes code climate.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let tasksList = document.querySelector('#tasks ol');
+const tasksList = document.querySelector('#tasks ol');
 
 function itemClickBg(event) {
-  let listItems = document.querySelectorAll('#tasks ol li');
+  const listItems = document.querySelectorAll('#tasks ol li');
   for (const i of listItems) {
     if (i.classList.contains('selected')) {
       i.classList.remove('selected');
@@ -11,16 +11,16 @@ function itemClickBg(event) {
 }
 
 function itemCompleted(event) {
-  if(event.target.classList.contains('completed')) {
-    event.target.classList.remove('completed');  
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
   } else {
     event.target.classList.add('completed');
   }
 }
 
 function createItem() {
-  let createItemElement = document.createElement('li');
-  let itemTxt = document.querySelector('#texto-tarefa').value;
+  const createItemElement = document.createElement('li');
+  const itemTxt = document.querySelector('#texto-tarefa').value;
   createItemElement.innerText = itemTxt;
   tasksList.appendChild(createItemElement);
   createItemElement.addEventListener('click', itemClickBg);
@@ -28,26 +28,26 @@ function createItem() {
   document.querySelector('#texto-tarefa').value = '';
 }
 
-let createItemBtn = document.querySelector('#criar-tarefa');
+const createItemBtn = document.querySelector('#criar-tarefa');
 createItemBtn.addEventListener('click', createItem);
 
 function eraseAll(event) {
   let tasks = document.querySelector('#tasks ol').children;
   if (tasks.length) {
-    alert('Certeza que quer apagar tudo?');
+    alert('Apagar tudo?');
     for (let task = tasks.length - 1; task >= 0; task -= 1) {
       tasksList.removeChild(tasks[task]);
     }
   }
 }
 
-let eraseAllBtn = document.querySelector('#apaga-tudo');
+const eraseAllBtn = document.querySelector('#apaga-tudo');
 eraseAllBtn.addEventListener('click', eraseAll);
 
 function eraseCompleted(event) {
   let tasks = document.querySelector('#tasks ol').children;
   if (tasks.length) {
-    alert('Certeza que quer apagar as tarefas completadas?');
+    alert('Apagar as tarefas completadas?');
     for (let task = tasks.length - 1; task >= 0; task -= 1) {
       if(tasks[task].classList.contains('completed')) {
         tasksList.removeChild(tasks[task]);
@@ -56,5 +56,21 @@ function eraseCompleted(event) {
   }
 }
 
-let eraseCompletedBtn = document.querySelector('#remover-finalizados');
+const eraseCompletedBtn = document.querySelector('#remover-finalizados');
 eraseCompletedBtn.addEventListener('click', eraseCompleted);
+
+function eraseSelected(event) {
+  let tasks = document.querySelector('#tasks ol').children;
+  if (tasks.length) {
+    for (let task = tasks.length - 1; task >= 0; task -= 1) {
+      if(tasks[task].classList.contains('selected')) {
+        alert('Remover tarefa selecionada?');
+        tasksList.removeChild(tasks[task]);
+        break;
+      }
+    }
+  }
+}
+
+const eraseSelectedBtn = document.querySelector('#remover-selecionado');
+eraseSelectedBtn.addEventListener('click', eraseSelected);
