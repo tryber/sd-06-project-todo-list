a81db02233105abdc68a48915d5e218773b11a48
FPhilipi <fabiphilipi@gmail.com>
2020-07-29 18:50:58 -0300

    vendo o que funciona

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
 //criar filho de head com o output da lista
 
 let elementOl = document.createElement('ol');
-  // Reqquisito 4
   elementOl.id = "lista-tarefas";
   document.body.appendChild(elementOl);  
   console.log(elementOl);
 
 let button = document.getElementById("criar-tarefa");
-let elementLi = document.createElement('li');
 
-button.addEventListener("click", function makeList() {
-  let activity = document.getElementById("texto-tarefa");
-  // console.log(activity);
-     
-  // let activityArray
-  
-    // elementLi.innerHTML = `${activity}`;
+let activity = document.getElementById("texto-tarefa");
+
+button.addEventListener("click", function() {
+  let elementLi = document.createElement('li');    
     elementLi.innerHTML = activity.value;
     elementOl.appendChild(elementLi);
-    // console.log(`${activity}`);
     activity.value = "";  
   });  
-  
-let lineElement = document.getElementsByTagName("li");
-elementLi.addEventListener("click", function(){
-  
-  for(let index = 0; index < lineElement.length; index += 1) {
-      lineElement[index].classList.remove("backGroundColor");
-  }
-  lineElement.classList.add("backGroundColor");
-  });
+
+// let lineElement = document.getElementsByTagName("li");
+
+// elementOl.addEventListener("click", function() {
+//   for(let index = 0; index < lineElement.length; index += 1) {
+//     lineElement[index].classList.remove("backGroundColor");
+//   // }
+//   // lineElement.classList.add("backGroundColor");
+//   });
 
 
-  // id="apaga-tudo">Apagar Lista</button> <button id="remover-finalizados">
+//   // id="apaga-tudo">Apagar Lista</button> <button id="remover-finalizados">
 
-let buttonCleanAll = document.querySelector("#apaga-tudo");
-  buttonCleanAll.addEventListener("click", function() {
-  document.getElementById("lista-tarefas").removeChild(elementLi);
-  console.log("oi");
-});
+// let buttonCleanAll = document.querySelector("#apaga-tudo");
+//   buttonCleanAll.addEventListener("click", function() {
+//   document.getElementById("lista-tarefas").removeChild(elementLi);
+//   console.log("oi");
+// });
 
 
 
