05f185ec5bf0db0da44a182d555df928087fc96e
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-07-30 15:43:35 -0300

    trying to save pos after every up-down

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ function enableMovingButtons() {
         renderTodaysTodos();
         const everyTodo = document.querySelectorAll(".daily");
         everyTodo[currentIndex - 1].classList.add('selected');
+        saveTodos();
       }
     }
 
@@ -76,6 +77,7 @@ function enableMovingButtons() {
         renderTodaysTodos();
         const everyTodo = document.querySelectorAll(".daily");
         everyTodo[currentIndex + 1].classList.add('selected');
+        saveTodos();
       }
     }
 
