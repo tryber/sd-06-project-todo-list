a154c0268f137786220f8912bf06782e83fcaf3b
kissyla <kissylamottapereira@gmail.com>
2020-07-30 16:49:43 -0300

    adicionando botão criar lista

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 window.onload = function(){
-//5 - Deve haver um botão com id="criar-tarefa" e ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo.
-//Será verificada a existência de um elemento do tipo button com o id criar-tarefa
-//No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
-//A adição de elementos na lista será feita algumas vezes, e será checado se todos os itens criados permanecem na lista na medida em que novos são adicionados.
 
-function createToDo (){
+function createToDo(event){
     
-}
+    //colocar o texto informado na variavel
+    let inputText = document.getElementById('texto-tarefa').value; //guardei input
+    //criar elemento li sendo filho do elemento id="lista-tarefas"
+    console.log(inputText);
+
+    let line = document.createElement('li');
+    //localizar onde criar li elementoPai (no caso lista-tarefas).appendchild('elemento html que eu criei')
+    console.log(line);
+    line.innerText = inputText;
+    //adicionar texto de inputText no elemento li criado
+    document.getElementById('lista-tarefas').appendChild(line);
     
+   }
+ 
+    document.getElementById('criar-tarefa').addEventListener('click', createToDo);
 }
