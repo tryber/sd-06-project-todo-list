4e87940f62ffe81f7b2abe80a2ec0437ff013e41
LeandroFeitozaGnu <leandro.feitoza8018@gmail.com>
2020-07-30 11:43:42 -0300

    Correçoes do codeclimate apontando 32 issues

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ document.querySelector('.entrada-dados').appendChild(buttonAdd);
 
 // Cria a lista ordenada
 const taskList = document.createElement('ol');
-taskList.id='lista-tarefas'
+taskList.id = 'lista-tarefas';
 document.querySelector('.lista-tarefas').appendChild(taskList);
 
 function generateItemList() {
-    let task = document.querySelector('#texto-tarefa').value
-    itemList = document.createElement('li')
-    itemList.classList = 'lista'
-    itemList.appendChild(document.createTextNode(task))
-    taskList.appendChild(itemList)
-    let LISTA = document.querySelector('li')
-    LISTA.classList = 'selected'
-    document.querySelector('#texto-tarefa').value = ''
+  let task = document.querySelector('#texto-tarefa').value;
+  itemList = document.createElement('li');
+  itemList.classList = 'lista';
+  itemList.appendChild(document.createTextNode(task));
+  taskList.appendChild(itemList);
+  let LISTA = document.querySelector('li');
+  LISTA.classList = 'selected';
+  document.querySelector('#texto-tarefa').value = '';
 };
 buttonAdd.addEventListener('click', generateItemList);
