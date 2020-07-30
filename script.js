c47e7a51a02aa6d7138c88f2b1b8f735f35bb79f
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-07-30 17:57:32 -0300

    mais javaScript

--- script.js
+++ script.js
@@ -23,31 +23,7 @@ window.onload = function () {
 
     function resetTexto() {
         document.querySelector('#texto-tarefa').value = '';
-    }
-    const text = document.querySelector('#criar-tarefa');
-    text.addEventListener('click', resetTexto);
-
-    function backColor() {
-        let listOl = document.querySelectorAll('li');
-        listOl = event.target;
-        const bgColor = document.querySelector('.bgColor');
-        if (bgColor) {
-            bgColor.classList.remove('bgColor');
         }
-        listOl.classList.add('bgColor');
-    }
-
-    function deleteList() {
-        const list = document.getElementById('lista-tarefas');
-        while (list.firstChild) {
-            list.removeChild(list.firstChild);
-        }
-    }
-
-    function removeComplet() {
-        const removeText = document.querySelectorAll('.completed');
-        for (let i = 0; i < removeText.length; i += 1) {
-            removeText[i].remove();
-        }
-    }
-};
+        const text = document.querySelector('#criar-tarefa');
+        text.addEventListener('click', resetTexto);
+        
