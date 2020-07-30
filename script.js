4b69cdd88ef3d7cc5cee3dd368c3a495076c4339
theCoyote <flaviolunaferreira@gmail.com>
2020-07-30 17:51:25 -0300

    adicionando btn incluir e limpar tudo

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+const btnIncluir = document.getElementById('criar-tarefa');
+const btnApagaTudo = document.getElementById('apaga-tudo');
 
+window.onload = function () {
+
+btnIncluir.addEventListener('click', incluirTarefa);
+btnApagaTudo.addEventListener('click', apagaTudo);
+}
+
+/* *********************************************** */
+/* trabalhando com o botão incluir                 */
+/* *********************************************** */
+function incluirTarefa() {
+  const texto = document.getElementById('texto-tarefa').value;
+  if (texto !== '') {
+    const item = document.createElement('li');
+    item.innerText = document.getElementById('texto-tarefa').value;
+    document.getElementById('lista-tarefas').appendChild(item);
+    item.style.cursor = 'pointer';
+    const tarefa = document.getElementById('texto-tarefa');
+    tarefa.value = '';
+    tarefa.focus();
+  } else {
+    alert('Campo tarefa precisa ser preenchido!!!');
+  }
+}
+/* *********************************************** */
+/* Apagando todas as tarefas da lista              */
+/* *********************************************** */
+function apagaTudo() {
+  const itens = document.querySelectorAll('li');
+  for (let i = 0; i < itens.length; i += 1) {
+    document.getElementById('lista-tarefas').removeChild(itens[i]);
+  }
+}
 
 
 
