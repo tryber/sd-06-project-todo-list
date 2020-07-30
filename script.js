41c5fceae97cd91b73f3e9757e38d3791ccf2499
Daniel Moura <darthur.moura@gmail.com>
2020-07-30 15:43:10 -0300

    Várias mudanças e implementações em script.js e style.css

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 let textInput = document.getElementById("texto-tarefa");
 let addBtn = document.getElementById("criar-tarefa");
 let listaTarefas = document.getElementById("lista-tarefas");
+let remover = document.getElementById("remover-selecionado");
+let apagarCompletos = document.getElementById("apagar-completos");
+let itens = listaTarefas.getElementsByTagName("li");
+let subir = document.getElementById("subir-selecionado");
+let descer = document.getElementById("descer-selecionado");
+
 
 // Funções e eventos para adicionar novo item à lista de tarefas
 addBtn.addEventListener('click', adicionarTarefa);
@@ -27,22 +33,50 @@ textInput.addEventListener("keyup", function(event){
 listaTarefas.addEventListener("click", adicionaSelecao);
 
 function adicionaSelecao(event) {
-  let itens = listaTarefas.getElementsByTagName("li");
   for(let i = 0; i < itens.length; i += 1) {
     if(itens[i].classList.contains("selecionado")) {
       itens[i].classList.remove("selecionado");
     }
   }
-  event.target.classList.add("selecionado");
+ if(event.target.id != "lista-tarefas"){
+   event.target.classList.add("selecionado");
+ }
 }
 
 // Marcar item como completo com duplo click
 listaTarefas.addEventListener("dblclick", marcaCompleto);
 
 function marcaCompleto(event) {
-  if(event.target.classList.contains("completed")) {
-    event.target.classList.remove("completed");
-  } else {
-    event.target.classList.add("completed");
+  if(event.target.id != "lista-tarefas") {
+    if(event.target.classList.contains("completed")) {
+      event.target.classList.remove("completed");
+    } else {
+      event.target.classList.add("completed");
+    }
   }
+}
+
+// Botão para apagar item selecionado
+remover.addEventListener("click", function() {
+  let selecionado = document.querySelector(".selecionado");
+  selecionado.remove();
+})
+
+// Botão para apagar os itens completos
+apagarCompletos.addEventListener("click", apagaCompletos);
+
+function apagaCompletos() {
+  for(let i = itens.length-1; i >= 0; i -= 1) {
+    if(itens[i].classList.contains("completed")) {
+      itens[i].remove();
+    }
+  }
+}
+
+// Botões de subir e descer item na lista
+subir.addEventListener("click", sobeItem);
+descer.addEventListener("click", desceItem);
+
+function sobeItem() {
+  let anterior = event.target.
 }
