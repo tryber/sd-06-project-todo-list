a7c5200f0e1035e65a2693d48b148abab37a5b7c
Pedro <pedroedu77@gmail.com>
2020-07-29 22:43:18 -0300

    Projeto Lista de Tarefas Concluidos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const createButton = document.getElementById('criar-tarefa');
 const listCreate = document.getElementById('lista-tarefas');
 const textCreate = document.getElementById('texto-tarefa');
+const span = document.getElementsByTagName('span');
 
 // Funções e Eventos //
 createButton.addEventListener('click', function () {
@@ -10,9 +11,23 @@ createButton.addEventListener('click', function () {
   item.innerText = listItem;
   item.className = 'work';
   if (listItem === '') {
-    
+    span.innerText = 'Caixa em branco, por favor digite um texto';
   } else {
     listCreate.appendChild(item);
     textCreate.value = '';
   }
 })
+
+listCreate.addEventListener('click', function (e) {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  e.target.className += ' selected';
+})
+
+listCreate.addEventListener('dblclick', function(e){
+  if (e.target.classList.contains('completed')) {
+    e.target.classList.remove('completed');
+  } else {
+    e.target.className += ' completed';
+  }
+});
