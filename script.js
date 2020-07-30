f6d534f1445f4c5a7ca334b65a3a05b73d9c1927
ricardorosa-dev <ricardorosa@gmail.com>
2020-07-30 18:23:38 -0300

    Requisito 9 - Ao clicar duas vezes em um item, ele deverá ser riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item.

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-let items = ["comer banana", "tomar chocolate quente"];
 let textValue;
 const inputText = document.getElementById("texto-tarefa");
 const lista = document.getElementById("lista-tarefas");
+let items = lista.children;
 let selected;
+
+//DEPOIS APAGA, SÓ PRA TESTE
+lista.innerHTML = "<li>uma coisa</li><li>duas coisa</li><li>três coisa</li>";
+//APAGA
+
+function selectItem() {
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].addEventListener("click", function () {
+      if (selected) {
+        selected.style.backgroundColor = "";
+        selected = "";
+      }
+      items[i].style.backgroundColor = "rgb(128,128,128)";
+      selected = items[i];
+    });
+
+    items[i].addEventListener("dblclick", function () {
+      if (items[i].classList.contains("completed")) {
+        items[i].classList.remove("completed");
+      } else {
+        items[i].classList.add("completed");
+      }
+    });
+  }
+}
 
 // reconstrói a lista toda vez que adiciona ou retira algo
 function rebuildList() {
@@ -12,17 +37,6 @@ function rebuildList() {
     const newItem = document.createElement("li");
     lista.appendChild(newItem);
     newItem.innerText = items[i];
-
-    let thisItem = lista.children[i];
-    // fica cinza quando clica
-    thisItem.addEventListener("click", function () {
-      if (selected) {
-        selected.style.backgroundColor = "";
-      }
-      selected = thisItem;
-      selected.style.backgroundColor = "rgb(128,128,128)";
-      console.log(selected);
-    });
   }
 }
 
@@ -31,7 +45,9 @@ function addItem() {
   textValue = undefined;
   inputText.value = "";
   rebuildList();
+  selectItem();
 }
+
 // Faz o input e o botão funcionarem
 function setInput() {
   inputText.addEventListener("input", function () {
@@ -64,7 +80,9 @@ function setButton() {
 window.onload = function () {
   setInput();
   setButton();
-  rebuildList();
+  //rebuildList();
+  selectItem(); //APAGA
 };
 
 // ir pro input apertando TAB, não importa onde esteja
+// armazenar a lista no webstorage, de modo que sempre que a pessoa entrar no site, a mesma lista está lá.
