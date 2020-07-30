ae9389a7a45f1c917ec800821f6a4ee62e1ce4b5
lucasvr39 <lucasveras1994@gmail.com>
2020-07-30 01:55:24 -0300

    add req 5 and 6 js and remove btns class

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let todoItems = [];
-const inputValue = document.querySelector('#texto-tarefa').value;
-const btnNewTask = document.querySelector('#criar-tarefa');
-btnNewTask.addEventListener('click', function () {
-  console.log(inputValue);
-});
+// btn to create new task
+
+const btnNewTask = document.getElementById('criar-tarefa');
+
+// parent element
+
+const parentList = document.getElementById('lista-tarefas');
+
+// create list item, get input value and push list value
+
+function pushListItem() {
+  const newListItem = document.createElement('li');
+  parentList.appendChild(newListItem);
+  newListItem.innerText = document.querySelector('#texto-tarefa').value;
+  document.querySelector('#texto-tarefa').value = '';
+}
+
+btnNewTask.addEventListener('click', pushListItem);
