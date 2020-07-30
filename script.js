818d2c6a3a2ef398638eb2c84ddac89788d0bac9
ricardorosa-dev <ricardorosa@gmail.com>
2020-07-29 23:13:31 -0300

    -resolvendo o code climate e adicionando estilização.

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
-window.onload = function () {
-
-    setInput();
-    rebuildList();
-}
-
 let items = [];
+let textValue;
+const inputText = document.getElementById("texto-tarefa");
 
+function addItem() {
+  items.push(textValue);
+  textValue = undefined;
+  inputText.value = "";
+  rebuildList();
+}
 // Faz o input e o botão funcionarem
 function setInput() {
-    let inputText = document.getElementById('texto-tarefa');
-    let textValue;
+  inputText.addEventListener("input", function () {
+    textValue = inputText.value;
+  });
 
-    inputText.addEventListener('input', function () {
-        textValue = inputText.value;
-    });
+  // permitir apertar enter em vez de clicar no botão
+  inputText.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+      if (textValue) {
+        addItem();
+      } else {
+        window.alert("Insira um item à lista!");
+      }
+    }
+  });
+}
 
-    // permitir apertar enter em vez de clicar no botão
-    inputText.addEventListener('keydown', function (e) {
-        if (e.code === 'Enter') {
-            if (textValue) {
-                items.push(textValue);
-                textValue = undefined;
-                inputText.value = "";
-                rebuildList();
-            } else {
-                window.alert('Insira um item à lista!');
-            }
-        }
-    });
-
-    let inputButton = document.getElementById('criar-tarefa');
-    inputButton.addEventListener('click', function () {
-
-        if (textValue) {
-            items.push(textValue);
-            textValue = undefined;
-            inputText.value = "";
-            rebuildList();
-        } else {
-            window.alert('Insira um item à lista!');
-        }
-    });
+function setButton() {
+  const inputButton = document.getElementById("criar-tarefa");
+  inputButton.addEventListener("click", function () {
+    if (textValue) {
+      addItem();
+    } else {
+      window.alert("Insira um item à lista!");
+    }
+  });
 }
 
 // reconstrói a lista toda vez que adiciona ou retira algo
 function rebuildList() {
-    let lista = document.getElementById('lista-tarefas');
-    lista.innerHTML = '';
+  const lista = document.getElementById("lista-tarefas");
+  lista.innerHTML = "";
 
-    for (let i = 0; i < items.length; i++) {
-        let newItem = document.createElement('li');
-        lista.appendChild(newItem);
-        newItem.innerText = items[i];
-    }
-
+  for (let i = 0; i < items.length; i += 1) {
+    const newItem = document.createElement("li");
+    lista.appendChild(newItem);
+    newItem.innerText = items[i];
+  }
 }
+
+window.onload = function () {
+  setInput();
+  setButton();
+  rebuildList();
+};
+
+// ir pro input apertando TAB, não importa onde esteja
