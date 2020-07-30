b3b86cfa8fdaec5ed3643fc8e536fa3b5844ba9e
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-07-30 08:24:01 -0300

    to do list primeiro commit teste de requisitos

---
+++ script.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+    const initialState = [];
+
+    const addNewTodo = (i) => {
+
+        let todoAdd = document.querySelector('#todoAddInput').value;
+
+        initialState.push({
+            todo: todoAdd
+        });
+
+        let ul = document.querySelector('#todoList');
+        let node = document.createElement('LI');
+        let textNode = document.createTextNode(initialState[i].todo);
+        node.className = "todoLi";
+        node.appendChild(textNode);
+        ul.appendChild(node);
+
+        node.addEventListener('click', () => {
+            node.className = "todoLi selected";
+        });
+
+        node.addEventListener('dblclick', () => {
+            node.className = "todoLi done";
+        })
+
+    }
+    const upTodo = () => {
+        initialState.sort((a, b) => {
+
+        });
+    }
+
+
+
+    let buttonAddTodo = document.querySelector('#newTodoButton');
+    let i = 0;
+
+    buttonAddTodo.addEventListener('click', () => {
+        addNewTodo(i);
+        i += 1;
+    });
+
+    let upLiButton = document.querySelector('#upLiButton');
+
+    upLiButton.addEventListener('click', () => {
+        upTodo();
+    })
+
+
+}
