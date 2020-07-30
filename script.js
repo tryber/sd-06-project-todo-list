607c9eaec707ff5b567426be42753bde53e89b8d
VinicinBraga <vinicius@bragamatta.com.br>
2020-07-30 17:02:29 -0300

    testando algumas funcoes - sem style

---
+++ script.js
@@ -0,0 +1,32 @@
+let addTask = document.querySelector("#criar-tarefa");
+addTask.addEventListener("click", function() {
+    let task = document.createElement("li");
+    task.className = "task";
+    let texto = document.querySelector("#texto-tarefa");
+    task.innerHTML = texto.value;
+    document.querySelector("#lista-tarefas").appendChild(task);
+    texto.value = "";
+})
+let selected = document.querySelector("#lista-tarefas");
+selected.addEventListener("click", function(){
+    if (document.querySelector(".selected") != null) {
+        document.querySelector(".selected").style.backgroundColor = "white";
+        document.querySelector(".selected").classList.remove("selected");
+    }
+    event.target.style.backgroundColor = "rgb(128,128,129)";
+    event.target.classList.add("selected");
+})
+selected.addEventListener("dblclick", function() {
+    if (event.target.className === "task selected"){
+        event.target.classList.add("completed");
+    }else {
+        event.target.classList.remove("completed");
+    }
+})
+
+let removeSelected= document.querySelector("#remover-selecionado");
+removeSelected.addEventListener("click",function(){
+    let itemSelected = document.querySelector(".selected");
+    itemSelected.remove();
+})
+
