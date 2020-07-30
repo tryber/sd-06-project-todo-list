f6e4954b3a4f90f4729285fece81c98d122e076e
Daniel Jesus <dev.danieljs@gmail.com>
2020-07-29 21:25:05 -0300

    Adding button clean all and fixing code climate

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
-window.onload = function () { 
+window.onload = function () {
   // Button click add iten list
   function buttonAddItenList() {
-    let inputText = document.querySelector('#texto-tarefa').value;
-    let list = document.querySelector('#lista-tarefas');
-    let itenLi = document.createElement('li');
-    // itenLi.style.backgroundColor = 'white';
+    const inputText = document.querySelector('#texto-tarefa').value;
+    const list = document.querySelector('#lista-tarefas');
+    const itenLi = document.createElement('li');
     itenLi.classList.add('lines');
     itenLi.innerHTML = inputText;
     list.appendChild(itenLi);
 
-    itenLi.addEventListener('click', colorLineClicked);
+    // Add color background in li line when clicked
+    itenLi.addEventListener('click', function() {
+      let listLi = document.querySelectorAll('li');
+      listLi = event.target;
+      const lineSelected = document.querySelector('.line-selected');
+      if (lineSelected) {
+        lineSelected.classList.remove('line-selected');
+      }
+        listLi.classList.add('line-selected');
+    });
 
     // add line through in li when double click
-    itenLi.addEventListener('dblclick', function() {
+    itenLi.addEventListener('dblclick', function () {
       itenLi.classList.toggle('completed');
-    })
+    });
+
+    // Button remove all list
+    const buttonCleanAll = document.querySelector('#apaga-tudo');
+    buttonCleanAll.addEventListener('click', function() {
+      const cleanLi = list;
+      cleanLi.removeChild(itenLi);
+    });
   }
 
   const button = document.querySelector('#criar-tarefa');
   button.addEventListener('click', buttonAddItenList);
 
   // Function reset input text
-  function cleanTextInput () {
+  function cleanTextInput() {
     document.querySelector('#texto-tarefa').value = '';
   }
 
   const text = document.querySelector('#criar-tarefa');
   text.addEventListener('click', cleanTextInput);
-
-  // Add color background in li line when clicked
-  function colorLineClicked () {
-    let listLi = document.querySelectorAll('li');
-    listLi = event.target;
-    let lineSelected = document.querySelector('.line-selected');
-    if (lineSelected) {
-      lineSelected.classList.remove('line-selected');
-    }
-      listLi.classList.add('line-selected');
-  }
 }
