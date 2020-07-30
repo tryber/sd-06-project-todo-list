63546d47b63ad1d61fffd2b4fb3fde2a9509abc7
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-30 18:12:00 -0300

    14. Removendo selecionado

--- script.js
+++ script.js
@@ -1,93 +1,104 @@
-window.onload = function() {
-// Criar a lista
-function criaLista() {
-  const listaItem = document.createElement('ol');
-  listaItem.id = 'lista-tarefas';
-  document.body.appendChild(listaItem);
-}
-criaLista();
-
-
-// Adicionar tarefa na lista
-function addTarefa() {
-  const textItem = document.querySelector('#texto-tarefa');
-  const item = document.createElement('li');
-  item.innerHTML = textItem.value;
-  textItem.value = '';
-  lista.appendChild(item);
-}
-const btnAdd = document.querySelector('#criar-tarefa');
-btnAdd.addEventListener('click', addTarefa);
-
-
-// Mudar cor de somente 1 elemento selecionado
-function selectingTask(event) {
-  const oldSelected = document.querySelector('.selected');
-  const selected = event.target;
-  if (oldSelected) {
-    oldSelected.classList.remove('selected');
-    selected.classList.add('selected');
-  } else {
-    selected.classList.add('selected');
-  }  
-}
-const lista = document.querySelector('#lista-tarefas');
-lista.addEventListener('click', selectingTask);
-
-
-// Riscar as tarefas completas
-let click = false;
-function completedTasks(event) {
-  const completedTask = event.target;
-  if (click === false) {
-    completedTask.classList.add('completed');
-    click = true;
-  } else if (click === true) {
-    completedTask.classList.remove('completed');
-    click = false;
+window.onload = function () {
+  // Criar a lista
+  function criaLista() {
+    const listaItem = document.createElement('ol');
+    listaItem.id = 'lista-tarefas';
+    document.body.appendChild(listaItem);
   }
-}
-lista.addEventListener('dblclick', completedTasks);
+  criaLista();
 
 
-// Apagar todos os ítens da lista
-function apagaTudo() {
-  while (lista.firstChild) {
-    lista.removeChild(lista.firstChild);
+  // Adicionar tarefa na lista
+  function addTarefa() {
+    const textItem = document.querySelector('#texto-tarefa');
+    const item = document.createElement('li');
+    item.innerHTML = textItem.value;
+    textItem.value = '';
+    lista.appendChild(item);
   }
-}
-const btnApagaTudo = document.querySelector('#apaga-tudo');
-btnApagaTudo.addEventListener('click', apagaTudo);
+  const btnAdd = document.querySelector('#criar-tarefa');
+  btnAdd.addEventListener('click', addTarefa);
 
 
-// Remover os ítens finalizados
-function removerFinalizados() {
-  const itens = document.querySelectorAll('.completed');
-  if (itens) {
-    for (let index = 0; index < itens.length; index += 1) {
-      lista.removeChild(itens[index]);
+  // Mudar cor de somente 1 elemento selecionado
+  function selectingTask(event) {
+    const oldSelected = document.querySelector('.selected');
+    const selected = event.target;
+    if (oldSelected) {
+      oldSelected.classList.remove('selected');
+      selected.classList.add('selected');
+    } else {
+      selected.classList.add('selected');
     }
-  }  
-}
-const btnApagaFinalizados = document.querySelector('#remover-finalizados');
-btnApagaFinalizados.addEventListener('click', removerFinalizados);
+  }
+  const lista = document.querySelector('#lista-tarefas');
+  lista.addEventListener('click', selectingTask);
 
-/********************************************** */
 
-// Salvar os dados no local storage
-function salvarDados() {
-  window.localStorage.setItem('minhaLista', lista.innerHTML);
-}
-const btnSalvar = document.querySelector('#salvar-tarefas');
-btnSalvar.addEventListener('click', salvarDados);
+  // Riscar as tarefas completas
+  let click = false;
+  function completedTasks(event) {
+    const completedTask = event.target;
+    if (click === false) {
+      completedTask.classList.add('completed');
+      click = true;
+    } else if (click === true) {
+      completedTask.classList.remove('completed');
+      click = false;
+    }
+  }
+  lista.addEventListener('dblclick', completedTasks);
 
-// Recuperar dados do local Stage
-function listaFoiSalva() {
-  const listaSalva = window.localStorage.getItem('minhaLista');
-  lista.innerHTML = listaSalva;
-}
 
-listaFoiSalva();
+  // Apagar todos os ítens da lista
+  function apagaTudo() {
+    while (lista.firstChild) {
+      lista.removeChild(lista.firstChild);
+    }
+  }
+  const btnApagaTudo = document.querySelector('#apaga-tudo');
+  btnApagaTudo.addEventListener('click', apagaTudo);
+
+
+  // Remover os ítens finalizados
+  function removerFinalizados() {
+    const itens = document.querySelectorAll('.completed');
+    if (itens) {
+      for (let index = 0; index < itens.length; index += 1) {
+        lista.removeChild(itens[index]);
+      }
+    }
+  }
+  const btnApagaFinalizados = document.querySelector('#remover-finalizados');
+  btnApagaFinalizados.addEventListener('click', removerFinalizados);
+
+  /********************************************** */
+
+  // Salvar os dados no local storage
+  function salvarDados() {
+    window.localStorage.setItem('minhaLista', lista.innerHTML);
+  }
+  const btnSalvar = document.querySelector('#salvar-tarefas');
+  btnSalvar.addEventListener('click', salvarDados);
+
+  // Recuperar dados do local Stage
+  function listaFoiSalva() {
+    const listaSalva = window.localStorage.getItem('minhaLista');
+    lista.innerHTML = listaSalva;
+  }
+
+  listaFoiSalva();
+
+
+  // Remover item selecionado
+  function removerSelecionado() {
+    const itemSelecionado = document.querySelector('.selected');
+    if (itemSelecionado) {
+      itemSelecionado.parentNode.removeChild(itemSelecionado);
+    }
+  }
+  const btnSelecao = document.querySelector('#remover-selecionado');
+  btnSelecao.addEventListener('click', removerSelecionado);
 
   // for (let index = 0; index < itens.length; index += 1) {
   //   dados[index] = { item: itens[index], itemTexto: itens[index].innerHTML };
@@ -114,7 +125,7 @@ listaFoiSalva();
 //   selecao.previousSibling.innerHTML = selecao.innerHTML;
 
 //   selecao.innerHTML = temp;
- 
+
 // }
 // const btnMoveCima = document.querySelector('#mover-cima');
 // btnMoveCima.addEventListener('click', moverAcima);
