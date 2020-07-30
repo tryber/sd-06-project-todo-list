04ccdd844d23898c1fc0c9c2681e3f670deecf97
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-30 15:33:10 -0300

    ajustes JS requisito 8

--- script.js
+++ script.js
@@ -11,31 +11,24 @@ function createTask() {
   listOl.appendChild(newItem);
   newItem.innerHTML = input.value;
   newItem.className = 'tarefa';
-  newItem.addEventListener('click', changeColor);
+  newItem.addEventListener('click', changeColor, unselect);
   newItem.addEventListener('dblclick', scratchItem);
   input.value = '';
 }
 
-// Torna cinza itens clicados
+// Altera o background para cinza quando clicado e branco quando outro item é clicado
 function changeColor() {
-  const item = event.target
-  item.style.backgroundColor = 'rgb(128, 128, 128)'
+  const oldItem = event.target
+  oldItem.style.backgroundColor = 'rgb(128, 128, 128)'
+  oldItem.classList.add('clicado');
 }
 
-// De-selecionar itens quando um item é selecionado  
-// function changeColor() {
-//   const createdItem = document.getElementsByTagName('li');
-//   for(let i = 0 ; i < createdItem.length ; i += 1) {
-//     createdItem[i].addEventListener('click', function() {
-//       createdItem[i].style.backgroundColor = 'rgb(128, 128, 128)';
-//       for(let x = 0 ; x < createdItem.length ; x += 1) {
-//         if (x !== i) {
-//           createdItem[x].style.backgroundColor = 'transparent';
-//         }
-//         }
-//     });
-//   }
-// }
+document.querySelectorAll('.tarefa').forEach(unselect)
+  function unselect() {
+    document.querySelectorAll('.clicado').style.backgroundColor = 'white'
+    oldItem.classList.remove('clicado');
+  }
+
 
 // Torna riscado itens com double click
 function scratchItem() {
@@ -55,3 +48,8 @@ buttonEraseFinished.addEventListener('click', eraseFinished);
 function eraseFinished() {
   document.querySelectorAll('.completed').forEach(e => e.remove());
 }
+
+//Salvar itens
+let tarefas = document.querySelectorAll('.tarefa')= JSON.stringify(tarefas)
+console.log(tarefas)
+
