8fb705824194178041197c188adc29e255459788
Micaeli Teixeira <luime6@gmail.com>
2020-07-29 21:45:10 -0300

    alterações codeclimate

--- script.js
+++ script.js
@@ -1,53 +1,46 @@
 
 window.onload = function () {
-
   /* parte da criação do botão */
   const bottonList = document.getElementById('criar-tarefa');
-
   /* parte do botão funcionar*/
-  bottonList.addEventListener('click', function (){
-    let texto = document.getElementById('texto-tarefa').value;
+  bottonList.addEventListener('click', function () {
+    const texto = document.getElementById('texto-tarefa').value;
     const list = document.getElementById('lista-tarefas');
     const listOl = document.createElement('li');
     listOl.innerHTML = texto;
     list.appendChild(listOl);
-
-    /*o que for relacionado a lista ol tem que sair daqui, pois é aqui que é criada*/
-   /* parte da alteração de cor */
-    listOl.addEventListener('click',backColor);
-
+    /* o que for relacionado a lista ol tem que sair daqui, pois é aqui que é criada */
+    /* parte da alteração de cor */
+    listOl.addEventListener('click', backColor);
     /* duplo click */
-    listOl.addEventListener('dblclick', function() {
-    listOl.classList.toggle('completed');
-    })
-
+    listOl.addEventListener('dblclick', function () {
+      listOl.classList.toggle('completed');
+    });
     /* botão apagar tudo*/
     const bottonListDelete = document.getElementById('apaga-tudo');
     bottonListDelete.addEventListener('click', deleteList);
   });
-
   /* fazendo a parte do reset da imput*/
-  function resetTexto () {
+  function resetTexto() {
     document.querySelector('#texto-tarefa').value = '';
   }
   const text = document.querySelector('#criar-tarefa');
   text.addEventListener('click', resetTexto);
-
   /* mudando a cor de fundo*/
-  function backColor () {
-  let listOl = document.querySelectorAll('li');
-  listOl = event.target;
-  let bgColor = document.querySelector('.bgColor');
-  if (bgColor) {
+  function backColor() {
+    let listOl = document.querySelectorAll('li');
+    listOl = event.target;
+    let bgColor = document.querySelector('.bgColor');
+    if (bgColor) {
     bgColor.classList.remove('bgColor');
   }
     listOl.classList.add('bgColor');
-}
+  }
   /* botão apaga tudo*/
   function deleteList() {
-  let list =  document.getElementById('lista-tarefas');
-  while (list.firstChild) {
-  list.removeChild(list.firstChild);
+    const list = document.getElementById('lista-tarefas');
+    while (list.firstChild) {
+    list.removeChild(list.firstChild);
   }
   }
-}
+};
