94ed2217ea3598eae69a3c652784844e2debd641
ricknois <rickssti@outlook.com>
2020-07-30 00:23:53 -0300

    ...

--- script.js
+++ script.js
@@ -38,45 +38,46 @@ window.onload = function () {
   listElement.addEventListener('click', function () {
     console.log(document.getElementsByTagName('li').length);
     for (const j of document.getElementsByTagName('li')) {
-        if (j.classList.contains('selected')) {
+      if (j.classList.contains('selected')) {
         j.classList.remove('selected');
         console.log(j);
-      } 
+      }
     }
     event.target.classList.add('selected');
   })
 
 
-  
 
-  ereseSel.addEventListener('click', function() {
+
+  ereseSel.addEventListener('click', function () {
     let pos = todos.indexOf(document.querySelector('.selected').innerHTML)
     todos.splice(pos, 1);
     showTodos();
   })
-  
+
   listElement.addEventListener('dblclick', function (event) {
-      for(const i of document.getElementsByTagName('li')) {
-        if (event.target.classList.contains('completed')) {
-            event.target.classList.remove('completed');
-        }
-        else {
-            event.target.classList.add('completed')
-        } 
+    for (const i of document.getElementsByTagName('li')) {
+      if (event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+        event.target.style.textDecoration = '';
+      } else {
+        event.target.classList.add('completed')
+        event.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
       }
-     
+    }
+
   })
-  
-  ereseFin.addEventListener('click', function(){
-    console.log(document.getElementsByClassName('completed'))
+
+  ereseFin.addEventListener('click', function () {
+
     let colecao = document.getElementsByClassName('completed')
-    for (let i = 0; i < colecao.length; i+=1) {
-        console.log(colecao.item(i).innerHTML)
-        for (const key in todos) {
-            if(colecao.item(i).innerHTML === todos[key]){
-                todos.splice(key, 1)
-            }
-        }  
+    for (let i = 0; i < colecao.length; i += 1) {
+      console.log(colecao.item(i).innerHTML)
+      for (const key in todos) {
+        if (colecao.item(i).innerHTML === todos[key]) {
+          todos.splice(key, 1)
+        }
+      }
     }
     showTodos();
   })
