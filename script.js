3c8a1142d3d52c6f8126185dc00be594ae3e25e0
Renan Cunha <renan2610@gmail.com>
2020-07-30 00:02:55 -0300

    adicionando botões para remover todas as tarefas e remover apenas as completadas

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const addTask = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
+const clearButton = document.querySelector('#apaga-tudo');
+const removeCompletedButton = document.querySelector('#remover-finalizados')
 
 function addTaskToList() {
   const inputTask = document.querySelector('#texto-tarefa');
@@ -20,24 +22,41 @@ function changeTaskToGrey(event) {
 }
 
 function isClassCompleted(classList) {
-  for (let index in classList) {
+  for (const index in classList) {
     if (classList[index] === 'completed') {
       return true;
     }
-    return false;
   }
+  return false;
 }
 
 function crossOutItem(event) {
-  let classList = event.target.classList;
-  let isCompleted = isClassCompleted(classList);
+  const classList = event.target.classList;
+  const isCompleted = isClassCompleted(classList);
   if (isCompleted) {
     classList.remove('completed');
-    return
-  } 
+    return;
+  }
   classList.add('completed');
+}
+
+function cleanTaskList() {
+  while (taskList.firstElementChild !== null) {
+    taskList.removeChild(taskList.firstElementChild);
+  }
+}
+
+function removeCompletedTasks() {
+  const completedTasks = document.querySelectorAll('.completed');
+  if (completedTasks.length !== 0) {
+    for (let index = 0; index < completedTasks.length; index += 1) {
+      taskList.removeChild(completedTasks[index]);
+    }
+  }
 }
 
 addTask.addEventListener('click', addTaskToList);
 taskList.addEventListener('click', changeTaskToGrey);
 taskList.addEventListener('dblclick', crossOutItem);
+clearButton.addEventListener('click', cleanTaskList);
+removeCompletedButton.addEventListener('click', removeCompletedTasks);
