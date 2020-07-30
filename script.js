a5df818c72c97e108112757ac187fa3145e70c55
kissyla <kissylamottapereira@gmail.com>
2020-07-30 17:32:10 -0300

    apagar input quando lista for criada

--- script.js
+++ script.js
@@ -13,8 +13,25 @@ function createToDo(event){
     line.innerText = inputText;
     //adicionar texto de inputText no elemento li criado
     document.getElementById('lista-tarefas').appendChild(line);
+
+    // apagar texto quando ele passa pra li
+    document.getElementById('texto-tarefa').value='';
     
    }
- 
+   //6 - Os itens da lista de tarefas devem ser ordenados por ordem de criação.
+   //As seguintes verificações serão feitas:
+   //Três itens serão criados na lista e será checado se eles estão ordenados por ordem de criação - ou seja, primeiro o primeiro item criado, depois o segundo, e assim por diante. 
+
+   //7 - Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128).
+  //As seguintes verificações serão feitas:
+  //Será verificado que, ao se carregar a página, os itens da lista não tem o estilo CSS background-color: rgb(128, 128, 128)
+  //Será verificado que, ao se clicar em um item da lista, ele passa a ter o estilo CSS background-color: rgb(128, 128, 128)
+  function changeBackgroundColor(event){
+   document.getElementsByTagName('li').style.backgroundColor = "green"
+  }
+
+   //document.getElementsByTagName('li').appendChild.addEventListener('click', changeBackgroundColor);
+
+
     document.getElementById('criar-tarefa').addEventListener('click', createToDo);
 }
