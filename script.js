fc6121751ebf80c8cf0ee0626fd406e5344b9e52
Alessandra <itsalematos@hotmail.com>
2020-07-30 16:42:16 -0400

    Ajustando codeclimate

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-var button = document.querySelector('#criar-tarefa');
-var list = document.querySelector('#lista-tarefas')
-
-button.addEventListener('click', insertTask);
+let button = document.querySelector('#criar-tarefa');
+let list = document.querySelector('#lista-tarefas');
 
 function insertTask() {
-    let task =  document.querySelector('#texto-tarefa').value;
-    let btn = document.createElement('li');
-    // btn.className = 'item';
-    btn.innerHTML = task;
-    list.appendChild(btn);
+  let task = document.querySelector('#texto-tarefa').value;
+  const btn = document.createElement('li');
+  // btn.className = 'item';
+  btn.innerHTML = task;
+  list.appendChild(btn);
 
-    let cleanInput = document.getElementById("texto-tarefa").value = "";
+  const cleanInput = document.getElementById('texto-tarefa').value = "";
 }
+button.addEventListener ('click', insertTask);
 
-list.addEventListener('click', function(event) {
-    let oldDiv = document.querySelector('.selected');  
-    if (oldDiv !== null){
-      oldDiv.classList.remove('selected');
-    }
-
-    let item = event.target; //quem recebe o click
-    item.classList.add('selected') 
-    console.log(item);
+list.addEventListener ('click', function(event) {
+  const oldDiv = document.querySelector('.selected');  
+  if (oldDiv !== null) {
+    oldDiv.classList.remove('selected');
+  }
+  const item = event.target; // quem recebe o click
+  item.classList.add('selected');
+  console.log(item);
 });
 
-list.addEventListener('dblclick', function(event){
-  let completed = event.target;
-  
-  if(completed.classList.contains('completed')) {
+list.addEventListener ('dblclick', function(event){
+  const completed = event.target;
+  if (completed.classList.contains('completed')) {
     completed.classList.remove('completed');
-    console.log(completed);
   } else {
     completed.classList.add('completed');
-    console.log(completed);
   }
 })
 
