b8cfbad72a7d025329279d9dbe2f927285eabf07
kissyla <kissylamottapereira@gmail.com>
2020-07-30 18:52:59 -0300

    botão apaga tudo

--- script.js
+++ script.js
@@ -15,23 +15,36 @@ function createToDo(event){
     document.getElementById('lista-tarefas').appendChild(line);
 
     // apagar texto quando ele passa pra li
-    document.getElementById('texto-tarefa').value='';
+    document.getElementById('texto-tarefa').value='';    
     
    }
-   //6 - Os itens da lista de tarefas devem ser ordenados por ordem de criação.
-   //As seguintes verificações serão feitas:
-   //Três itens serão criados na lista e será checado se eles estão ordenados por ordem de criação - ou seja, primeiro o primeiro item criado, depois o segundo, e assim por diante. 
-
-   //7 - Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128).
-  //As seguintes verificações serão feitas:
-  //Será verificado que, ao se carregar a página, os itens da lista não tem o estilo CSS background-color: rgb(128, 128, 128)
-  //Será verificado que, ao se clicar em um item da lista, ele passa a ter o estilo CSS background-color: rgb(128, 128, 128)
-  function changeBackgroundColor(event){
-   document.getElementsByTagName('li').style.backgroundColor = "green"
+   
+   //req 7 - mudar cor de fundo - não funciona
+  //function changeBackgroundColor(event){
+   //document.getElementsByTagName('li').style.backgroundColor = "rgb(128, 128, 128)";
+  //}
+  //document.getElementsByTagName('li').addEventListener('click', changeBackgroundColor);
+  //req 9
+  function completedItem(event){
+// elemento doubleclick assume a classe completed
+  event.target.classList.add('completed');
   }
+  document.getElementById('lista-tarefas').addEventListener('dblclick', completedItem);
 
-   //document.getElementsByTagName('li').appendChild.addEventListener('click', changeBackgroundColor);
+  //req 10 botao apaga td -não funciona
+  function clearAll(event){ 
+    //teste se lista foi criada, .children
+    //for pra passr em todo mundo
+    let children = document.getElementById('lista-tarefas').children
 
-
+           while(children.length !== 0){
+            
+            document.getElementById('lista-tarefas').removeChild(children[0]);
+            
+        }  
+    
+  }
+   
+    document.getElementById('apaga-tudo').addEventListener('click', clearAll);
     document.getElementById('criar-tarefa').addEventListener('click', createToDo);
 }
