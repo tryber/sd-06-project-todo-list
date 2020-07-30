9495c813a23b6358fc6b0126cd571e6cf9672d27
Victor Bergamini <vbergadev@gmail.com>
2020-07-30 11:46:49 -0300

    nova tarefa

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function inicia() {
+ document.getElementById("criar-tarefa").addEventListener("click", function(){
+   let lista=document.getElementById("lista-tarefas");
+   let texto=document.getElementById("texto-tarefa");
+   let li=document.createElement("li");
+   li.textContent=texto.value;
+   lista.appendChild(li);  
+   texto.value="";
+   texto.focus()
+ });
+}
+window.addEventListener("load", inicia);
