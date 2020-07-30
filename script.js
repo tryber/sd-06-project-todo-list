32598f9227296d4cd2b52363a875ae75d29675bd
Stefano Branz <stefano.branz@gmail.com>
2020-07-30 17:28:56 -0300

    Ninth requirement done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Create task list
 document.getElementById('criar-tarefa').onclick = function() {
   let inputedTask = document.getElementById('texto-tarefa').value;
   let taskItem = document.createElement('li');
@@ -7,6 +8,7 @@ document.getElementById('criar-tarefa').onclick = function() {
   document.getElementById('lista-tarefas').appendChild(taskItem);
   document.getElementById('texto-tarefa').value = ""; 
 }
+// Change list background on click
 document.getElementById('lista-tarefas').onclick = function() {
   if (document.querySelector(".selected") != null) {
     document.querySelector(".selected").style.backgroundColor = "";
@@ -14,4 +16,12 @@ document.getElementById('lista-tarefas').onclick = function() {
   }
   event.target.style.backgroundColor = "rgb(128,128,128)";
   event.target.classList.add("selected");
+}
+// Risk list item on double click
+document.getElementById('lista-tarefas').ondblclick = function() {
+  if (event.target.className === 'selected') {
+    event.target.classList.add('completed');
+  } else {
+    event.target.classList.remove('completed');
+  }
 }
