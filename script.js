cb73319f2be7309f9c3d76ac79a299a46b17f93d
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-07-30 16:48:28 -0300

    Verificando o requisito 13

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
 const tarefa = document.querySelector('#texto-tarefa');
 const tarefaBtn = document.querySelector('#criar-tarefa');
-const lista = document.querySelector('#lista-tarefas');
+const list = document.querySelector('#lista-tarefas');
 const listItens = document.getElementsByTagName('li');
 const removeBtn = document.querySelector('#apaga-tudo');
 const removeComplete = document.querySelector('#remover-finalizados');
 const save = document.querySelector('#salvar-tarefas');
+const btnUp = document.querySelector('#mover-cima');
+const btnDown = document.querySelector('#mover-baixo');
 
 window.onload = function () {
-  lista.innerHTML = localStorage.getItem('list');
-}
+  list.innerHTML = localStorage.getItem('list');
+};
 
 tarefaBtn.addEventListener('click', function () {
   const element = document.createElement('li');
   element.innerText = tarefa.value;
-  lista.appendChild(element);
+  list.appendChild(element);
   tarefa.value = '';
 });
 
-lista.addEventListener('click', function (event) {
+list.addEventListener('click', function (event) {
   for (let i = 0; i < listItens.length; i += 1) {
     if (listItens[i].classList.contains('selected') === true) {
       listItens[i].classList.remove('selected');
     }
   }
   event.target.classList.add('selected');
-  if (lista.classList.contains('selected') === true) {
-    lista.classList.remove('selected');
+  if (list.classList.contains('selected') === true) {
+    list.classList.remove('selected');
   }
 });
 
-lista.addEventListener('dblclick', function (event) {
+list.addEventListener('dblclick', function (event) {
   event.target.classList.toggle('completed');
-  if (lista.classList.contains('completed') === true) {
-    lista.classList.remove('completed');
+  if (list.classList.contains('completed') === true) {
+    list.classList.remove('completed');
   }
 });
 
 removeBtn.addEventListener('click', function () {
-  lista.innerHTML = '';
+  list.innerHTML = '';
 });
 
 removeComplete.addEventListener('click', function () {
   for (let i = 0; i < listItens.length; i += 1) {
     for (let j = 0; j < listItens.length; j += 1) {
       if (listItens[i] !== undefined && listItens[i].classList.contains('completed') === true) {
-        lista.removeChild(listItens[i]);
+        list.removeChild(listItens[i]);
       }
     }
   }
@@ -54,5 +56,24 @@ save.addEventListener('click', function () {
   if (localStorage.getItem('list') !== null) {
     window.localStorage.clear();
   }
-  window.localStorage.setItem('list', lista.innerHTML);
+  window.localStorage.setItem('list', list.innerHTML);
+});
+
+btnUp.addEventListener('click', function () {
+  for (let i = 0; i < listItens.length; i += 1) {
+    if (listItens[i].classList.contains('selected') === true) {
+      if (i === 0) {
+        break;
+      }
+      list.insertBefore(listItens[i], list.childNodes[i - 1]);
+    }
+  }
+});
+
+btnDown.addEventListener('click', function () {
+  for (let i = listItens.length - 2; i >= 0; i -= 1) {
+    if (listItens[i].classList.contains('selected') === true) {
+      list.insertBefore(list.children[i + 1], list.children[i]);
+    }
+  }
 });
