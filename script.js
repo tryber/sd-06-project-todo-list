dfa23b846cc5d102bfad597edd65d8e0d4b41151
Stefano Branz <stefano.branz@gmail.com>
2020-07-30 17:09:26 -0300

    Seventh/Eighth requirement done

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.getElementById('criar-tarefa').onclick = function() {
   let inputedTask = document.getElementById('texto-tarefa').value;
   let taskItem = document.createElement('li');
-  // taskItem.textContent = inputedTask.value;
   let inputedTaskNode = document.createTextNode(inputedTask);
   taskItem.appendChild(inputedTaskNode);
 
   document.getElementById('lista-tarefas').appendChild(taskItem);
   document.getElementById('texto-tarefa').value = ""; 
+}
+document.getElementById('lista-tarefas').onclick = function() {
+  if (document.querySelector(".selected") != null) {
+    document.querySelector(".selected").style.backgroundColor = "";
+    document.querySelector(".selected").classList.remove("selected");
+  }
+  event.target.style.backgroundColor = "rgb(128,128,128)";
+  event.target.classList.add("selected");
 }
