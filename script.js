87912d6fce5ca1359ec92b67e0c15c2bcdbaf7b8
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-30 17:21:30 -0300

    Ao clicar em um item da lista, altere a cor de fundo do item para cinza

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+//  Criando variaveis
 const criarTarefa = document.getElementById('criar-tarefa');
 const textoTarefa = document.getElementById('texto-tarefa');
-const listaTarefas = document.getElementById('lista-tarefas');
+const itemlistaTarefas = document.getElementById('lista-tarefas');
+//  Função para quando digitar uma tarefa ela ser adciona a lista clicando no botão indicado
 function listarItens() {
   const itemLista = document.createElement('li');
   itemLista.innerHTML = textoTarefa.value;
-  listaTarefas.appendChild(itemLista);
+  itemlistaTarefas.appendChild(itemLista);
   textoTarefa.value = '';
 }
 
 criarTarefa.addEventListener('click', listarItens);
+
+//  Função para Ao clicar em um item da lista, altere a cor de fundo do item para cinza
+function alterarCorDoItemParaCinza(event) {
+  event.target.style.backgroundColor = 'rgb(128 , 128 , 128)';
+}
+
+itemlistaTarefas.addEventListener('click', alterarCorDoItemParaCinza);
