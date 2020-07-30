797641ca148179b5af76bde25c2c5f1117c92f8c
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-07-30 00:17:54 -0300

    Add event to change background of item list

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const taskText = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
 // Cria a lista de tarefas ao clicar;
-btnAddTasks.addEventListener('click', generateList);
 function generateList() {
   const taskItem = document.createElement('li');
   taskList.appendChild(taskItem);
   taskItem.innerHTML = taskText.value;
   taskText.value = '';
 }
+btnAddTasks.addEventListener('click', generateList);
+
+taskList.addEventListener('click', function (event) {
+  const taskItem = event.target;
+  taskItem.style.backgroundColor = 'rgb(128 , 128 , 128)';
+})
