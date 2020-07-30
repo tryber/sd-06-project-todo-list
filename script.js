0bd77d74653d416d067dad222b3477a34e786bc7
CristianoNunes <cristianonunes99@gmail.com>
2020-07-30 17:30:27 -0300

    Requisito 12 Concluido Salvar Tarefas

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+//Busca no LocalStorage se tem lista salva
+window.onload = function() {
+  if (typeof Storage !== 'undefined') {
+    if (localStorage.tasks !== undefined) {
+      getTasksLocalStorage();
+    }
+  } else {
+    document.write('Sem suporte para Web Storage.');
+  }
+};
+
+//Recupera as tarefas salvas
+function getTasksLocalStorage() {
+  let objTasks = localStorage.getItem('tasks').split(',');
+  let objClass = localStorage.getItem('class').split(',');
+  for (let i = 0; i < objTasks.length; i += 1) {
+    let listTask = document.querySelector('#lista-tarefas');
+    let itemList = document.createElement('li');
+    itemList.innerHTML = objTasks[i];
+    itemList.className = objClass[i];
+    itemList.id ='lista';
+    listTask.appendChild(itemList);
+    itemList.addEventListener('click', selectItem);
+    itemList.addEventListener('dblclick', itemCompleted);
+  }
+}
+
 //Adiciona tarefa a lista
 function addTask() {
   let task = document.createElement('li');
@@ -19,6 +46,7 @@ function selectItem(event) {
   selectItemList.classList.add('selected');
   selectItemList.addEventListener('dblclick', itemCompleted);
 }
+
 //Seta item como completado
 function itemCompleted(event) {
   let completedItem = event.target;
@@ -28,6 +56,7 @@ function itemCompleted(event) {
     completedItem.classList.add('completed');
   }
 }
+
 //Limpa toda a lista de tarefas
 function clearList() {
   let list = document.querySelector('#lista-tarefas');
@@ -35,10 +64,29 @@ function clearList() {
     list.removeChild(list.firstChild);
   }
 }
+
 //Exclui todas as tarefas completadas
 function clearTasksCompleted() {
   let itemsCompleteds = document.getElementsByClassName('completed');
   while (itemsCompleteds.length > 0) {
     itemsCompleteds[0].remove();
+  }
+}
+
+//Salva a lista de tarefas
+function saveList() {
+  if (typeof Storage !== 'undefined') {
+    localStorage.clear();
+    const tasks = document.querySelector('#lista-tarefas');
+    const objTasks = [];
+    const objClass = [];
+    for (let i = 0; i < tasks.children.length; i += 1) {
+      objTasks[i] = tasks.children[i].innerText;
+      objClass[i] = tasks.children[i].classList;
+    }
+    localStorage.setItem('tasks', objTasks.join());
+    localStorage.setItem('class', objClass.join());
+  } else {
+    document.write('Sem suporte para Web Storage.');
   }
 }
