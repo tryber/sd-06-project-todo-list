2308a632f9a03f30749d1301e4af5cc61bda8499
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-07-31 12:58:11 -0300

    Resolução Requisito 12

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.onload = function() {
   
-  let myList = document.getElementById("lista-tarefas");
+  const myList = document.getElementById("lista-tarefas");
+  // Verifica se a lista foi salva
+  if (sessionStorage.getItem("myListSaved") == "true") {
+    console.log("entrou");
+    myList.innerHTML = sessionStorage.getItem("myList");
+    sessionStorage.setItem("myListSaved", false);
+  }
 
   // Tratamento do botão para incluir o item
   let btnAdd = document.getElementById("criar-tarefa");
@@ -27,6 +33,13 @@ window.onload = function() {
     myListCompleted.forEach(function(item) {
       item.parentNode.removeChild(item);
     });
+  });
+
+  // Tratamento do botão para Salvar Lista
+  let btnSave = document.getElementById("salvar-tarefas");
+  btnSave.addEventListener("click", function () {
+    sessionStorage.setItem("myList", myList.innerHTML);
+    sessionStorage.setItem("myListSaved", "true");
   });
 
   // Função para adicionar um item na lista
@@ -64,66 +77,5 @@ window.onload = function() {
       newSelected.classList.add("completed");
     }
   }
-
-
-  // function clearColorSelect() {
-  //   for(var i = 0; i < myList.length; i += 1) {
-  //     let myItem = myList[i];
-  //     myList.style.backgroundColor = "rgb(64 , 119 , 100)";
-  //   }
-  // }
-
-  // let btnClear = document.getElementById("clear-board");
-  // btnClear.addEventListener('click', function() {
-  //   for(var i = 0; i < Pixels.length; i += 1) {
-  //     let myItemPixel = Pixels[i];
-  //     myItemPixel.style.backgroundColor = "rgb(255 , 255 , 255)";
-  //   }
-  // });
-
-
-  // let myColor02 = document.getElementById("color02");
-  // myColor02.style.backgroundColor = "rgb(155 , 50 , 8)";  
-	// myColor02.addEventListener("click", function () {
-	// 	chageSelected(myColor02);
-	// });
-
-  // let myColor03 = document.getElementById("color03");
-  // myColor03.style.backgroundColor = "rgb(198 , 168 , 108)";  
-	// myColor03.addEventListener("click", function () {
-	// 	chageSelected(myColor03);
-	// });	
-
-  // let myColor04 = document.getElementById("color04");
-  // myColor04.style.backgroundColor = "rgb(45 , 75 , 139)";
-	// myColor04.addEventListener("click", function () {
-	// 	chageSelected(myColor04);
-  // });	
-  
-  // let colorSelected = "rgb(0 , 0 , 0)";
-  
-  // function chageSelected(newSelected){
-  //   let oldSelected = document.querySelector(".selected");
-  //   oldSelected.classList.remove("selected");
-  //   newSelected.classList.add("selected");
-
-  //   colorSelected = newSelected.style.backgroundColor;
-  // }
-
-  // let Pixels = document.getElementsByClassName("pixel");
-  // for(var i = 0; i < Pixels.length; i += 1) {
-  //   let myItemPixel = Pixels[i];
-  //   myItemPixel.addEventListener('click', function() {
-  //     myItemPixel.style.backgroundColor = colorSelected;
-  //   });
-  // }
-
-  // let btnClear = document.getElementById("clear-board");
-  // btnClear.addEventListener('click', function() {
-  //   for(var i = 0; i < Pixels.length; i += 1) {
-  //     let myItemPixel = Pixels[i];
-  //     myItemPixel.style.backgroundColor = "rgb(255 , 255 , 255)";
-  //   }
-  // });
 
 }
