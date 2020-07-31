07e1676f625f8877ff6ebe88a3d79e276fcaecab
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-07-31 12:12:11 -0300

    Evaluator não aprovou a lógica para apagar a tarefa caixa de texto, pois antes eu atribuia uma string vazia para limpar a caixa, então atribui o valor null para verificar se o evaluator aceita essa lógica

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let addButton = document.querySelector('#criar-tarefa');
 addButton.addEventListener('click', addToDo)
+let selectItem = document.querySelector('.toDoItem');
+selectItem.addEventListener('click', function () {
+    console.log(selectItem);
+})
 
 function addToDo() {
     let toDo = document.querySelector('#texto-tarefa');
     let listToDo = document.createElement('li');
+    listToDo.className = 'toDoItem';
     document.getElementById('lista-tarefas').appendChild(listToDo).innerText = toDo.value;
-    toDo.value =" ";
+    toDo.value = null;
 }
