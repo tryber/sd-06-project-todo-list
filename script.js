4229b48463aacd78f1e851961e628820aaae2173
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-30 22:54:53 -0300

    selecionando lista

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-let tagList = document.querySelector("#lista-tarefas");
+window.onload = function() {
 
+let tagList = document.querySelector("#lista-tarefas");
 let list = document.getElementById("lista-tarefas");
 let buttonCreateList = document.querySelector("#criar-tarefa");
 let inputText = document.querySelector("#texto-tarefa");
 
+// funcao que cria atag <li> dentro da tag <ol> e recebe o valor digitado no input.
 buttonCreateList.addEventListener("click", function createList() {
     let tagLi = document.createElement("li");
     list.appendChild(tagLi);
+    //console.log(list)
     tagLi.innerHTML = inputText.value;
+    //console.log(tagLi)
     inputText.value = "";
-    console.log(list);
+    //console.log(tagLi);
 });
 
+function selectedList(event) {
+    let tagLiColor = document.getElementsByTagName("li");
+
+        if (event.target !== document.querySelector("#selected"));
+            event.target.classList.add("selected");
+            //console.log(event.target);
+}
+tagList.addEventListener("click", selectedList);
 
 
-// function createList() {
-//     let textList = document.querySelector("input").value;
-//     lista = lista +"<li>"+textList+"</li>";
-//     document.getElementById("lista-tarefas").innerHTML = lista;
-// } 
-// buttonCreateList.addEventListener("click", createList);    
 
-// buttonCreateList.addEventListener("click", function createList() {
-    
-// });
+}
+
+//for (let index = 0; index < tagLiColor.length; index += 1);
