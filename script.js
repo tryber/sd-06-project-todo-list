59410d490e392692b63440e2ab93e08ebb9f914c
Anderson R <misterregisslv@gmail.com>
2020-07-31 01:32:32 -0300

    tarefa 9  finalizada

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 window.onload = function () {
-  //evento para criar lista
-  const botao = document.querySelector('#criar-tarefa');
-  botao.addEventListener('click', function () {
+  // evento para criar lista
+  const button = document.querySelector('#criar-tarefa');
+  button.addEventListener('click', function () {
     const areaTexto = document.querySelector('#texto-tarefa').value;
     const tagli = document.createElement('li');
     tagli.innerHTML = areaTexto;
     document.querySelector('#lista-tarefas').appendChild(tagli);
   });
-  //evento para limpar input
-  botao.addEventListener('click', function () {
+  // evento para limpar input
+  button.addEventListener('click', function () {
     document.getElementById('texto-tarefa').value = '';
   });
   oneLineSelection();
   addItembackgroundColor();
   scratch();
+  eraseAll();
 };
 
-function addItembackgroundColor () {
+function addItembackgroundColor() {
   const changeColor = document.querySelector('#lista-tarefas');
   changeColor.addEventListener('click', function (event) {
     const backgoundItem = event.target;
@@ -24,28 +25,32 @@ function addItembackgroundColor () {
   });
 }
 
-function oneLineSelection () {
+function oneLineSelection() {
   const lineWhite = document.querySelector('#lista-tarefas');
-  lineWhite.addEventListener('click', function(event){
+  lineWhite.addEventListener('click', function () {
     const count = document.querySelector('#lista-tarefas').children;
     const color = document.getElementsByTagName('li');
-    //tirar duvida do index do for
-    for(index = 0; index < count.length; index += 1){
+    for (let index = 0; index < count.length; index += 1) {
       color[index].style.backgroundColor = 'white';
     }
   });
 }
 
-function scratch () {
+function scratch() {
   const eventSelect = document.querySelector('#lista-tarefas');
   eventSelect.addEventListener('dblclick', function (event) {
-    let scratchItem = event.target;
-    console.log(scratchItem.classList.contains('completed'));
-    if (scratchItem.classList.contains('completed') == false){
-      scratchItem.classList.add('completed'); 
-    }else{
+    const scratchItem = event.target;
+    if (scratchItem.classList.contains('completed') === false) {
+      scratchItem.classList.add('completed');
+    } else {
       scratchItem.classList.remove('completed');
     }
-     
+  });
+}
+
+function eraseAll() {
+  const eventSelect = document.querySelector('#apaga-tudo');
+  eventSelect.addEventListener('click', function () {
+    document.querySelector('#lista-tarefas').innerHTML = '';
   });
 }
