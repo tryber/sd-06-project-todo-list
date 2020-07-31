bb158c4f92a363b8a7053e42349776ab3c014d2b
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-30 21:09:36 -0300

    add function createList

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const button = document.querySelector("#criar-tarefa");
-const input = document.querySelector("#texto-tarefa");
-const tagList = document.querySelector("#lista-tarefas");
+let tagList = document.querySelector("#lista-tarefas");
 
-function creatList() {
-    let textList = document.querySelector("input").value;
-    let lista  = document.getElementById("lista-tarefas").innerHTML;
-    lista = lista +"<li>"+textList+"</li>";
-    document.getElementById("lista-tarefas").innerHTML = lista;
-    document.querySelector('input').value = '';
-}
+let list = document.getElementById("lista-tarefas");
+let buttonCreateList = document.querySelector("#criar-tarefa");
+let inputText = document.querySelector("#texto-tarefa");
+
+buttonCreateList.addEventListener("click", function createList() {
+    let tagLi = document.createElement("li");
+    list.appendChild(tagLi);
+    tagLi.innerHTML = inputText.value;
+    inputText.value = "";
+    console.log(list);
+});
 
 
-button.addEventListener("click", creatList); 
+
+// function createList() {
+//     let textList = document.querySelector("input").value;
+//     lista = lista +"<li>"+textList+"</li>";
+//     document.getElementById("lista-tarefas").innerHTML = lista;
+// } 
+// buttonCreateList.addEventListener("click", createList);    
+
+// buttonCreateList.addEventListener("click", function createList() {
+    
+// });
