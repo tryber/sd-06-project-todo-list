5e0fdf259481055cf9822e7bb80b6d099713a0f8
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-01 12:06:30 -0300

    corrigindo erros do codeclimate e refazer dinovo o ex-8

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-
 //  Criando as variaveis do html
-  const criarTarefa = document.getElementById('criar-tarefa');
-  const textoTarefa = document.getElementById('texto-tarefa');
-  const itemlistaTarefas = document.getElementById('lista-tarefas');
+const criarTarefa = document.getElementById('criar-tarefa');
+const textoTarefa = document.getElementById('texto-tarefa');
+const itemlistaTarefas = document.getElementById('lista-tarefas');
 //  Função para quando digitar uma tarefa ela ser adciona a lista clicando no botão indicado
-  function listarItens() {
-    const itemLista = document.createElement('li');
-    itemLista.innerHTML = textoTarefa.value;
-    itemlistaTarefas.appendChild(itemLista);
-    itemLista.className = 'tarefa';
-    itemLista.addEventListener('click');
-    textoTarefa.value = '';
-  }
+function listarItens() {
+  const itemLista = document.createElement('li');
+  itemLista.innerHTML = textoTarefa.value;
+  itemlistaTarefas.appendChild(itemLista);
+  itemLista.className = 'tarefa';
+  itemLista.addEventListener('click', alterarCorDoItemParaCinza);
+  textoTarefa.value = '';
+}
 
-  criarTarefa.addEventListener('click', listarItens);
+criarTarefa.addEventListener('click', listarItens);
 
-//  Ao clicar no item da lista, altere a cor para cinza e quando clicar em outro substituir
-  let ultimoSelecionado = itemlistaTarefas;
-  itemlistaTarefas.addEventListener('click', function (event) {
-    const selecionado1 = event.target;
-    if (selecionado1.id !== 'lista-tarefas') {
-      ultimoSelecionado.style.backgroundColor = '';
-      selecionado1.style.backgroundColor = 'rgb(128,128,128)';
-      ultimoSelecionado = selecionado1;
-    }
-  });
+//  Função para Ao clicar em um item da lista, altere a cor de fundo do item para cinza
+function alterarCorDoItemParaCinza(event) {
+  const selecionaItemLista = event.target;
+  selecionaItemLista.style.backgroundColor = 'rgb(128 , 128 , 128)';
+}
 
+itemlistaTarefas.addEventListener('click', alterarCorDoItemParaCinza);
