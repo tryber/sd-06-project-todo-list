7accfc1ebd2aede52b50464816bdedf58e42ff3c
Pedro Duarte <ikbenpedro@gmail.com>
2020-08-01 19:20:04 -0300

    html css js modificados - melhorando css- 90%

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 window.onload = function() {
 
     //salvar tudo
-
     if (localStorage.ListaTarefas !== undefined) {
         document.getElementById('lista-tarefas').innerHTML = localStorage.ListaTarefas;
     }
 
-    let btnSave = document.getElementById("salvar-tarefas")
+    let btnSave = document.getElementById("salvar-tarefas");
 
     function saveList() {
         const listSaved = document.getElementById("lista-tarefas").innerHTML
         localStorage.setItem('ListaTarefas', listSaved);
     }
-    btnSave.addEventListener("click", saveList)
+    btnSave.addEventListener("click", saveList);
 
     // apagar tudo
-
     let eraseAll = document.getElementById("apaga-tudo");
 
     function eraseAllTasks() {
@@ -24,7 +22,7 @@ window.onload = function() {
             taskList.removeChild(taskList.firstChild);
         }
     }
-    eraseAll.addEventListener("click", eraseAllTasks)
+    eraseAll.addEventListener("click", eraseAllTasks);
 
     // add classe completed
     var listOfTasks = document.querySelector('ol');
@@ -44,7 +42,7 @@ window.onload = function() {
     }, false);
 
     //remover selecionada
-    var rmvSelected = document.getElementById("remover-selecionado")
+    var rmvSelected = document.getElementById("remover-selecionado");
 
     function rmvSelection() {
         var list = document.getElementById("lista-tarefas");
@@ -54,7 +52,7 @@ window.onload = function() {
             }
         }
     }
-    rmvSelected.addEventListener('click', rmvSelection)
+    rmvSelected.addEventListener('click', rmvSelection);
 
     //remover completo
     var rmvCompleted = document.getElementById("remover-finalizados")
@@ -67,10 +65,10 @@ window.onload = function() {
             }
         }
     }
-    rmvCompleted.addEventListener('click', rmvFinished)
+    rmvCompleted.addEventListener('click', rmvFinished);
 
     //criar novo item
-    const addTask = document.getElementById("criar-tarefa")
+    const addTask = document.getElementById("criar-tarefa");
 
     function newTask() {
         var li = document.createElement("li");
@@ -85,7 +83,7 @@ window.onload = function() {
         document.getElementById("texto-tarefa").value = "";
 
     }
-    addTask.addEventListener("click", newTask)
+    addTask.addEventListener("click", newTask);
 
 
 
