fbb97c8c0afbfc6b3305291f5c4272eb22fe26c1
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-01 10:37:25 -0300

    ex-7/8-Ao clicar no item da lista, altere a cor para cinza e quando clicar em outro substituir

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+window.onload = function () {
 //  Criando as variaveis do html
-const criarTarefa = document.getElementById('criar-tarefa');
-const textoTarefa = document.getElementById('texto-tarefa');
-const itemlistaTarefas = document.getElementById('lista-tarefas');
+  const criarTarefa = document.getElementById('criar-tarefa');
+  const textoTarefa = document.getElementById('texto-tarefa');
+  const itemlistaTarefas = document.getElementById('lista-tarefas');
 //  Função para quando digitar uma tarefa ela ser adciona a lista clicando no botão indicado
-function listarItens() {
-  const itemLista = document.createElement('li');
-  itemLista.innerHTML = textoTarefa.value;
-  itemlistaTarefas.appendChild(itemLista);
-  textoTarefa.value = '';
-}
+  function listarItens() {
+    const itemLista = document.createElement('li');
+    itemLista.innerHTML = textoTarefa.value;
+    itemlistaTarefas.appendChild(itemLista);
+    itemLista.className = 'tarefa';
+    itemLista.addEventListener('click');
+    textoTarefa.value = '';
+  }
 
-criarTarefa.addEventListener('click', listarItens);
+  criarTarefa.addEventListener('click', listarItens);
 
-//  Função para Ao clicar em um item da lista, altere a cor de fundo do item para cinza
-function alterarCorDoItemParaCinza(event) {
-  event.target.style.backgroundColor = 'rgb(128 , 128 , 128)';
-}
-
-itemlistaTarefas.addEventListener('click', alterarCorDoItemParaCinza);
+//  Ao clicar no item da lista, altere a cor para cinza e quando clicar em outro substituir
+  let ultimoSelecionado = itemlistaTarefas;
+  itemlistaTarefas.addEventListener('click', function (event) { 
+    const selecionado1 = event.target;
+    if (selecionado1.id !== 'lista-tarefas') {
+      ultimoSelecionado.style.backgroundColor = '';
+      selecionado1.style.backgroundColor = 'rgb(128,128,128)';
+      ultimoSelecionado = selecionado1;
+    }
+  });
+};
