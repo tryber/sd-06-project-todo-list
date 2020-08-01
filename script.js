82f565e8862866a1f690ed290a4cf774590b2d70
Veruska Bittencourt <sousaveruska@gmail.com>
2020-08-01 18:40:34 -0300

    correcoes html/css/js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const newTarefa=document.getElementById('criar-tarefa');
 const inicioList=localStorage.getItem('saveList');
-const listaTarefa=document.getElementById('lista-tarefas')
+const listaTarefa=document.getElementById('lista-tarefas');
+const moveUp=document.getElementById("mover-cima");
+const moveDown=document.getElementById("mover-baixo");
+const deleteTudo=document.getElementById("apaga-tudo");
+const removeFinal=document.getElementById("remover-finalizados");
+const removeSelect=document.getElementById("remover-selecionado");
+const saveTarefa=document.getElementById("salvar-tarefas");
+
 
 if (inicioList){
     const lista=document.getElementById('lista-tarefas');
@@ -29,4 +36,31 @@ listaTarefa.addEventListener('click', function(event){
     }
     event.target.style.backgroundColor='rgb(128,128,128)';
 });
+moveUp.addEventListener('click', function () {
+    const aux = document.getElementsByTagName('li');
+    for (let i = 0; i < aux.length; i ++) {
+        let aux2;
+        if (aux[i].style.backgroundColor === 'rgb(128, 128, 128)' && i > 0) {
+            aux2 = aux[ i-1 ].innerText;
+            aux[ i-1 ].innerText = aux[i].innerText;
+            aux[i].innerText = aux2;
+            aux[ i-1 ].style.backgroundColor = 'rgb(128, 128, 128)';
+            aux[i].style.backgroundColor = 'transparent';
+        }
+    }
+});
 
+moveDown.addEventListener('click', function () {
+    const aux = document.getElementsByTagName('li');
+    for (let i = 0; i < aux.length; i ++) {
+        let aux2 = document.getElementsByTagName('li');
+        if (aux[i].style.backgroundColor === 'rgb(128, 128, 128)' && i <= aux.length) {
+            aux2 = aux[ i+1 ].innerText;
+            aux[ i+1 ].innerText = aux[i].innerText;
+            aux[i].innerText = aux2;
+            aux[ i+1 ].style.backgroundColor = 'rgb(128, 128, 128)';
+            aux[i].style.backgroundColor = 'transparent';
+            break;
+        }
+    }
+});
