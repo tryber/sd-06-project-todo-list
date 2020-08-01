821585d640d17a9e28e19eb61233b40a539da12b
LeonardoCavachini <cavachini240@gmail.com>
2020-07-31 21:10:02 -0300

    projeto 100%??

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 let btn = document.getElementById("criar-tarefa");
-let ol = document.getElementById("lista-tarefas");
-
-
-
+let ol = document.getElementById('lista-tarefas');
+// Adicionando tarefas na lista ordenada.
 btn.addEventListener('click', function() {
-    
-    let lista = document.getElementById("texto-tarefas").value;
+    let lista = document.getElementById("texto-tarefa").value;
     let li = document.createElement('li');
+    li.classList.add('conteudo');
+    li.id = 'task'
     li.textContent = lista;
     document.getElementById("lista-tarefas").appendChild(li);
-    document.getElementById('texto-tarefas').value = '';
+    document.getElementById('texto-tarefa').value = '';
+    if(li){
+        li.addEventListener('click',function(event){
+           let old = document.getElementsByClassName('selected')[0];
+           let select = event.target;
+           if(old) {
+            old.classList.remove('selected');
+            select.classList.add('selected');
+           } else {
+            select.classList.add('selected');
+           }
+        });
+    }
+});
+/*function change(event) {
+    let old = document.getElementsByClassName('selected');
+    let secondclick = event.target;
+    old.classList.remove('selected');
+    secondclick.classList.add('selected');
+};
+*/
 
-})
+
