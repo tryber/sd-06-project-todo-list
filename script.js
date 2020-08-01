416d4b4b525a235530ab25c055c00c5df477e71a
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-01 12:44:24 -0300

    exercicio 7/8 Ao clicar no item da lista, altere a cor para cinza e quando clicar em outro substitui,corrigindo erros do codeclimate

--- script.js
+++ script.js
@@ -8,16 +8,25 @@ function listarItens() {
   itemLista.innerHTML = textoTarefa.value;
   itemlistaTarefas.appendChild(itemLista);
   itemLista.className = 'tarefa';
-  itemLista.addEventListener('click', alterarCorDoItemParaCinza);
   textoTarefa.value = '';
 }
 
 criarTarefa.addEventListener('click', listarItens);
 
-//  Função para Ao clicar em um item da lista, altere a cor de fundo do item para cinza
-function alterarCorDoItemParaCinza(event) {
-  const selecionaItemLista = event.target;
-  selecionaItemLista.style.backgroundColor = 'rgb(128 , 128 , 128)';
-}
+// Ao clicar em um item da lista, altere a cor de fundo para cinza ao clicar em outro mude
+// function alterarCorDoItemParaCinza(event) {
+//   const selecionaItemLista = event.target;
+//   selecionaItemLista.style.backgroundColor = 'rgb(128 , 128 , 128)';
+// }
 
-itemlistaTarefas.addEventListener('click', alterarCorDoItemParaCinza);
+// itemlistaTarefas.addEventListener('click', alterarCorDoItemParaCinza);
+
+//  Ao clicar no item da lista, altere a cor para cinza e quando clicar em outro substituir
+itemlistaTarefas.addEventListener('click', function (event) {
+  const removeClass = document.querySelector('.selected');
+  if (removeClass !== null) {
+    removeClass.classList.remove('selected');
+  }
+  const selectClass = event.target;
+  selectClass.className += ' selected';
+});
