2f6c036592af9a0e452ae69463e7c031d104d25a
Havyner Caetano <hrcgeo@gmail.com>
2020-08-02 16:02:49 -0300

    Refactor single and double click events, remove unnecessary variables

--- script.js
+++ script.js
@@ -2,53 +2,40 @@ const addTaskButton = document.querySelector('button:nth-of-type(1)');
 const taskList = document.querySelector('ol');
 const taskText = document.querySelector('input');
 const clearAllButton = document.querySelector('#apaga-tudo');
-let currentLi;
-const tasks = [];
 
-function addClickListener() {
-  for (let i = 0; i < tasks.length; i += 1) {
-    tasks[i].addEventListener('click', function (event) {
-      if (!currentLi) {
-        event.target.className = 'selected';
-        currentLi = tasks[i];
-      } else {
-        currentLi.classList.remove('selected');
-        currentLi = event.target;
-        currentLi.className = 'selected';
-      }
-    })
-  }
+function addClickListener(newElement) {
+  newElement.addEventListener('click', function (event) {
+    const previouslySelected = document.querySelector(".selected");
+    const currentSelected = event.target;
+
+    if (previouslySelected) {
+      previouslySelected.classList.remove("selected");
+    }
+
+    currentSelected.classList.add("selected");
+  })
 }
 
-function addDoubleClickListener() {
-  for (let i = 0; i < tasks.length; i += 1) {
-    tasks[i].addEventListener('dblclick', function (event) {
-      const li = event.target;
-      const classNames = [];
+function addDoubleClickListener(newElement) {
+  newElement.addEventListener('dblclick', function (event) {
+    const clicked = event.target;
 
-      for (let i = 0; i < event.target.classList.length; i++) {
-        classNames.push(event.target.classList[i]);
-      }
-
-      if (!classNames.includes('completed')) {
-        li.classList.add('completed');
-      } else {
-        li.classList.remove('completed');
-      }
-    })
-  }
+    if (clicked.classList.contains('completed')) {
+      clicked.classList.remove('completed');
+    } else {
+      clicked.classList.add('completed');
+    }
+  })
 }
 
 addTaskButton.addEventListener('click', function () {
   const li = document.createElement('li');
-
   li.innerText = taskText.value;
   taskText.value = '';
   taskList.appendChild(li);
-  tasks.push(li);
 
-  addClickListener();
-  addDoubleClickListener();
+  addClickListener(taskList.lastChild);
+  addDoubleClickListener(taskList.lastChild);
 })
 
 clearAllButton.addEventListener('click', function () {
