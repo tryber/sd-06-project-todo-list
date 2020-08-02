bf38a812dbc282f68806c0f37cfbb9842bf70413
Zeca Costa <jccostaso@gmail.com>
2020-08-02 11:42:44 -0300

    Atualizaçao para inclusao requisito #5

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 window.onload = function () {
-
+  insertButtonEvent();
 };
+
+// INSERT ITENS IN LIST
+function clearText() {
+  document.querySelector('#texto-tarefa').value = '';
+}
+
+function insertButtonEvent() {
+  const addBtn = document.querySelector('#criar-tarefa');
+  addBtn.addEventListener('click', addItensOfList);
+}
+
+function addItensOfList(event) {
+  const list = document.querySelector('#lista-tarefas');
+  const itemElement = createItensOfList();
+  list.appendChild(itemElement);
+}
+
+function createItensOfList() {
+  const itemElement = document.createElement('li');
+  const textValue = document.querySelector('#texto-tarefa');
+  itemElement.innerText = textValue.value;
+  clearText();
+  // itemElement.id = 'meme-image';
+  // ImageElement.className = 'meme-image';
+  return itemElement;
+}
+
+// function handleAddButtonEvent(event) {
+    // const previousSelectedItem = document.querySelector('.selected');
+    // const currentSelectedItem = event.target;
+    // previousSelectedItem.classList.remove('selected');
+    // currentSelectedItem.classList.add('selected');
+    // selectedColor = nowSelectedDiv.style.backgroundColor;
+//}
