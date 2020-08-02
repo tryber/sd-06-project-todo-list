dc5710a409b0aa34495e03a02aaf14b82bb5e511
Zeca Costa <jccostaso@gmail.com>
2020-08-02 19:19:40 -0300

    Atualizaçao para inclusao dos requisitos obrigatorios restantes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.onload = function () {
   insertButtonEvent();
   clearAllButtonEvent();
+  oneClickListEvent();
+  doubleClickListEvent();
+  clearCompletedButtonEvent();
 };
 
 // INSERT ITEMS IN TO-DO LIST
@@ -24,31 +27,79 @@ function createItemsOfList() {
   const textValue = document.querySelector('#texto-tarefa');
   itemElement.innerText = textValue.value;
   clearText();
-  // itemElement.id = 'meme-image';
-  // ImageElement.className = 'meme-image';
   return itemElement;
 }
 
-// CLEAR ALL ITENS IN TO-DO LIST
+// CLEAR ALL ITEMS IN TO-DO LIST
 function clearAllButtonEvent() {
   const clearBtn = document.querySelector('#apaga-tudo');
   clearBtn.addEventListener('click', ClearAllItems);
-} 
+}
 
-function ClearAllItems(){
-  const list = document.querySelector('#lista-tarefas');
+function ClearAllItems() {
+  const allList = document.querySelector('#lista-tarefas');
 
-  while (list.hasChildNodes()) {  
-    list.removeChild(list.firstChild);
+  while (allList.hasChildNodes()) {
+    allList.removeChild(allList.firstChild);
   }
 }
 
-// function handleAddButtonEvent(event) {
-    // const previousSelectedItem = document.querySelector('.selected');
-    // const currentSelectedItem = event.target;
-    // previousSelectedItem.classList.remove('selected');
-    // currentSelectedItem.classList.add('selected');
-    // selectedColor = nowSelectedDiv.style.backgroundColor;
-// }
+// SELECTION ('click') ITEMS IN TO-DO LIST
+function oneClickListEvent () {
+  const listWrap = document.querySelector('#lista-tarefas');
+  listWrap.addEventListener('click', handleOneClickListEvent);
+}
 
-//<button onclick="ClearAllItens()" id='apaga-tudo'>Limpar Lista</button>
+function handleOneClickListEvent(event) {
+  const previousSelectedItem = document.querySelector('.selected');
+  
+  if (previousSelectedItem !== null) {
+    previousSelectedItem.classList.remove('selected');
+  }
+  
+  const selectedItem = event.target;
+
+  if (selectedItem.classList.contains('selected')) {
+    selectedItem.classList.remove('selected');
+  } else {
+    selectedItem.classList.add('selected');
+  }
+}
+// SELECTION ('double click') ITEMS IN TO-DO LIST
+function doubleClickListEvent () {
+  const listWrap = document.querySelector('#lista-tarefas');
+  listWrap.addEventListener('dblclick', handleDoubleClickListEvent);
+}
+
+function handleDoubleClickListEvent(event) {
+  const previousSelectedItem = document.querySelector('.selected');
+  
+  if (previousSelectedItem !== null) {
+    previousSelectedItem.classList.remove('selected');
+  }
+  
+  const selectedItem = event.target;
+
+  if (selectedItem.classList.contains('completed')) {
+    selectedItem.classList.remove('completed');
+  } else {
+    selectedItem.classList.add('completed');
+  }
+}
+
+// CLEAR COMPLETED ITEMS IN TO-DO LIST
+function clearCompletedButtonEvent() {
+  const completedBtn = document.querySelector('#remover-finalizados');
+  completedBtn.addEventListener('click', ClearCompletedItems);
+}
+
+function ClearCompletedItems() {
+  const allList = document.querySelector('#lista-tarefas');
+  let listSize = allList.childNodes.length -1;
+
+  for (listSize; listSize >=0; listSize -= 1) {
+    if (allList.childNodes[listSize].classList.contains('completed')) {
+      allList.removeChild(allList.childNodes[listSize]);
+    }
+  }
+}
