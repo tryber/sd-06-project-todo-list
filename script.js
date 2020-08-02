18a6e7a79730774a27f7da6fe9818c84e6632dba
LeandroFeitozaGnu <leandro.feitoza8018@gmail.com>
2020-08-02 15:41:49 -0300

    Feito a funçao que cria a classe completed e define a fonte riscada para o evento de Double Click

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Botão Adicionar
-const buttonAdd = document.createElement('button');
-buttonAdd.id = 'criar-tarefa';
-const labelButton = document.createTextNode('Adicionar');
-buttonAdd.appendChild(labelButton);
-document.querySelector('.entrada-dados').appendChild(buttonAdd);
 
 // Cria a lista ordenada
 const taskList = document.createElement('ol');
@@ -16,8 +10,55 @@ function generateItemList() {
   itemList.classList = 'lista';
   itemList.appendChild(document.createTextNode(task));
   taskList.appendChild(itemList);
-  const LISTA = document.querySelector('li');
-  LISTA.classList = 'selected';
+  itemList.addEventListener('click', handleListItemClick);
+  itemList.addEventListener('dblclick', handleListItemDoubleClick)
+  itemList.addEventListener('dblclick', tiraORisco)
+ // const LISTA = document.querySelector('li');
+ // LISTA.classList = 'selected';
   document.querySelector('#texto-tarefa').value = '';
 }
+
+// Botão Adicionar
+const buttonAdd = document.createElement('button');
+buttonAdd.id = 'criar-tarefa';
+const labelButton = document.createTextNode('Adicionar');
+buttonAdd.appendChild(labelButton);
+document.querySelector('.entrada-dados').appendChild(buttonAdd);
 buttonAdd.addEventListener('click', generateItemList);
+
+function handleListItemClick(event){
+  clearItemListSelecions();
+  let selectedListItem = event.target;
+  selectedListItem.style.backgroundColor = 'rgb(128 , 128 , 128)';
+}
+
+function handleListItemDoubleClick(event) {
+  let selectedListItem = event.target;
+  if (selectedListItem.classList == 'lista') {
+    selectedListItem.classList = ('completed')
+    selectedListItem.style.textDecoration = 'line-through'
+  } else {
+    selectedListItem.classList = ('lista')
+    selectedListItem.style.textDecoration = 'none'
+  }
+}
+
+function tiraORisco(event) {
+  let selectedListItem = event.target;
+  selectedListItem.textDecoration ='none'
+}
+
+
+function clearItemListSelecions() {
+ /* let itemList = document.querySelectorAll('li')
+    for (index = 0; index <= itemList.length; index ++) {
+      let list = itemList[index]
+      list.style.backgroundColor = 'green'
+    }
+  
+  for (let index in itemList) {
+    let list = itemList[index];
+    list.style.backgroundColor = 'green'
+    console.log(list)
+  }*/
+}
