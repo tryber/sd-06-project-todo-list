fe033c1c58f1f32131f81a3615ecabeb76bbe5f9
William Dieter <williamzdieter@gmail.com>
2020-08-03 19:09:03 -0300

    Teste

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-iuouio
+var ul = document.getElementById("myList");
+var li;
+var itemId;
+var item;
+
+
+addTask = function() {
+    if(document.getElementById("task").value != "") {
+        item = document.getElementById("task");
+        itemId = ul.childElementCount;
+        li = createItemEl(item.value,itemId);
+        li.appendChild(createRemoveTaskButton(itemId));
+        ul.appendChild(li);
+        item.value = ""; 
+    }
+    
+    
+}
+
+removeTask = function(itemId) {
+    for (i = 0; i < ul.children.length; i++) {
+        if (ul.children[i].getAttribute("index") == itemId) {
+            ul.children[i].remove();
+        }
+    }
+
+}
+
+createItemEl = function(itemValue, itemId) {
+    let li = document.createElement("li");
+    li.setAttribute("index", itemId);
+    li.appendChild(document.createTextNode(itemValue));
+    return li;
+
+}
+
+createRemoveTaskButton = function(itemId) {
+    let button = document.createElement("button");
+    button.setAttribute("onclick", "removeTask("+itemId+")");
+    button.innerHTML ="X";
+    return button;
+}
