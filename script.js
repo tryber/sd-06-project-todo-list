1b858f5e18a3f25524f4a9da5d72cec6cb147e3c
RenatoCostaDev <renatotrybe@gmail.com>
2020-08-03 20:30:49 -0300

    11- colocando a função que marca a tarefa

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ window.onload = function () {
 
   const botaoCriarTarefa = document.getElementById('criar-tarefa');
   const botaoApagarTudo = document.getElementById('apaga-tudo');
-  
+
   botaoCriarTarefa.addEventListener('click', addTask);
   botaoApagarTudo.addEventListener('click', removeTask);
-  
-}
 
+}
+//adiciona tarefa
 addTask = () => {
   const ol = document.getElementById('lista-tarefas');
   event.preventDefault();
@@ -26,8 +26,22 @@ createItemEl = (itemValue) => {
   return li;
 }
 
+//apaga todas as tarefas
 removeTask = () => {
   if (document.getElementById('lista-tarefas').innerHTML.value != ''){    
     document.querySelectorAll('.task').forEach(e => e.remove());
   }
 }
+
+//marca 1 elemento
+const taskGray = document.querySelector('#lista-tarefas');
+taskGray.addEventListener('click', function (event) {
+  const oldSelected = document.querySelector('.selected');
+  const selected = event.target;
+  if (oldSelected) {
+    oldSelected.classList.remove('selected');
+    selected.classList.add('selected');
+  } else {
+    selected.classList.add('selected');
+  }
+});
