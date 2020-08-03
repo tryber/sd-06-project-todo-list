28cbc80e835e242986975d341657b9b33176d51a
Marcelo Pessini <marceloppessini@gmail.com>
2020-08-03 10:31:53 -0300

    Agora classe completed acompanha elementos que se movem na lista

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function addClickChangeColor(){
   })
 })
 }
-
 function clearBgFromList(){
   document.querySelectorAll('li').forEach(item => {
    item.style.backgroundColor = "";
@@ -34,7 +33,6 @@ function addRiskOnDblClick(){
   })
 })
 }
-
 function removeRiskOnDblClick(){
   document.querySelectorAll('.completed').forEach(item => {
     item.addEventListener('dblclick', event => {
@@ -54,16 +52,59 @@ document.getElementById("remover-finalizados").addEventListener('click', functio
  })
 })
 
-document.getElementById('teste').addEventListener('click', function(){
-  for (i = 0; i <= 5; i += 1){
-  let liTag = document.createElement('li');
-  liTag.innerHTML = 'TESTE';
-  oList.appendChild(liTag);
-  }
-  addClickChangeColor();
-  addRiskOnDblClick();
-})
-
 document.getElementById('remover-selecionado').addEventListener('click', function(){
   document.querySelector('.selected').remove();
 })
+
+function findIndex(itemToMoveUp){
+  let itemListCheck = document.querySelectorAll('li');
+  for(i = 0; i < itemListCheck.length; i += 1){
+    if(itemToMoveUp === itemListCheck[i].innerHTML){
+      return i;
+    }
+  }
+}
+
+document.getElementById('mover-cima').addEventListener('click', function(){
+  if(document.querySelector('.selected') !== null){
+  let itemToMoveUp = document.querySelector('.selected').innerHTML;
+  let indexSelected = findIndex(itemToMoveUp);
+  if(indexSelected > 0){
+  let previousItem = document.querySelectorAll('li')[indexSelected-1].innerHTML;
+  document.querySelectorAll('li')[indexSelected-1].innerHTML = itemToMoveUp;
+  document.querySelectorAll('li')[indexSelected].innerHTML = previousItem;
+  document.querySelectorAll('li')[indexSelected-1].classList.add('selected');
+  document.querySelectorAll('li')[indexSelected].classList.remove('selected');
+  document.querySelector('.selected').style.backgroundColor = 'rgb(128, 128, 128)';
+  document.querySelectorAll('li')[indexSelected].style.backgroundColor = "";
+  if(document.querySelectorAll('li')[indexSelected].classList.contains("completed") && document.querySelectorAll('li')[indexSelected-1].classList.contains("completed") == false){
+    document.querySelectorAll('li')[indexSelected-1].classList.add('completed');
+    document.querySelectorAll('li')[indexSelected].classList.remove('completed');
+    } else if(document.querySelectorAll('li')[indexSelected].classList.contains("completed")== false && document.querySelectorAll('li')[indexSelected-1].classList.contains("completed")){
+      document.querySelectorAll('li')[indexSelected].classList.add('completed');
+      document.querySelectorAll('li')[indexSelected-1].classList.remove('completed');
+    }
+  }} 
+}) 
+
+document.getElementById('mover-baixo').addEventListener('click', function(){
+  if(document.querySelector('.selected') !== null){
+  let itemToMoveUp = document.querySelector('.selected').innerHTML;
+  let indexSelected = findIndex(itemToMoveUp);
+    if(indexSelected < document.querySelectorAll('li').length-1){
+    let previousItem = document.querySelectorAll('li')[indexSelected+1].innerHTML;
+    document.querySelectorAll('li')[indexSelected+1].innerHTML = itemToMoveUp;
+    document.querySelectorAll('li')[indexSelected].innerHTML = previousItem;
+    document.querySelectorAll('li')[indexSelected+1].classList.add('selected');
+    document.querySelectorAll('li')[indexSelected].classList.remove('selected');
+    document.querySelector('.selected').style.backgroundColor = 'rgb(128, 128, 128)';
+    document.querySelectorAll('li')[indexSelected].style.backgroundColor = "";
+    if(document.querySelectorAll('li')[indexSelected].classList.contains("completed") && document.querySelectorAll('li')[indexSelected+1].classList.contains("completed") == false){
+    document.querySelectorAll('li')[indexSelected+1].classList.add('completed');
+    document.querySelectorAll('li')[indexSelected].classList.remove('completed');
+    } else if(document.querySelectorAll('li')[indexSelected].classList.contains("completed")== false && document.querySelectorAll('li')[indexSelected+1].classList.contains("completed")){
+      document.querySelectorAll('li')[indexSelected].classList.add('completed');
+      document.querySelectorAll('li')[indexSelected+1].classList.remove('completed');
+    }
+  }}  
+}) 
