f29cbc3a968422250fbc41be7ddec4a6d794824e
DekoPossas <andrepossaster@gmail.com>
2020-08-03 00:07:44 -0300

    adicionei funcao de apagar o testo depois de adicionar o item, junto com uma caixa de span caso o campo de imput estiver vazio

--- script.js
+++ script.js
@@ -3,13 +3,23 @@
 const buttonNewTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const inputText = document.querySelector('#texto-tarefa');
+const buttonClean = document.getElementById('apaga-tudo');
 
 buttonNewTask.addEventListener('click', function(){
   const textList = inputText.value;
   const itenList = document.createElement('li')
   itenList.innerText = textList;
 
-  taskList.appendChild(itenList);
+  if(textList === '') {
+    const span =document.createElement('span');
+    span.innerText = 'Digite Titulo para sua tarefa.';
+    span.className = 'span';
+    document.querySelector('#tarefas').appendChild(span);
+  } else {
+    taskList.appendChild(itenList);
+    inputText.value = '';
+  }
+
 })
 
 function changeCollorOnSelect(select) {
@@ -33,3 +43,13 @@ function completTask(complete) {
 taskList.addEventListener('dblclick', function(event) {
   completTask(event.target);
 });
+
+
+
+buttonClean.addEventListener('click', function() {
+  let itens = document.getElementsByTagName('li');
+
+  for (let c = 0; c < itens.length; c += 1){
+    taskList.removeChild(itens[c]);
+  }
+})
