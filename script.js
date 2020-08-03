67e9e6b2a683b59f21e760d0800f32826473904c
DekoPossas <andrepossaster@gmail.com>
2020-08-02 22:16:37 -0300

    depois de muito sofrimento, consegui criar a funcao que diciona um texto do input dentro da innertext da lista do item

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // Variáveis
 
-const buttonNewTask = document.getElementById("criar-tarefa");
-const taskList = document.getElementById("lista-tarefas");
+const buttonNewTask = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+const inputText = document.querySelector('#texto-tarefa');
 
 buttonNewTask.addEventListener('click', function(){
-  let itemLista = document.createElement('li')
-  taskList.appendChild(itemLista);
+  const textList = inputText.value;
+  const itenList = document.createElement('li')
+  itenList.innerText = textList;
+
+  taskList.appendChild(itenList);
+
 })
