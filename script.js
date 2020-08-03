211188371399c924cc3622ee12993eae42018896
Alana Monteiro <lanaah.15@gmail.com>
2020-08-03 16:02:19 -0300

    classe finalizados

--- script.js
+++ script.js
@@ -9,9 +9,22 @@ function newTask() {
     let container = document.getElementsByTagName('li');
     let lastItem = container[container.length - 1];
 
-//Inserir na lista o novo item
+    //inserir classe no novo item
+    lastItem.classList.add('a-fazer');
+
+    //inserir evento no novo item
+    lastItem.setAttribute('ondblclick', 'finalizar()');
+
+    //Inserir na lista o novo item
     lastItem.innerText = caixaTarefa;
 
     //Limpar o campo
     document.getElementById('texto-tarefa').value = '';
+}
+
+
+function finalizar() {
+    let itemClicado = event.target;
+    itemClicado.classList.remove('a-fazer');
+    itemClicado.classList.add('finalizado');
 }
