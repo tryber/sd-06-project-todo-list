f18185cc281ed273299d426572a832896cbdd04b
Victor Bergamini <vbergadev@gmail.com>
2020-08-03 09:33:52 -0300

    APAGAR TUDO E CSS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let lista=document.getElementById("lista-tarefas");
+/*let lista=document.getElementById("lista-tarefas");
 function inicia() {
  document.getElementById("criar-tarefa").addEventListener("click", function(){
    let texto=document.getElementById("texto-tarefa");
@@ -16,4 +16,53 @@ function apagarTudo(){
   
   
 }
-window.addEventListener("load", inicia);
+window.addEventListener("load", inicia);*/
+const orderedList = document.getElementById('lista-tarefas');
+const typedtext = document.getElementById('texto-tarefa');
+const addTaskButton = document.getElementById('criar-tarefa');
+const clearButton = document.getElementById('apaga-tudo');
+const removeCompletedItens = document.getElementById('remover-finalizados');
+
+
+//Create the list itens, and add them in the orderedList when the button is clicked.
+function addItens() {
+    if (typedtext.value !== '') {
+        const listItens = document.createElement('li');
+        listItens.className = 'iten';
+        listItens.innerHTML = typedtext.value;
+        orderedList.appendChild(listItens);
+        typedtext.value = '';
+    }
+}
+addTaskButton.addEventListener('click', addItens);
+
+//Change the background color of a selected iten when clicked
+orderedList.addEventListener('click', function() {
+  if (document.querySelector('.selected-task') !== null) {
+    document.querySelector('.selected-task').style.backgroundColor = 'white';
+    document.querySelector('.selected-task').classList.remove('selected-task');
+  }
+  if (event.target.className === 'iten') {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+    event.target.classList.add('selected-task');
+  }
+  event.target.style.backgroundColor = 'rgb(128,128,128)'
+  event.target.classList.add('selected-task');
+})
+
+//Risk the completed itens
+orderedList.addEventListener('dblclick', function() {
+  if (event.target.className === 'iten selected-task') {
+    event.target.classList.add('completed');
+  } else {
+    event.target.classList.remove('completed')
+  }
+})
+
+  //Clear the list content
+  clearButton.addEventListener('click', function() {
+    let displayList = document.querySelectorAll('.iten');
+    for (let index = 0; index < displayList.length; index += 1) {
+      orderedList.removeChild(displayList[index]);
+    }
+  })
