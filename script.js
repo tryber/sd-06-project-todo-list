c24164e4563b40677524cfe43bed07b326c73837
Danilo <dansantosalfa@gmail.com>
2020-08-04 20:26:58 -0300

    Adicionando terminando os botões

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const insert = document.querySelector('#criar-tarefa')
-const remove = document.querySelector('#remover-finalizados')
-const clear = document.querySelector('#apaga-tudo')
+const insert = document.querySelector('#criar-tarefa');
+const remove = document.querySelector('#remover-finalizados');
+const clear = document.querySelector('#apaga-tudo');
 
 function addItemList(){
     if (document.querySelector('#texto-tarefa').value !== "") {
-    let newItem = document.createElement('li')
-    newItem.innerHTML = document.querySelector('#texto-tarefa').value
-    document.querySelector('#lista-tarefas').appendChild(newItem)
-    document.querySelector('#texto-tarefa').value = ''    
+    let newItem = document.createElement('li');
+    newItem.innerHTML = document.querySelector('#texto-tarefa').value;
+    document.querySelector('#lista-tarefas').appendChild(newItem);
+    document.querySelector('#texto-tarefa').value = '';    
     }else{
-        console.log("vazio")
+        console.log("vazio");
     }
 }
 
-function handleSelectedItem(event) {
+function itemSelected(event) {
     if (document.querySelector('.selected') !== null) {
       document.querySelector('.selected').classList.remove('selected');
       event.target.classList.add('selected');
@@ -21,4 +21,31 @@ function handleSelectedItem(event) {
       event.target.classList.add('selected');
     }
   }
+
+function taskTop(event) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  }
   
+remove.addEventListener('click', function () {
+    for (let i = 0; i < document.querySelectorAll('.completed').length; i += 1) {
+      if (document.querySelectorAll('.completed')[i].classList.contains('completed')) {
+        document.querySelectorAll('.completed')[i].remove();
+      }
+    }
+  });
+
+clear.addEventListener('click', function () {
+    for (let i = 0; i < document.querySelectorAll('#lista-tarefas').length; i += 1) {
+      document.querySelectorAll('#lista-tarefas')[i].remove();
+    }
+  });
+
+insert.addEventListener('click', addItemList);  
+
+document.querySelector('#lista-tarefas').addEventListener('click', itemSelected);
+
+document.querySelector('#lista-tarefas').addEventListener('dblclick', taskTop);
