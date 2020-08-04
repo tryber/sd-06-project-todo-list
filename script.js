b3a4901452008df0a14e6df626d91a79262bebb4
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2020-08-04 13:54:16 -0300

    Versao 1 - itens 7-13

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-var button = document.querySelector('#criar-tarefa');
-var list = document.querySelector('#lista-tarefas')
-
-button.addEventListener('click', insertTask);
-
+let button = document.querySelector('#criar-tarefa');
+let list = document.querySelector('#lista-tarefas');
 function insertTask() {
-    let task =  document.querySelector('#texto-tarefa').value;
-    let button = document.createElement('li');
-    button.innerHTML = task;
-    list.appendChild(button);
+  let lista = document.querySelector('#texto-tarefa').value;
+  const task = document.createElement('li');
+  task.className = 'item';
+  task.innerHTML = lista;
+  list.appendChild(task);
+  const cleanInput = document.getElementById('texto-tarefa').value = "";
+}
 
-    let cleanInput = document.getElementById("texto-tarefa").value = "";
-} 
+button.addEventListener ('click', insertTask);
+list.addEventListener ('click', function(event) {
+  const oldDiv = document.querySelector('.selected');  
+  if (oldDiv !== null) {
+    oldDiv.classList.remove('selected');
+  }
+  const item = event.target; // quem recebe o click
+  item.classList.add('selected');
+  console.log(item);
+});
+
+list.addEventListener ('dblclick', function(event){
+  const completed = event.target;
+  if (completed.classList.contains('completed')) {
+    completed.classList.remove('completed');
+  } else {
+    completed.classList.add('completed');
+  }
+})
+
+let cleanUp = document.querySelector('#apaga-tudo');
+cleanUp.addEventListener ('click', function(){
+  let items = document.querySelectorAll('.item');
+  for (let i = 0; i < items.length; i += 1) {
+    list.lastChild.remove();
+  }
+})
+
+let clean = document.querySelector('#remover-finalizados');
+clean.addEventListener('click', function(){
+  let items = document.querySelectorAll('.item');
+  for (let i = 0; i < items.length; i += 1){
+    if (items[i].classList.contains('completed')){
+      list.removeChild(items[i]);
+    }
+  }
+})
