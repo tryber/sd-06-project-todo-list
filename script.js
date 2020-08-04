427e3e9cedbf94c5cfae7347cde145ca554a7c9d
Danilo <dansantosalfa@gmail.com>
2020-08-04 19:50:06 -0300

    Mexendo no buttoens do script

---
+++ script.js
@@ -0,0 +1,7 @@
+const insert = document.querySelector('#criar-tarefa')
+const remove = document.querySelector('#remover-finalizados')
+const clear = document.querySelector('#apaga-tudo')
+insert.addEventListener('click', function () {
+
+    
+})
