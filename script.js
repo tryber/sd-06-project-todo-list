df892dc2198aea00d6f30b6cfc9b07e2ade8e0f9
Danilo <dansantosalfa@gmail.com>
2020-08-04 20:03:59 -0300

    Adicionando funções

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const insert = document.querySelector('#criar-tarefa')
 const remove = document.querySelector('#remover-finalizados')
 const clear = document.querySelector('#apaga-tudo')
-insert.addEventListener('click', function () {
 
-    
-})
+function addItemList(){
+    if (document.querySelector('#texto-tarefa').value !== "") {
+    let newItem = document.createElement('li')
+    newItem.innerHTML = document.querySelector('#texto-tarefa').value
+    document.querySelector('#lista-tarefas').appendChild(newItem)
+    document.querySelector('#texto-tarefa').value = ''    
+    }else{
+        console.log("vazio")
+    }
+}
