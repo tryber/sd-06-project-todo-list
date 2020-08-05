86fdc20aa90ba92cbfcfb8940bbf9a26ee2b7fb4
vania patricia <vapatty@gmail.com>
2020-08-05 20:24:49 -0300

    tentando achar o erro

--- script.js
+++ script.js
@@ -2,47 +2,50 @@ const insert = document.querySelector('#criar-tarefa');
 const remove = document.querySelector('#remover-finalizados');
 const clear = document.querySelector('#apaga-tudo');
 
-
-function addItemlist() {
-  if(document.querySelector('#texto-tarefa').value !== ""){
+function addItemList(){
+    if (document.querySelector('#texto-tarefa').value !== "") {
     let newItem = document.createElement('li');
     newItem.innerHTML = document.querySelector('#texto-tarefa').value;
     document.querySelector('#lista-tarefas').appendChild(newItem);
-    document.querySelector('#texto-tarefas').value='';
-  } else {
-      console.log("vazio");
-  }
+    document.querySelector('#texto-tarefa').value = ' ';
+    }else{
+        console.log('vazio');
+    }
 }
 
 function itemSelected(event) {
-   if (document.querySelector('.selected') !== null) {
-     document.querySelector('.selected').classList.remove('selected');
-     event.target.classList.add('.selected');
-   }
-}
-
-function taskTop(event) {
-  if(event.target.classList.contains('completed')) {
-    event.target.classList.remove('completed');
-  } else {
-    event.target.classList.add('completed');
-  }
-}
-
-remove.addEventListener('click', function () {
-  for (let i = 0; i < document.querySelectorAll('.completed').length; i += 1) {
-    if (document.querySelectorAll('.completed')[i].classList.contains('completed')) {
-      document.querySelectorAll('.completed')[i].remove();
+    if (document.querySelector('.selected') !== null) {
+      document.querySelector('.selected').classList.remove('selected');
+      event.target.classList.add('selected');
+    } else {
+      event.target.classList.add('selected');
     }
   }
-});
+
+function taskTop(event) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  }
+
+remove.addEventListener('click', function () {
+    for (let i = 0; i < document.querySelectorAll('.completed').length; i += 1) {
+      if (document.querySelectorAll('.completed')[i].classList.contains('completed')) {
+        document.querySelectorAll('.completed')[i].remove();
+      }
+    }
+  });
 
 clear.addEventListener('click', function () {
-  for (let i = 0; i < document.querySelectorAll('#lista-tarefas').length; i += 1) {
-    document.querySelectorAll('#lista-tarefas')[i].remove();
-  }
-});
+    for (let i = 0; i < document.querySelectorAll('#lista-tarefas').length; i += 1) {
+      document.querySelectorAll('#lista-tarefas')[i].remove();
+    }
+  });
 
 insert.addEventListener('click', addItemList);
-document.querySelector('#lista-taefas').addEventListener('click', itemSelected);
-document.querySelector('#lista-tarefas').addEventListener('dbclick', taskTop);
+
+document.querySelector('#lista-tarefas').addEventListener('click', itemSelected);
+
+document.querySelector('#lista-tarefas').addEventListener('dblclick', taskTop);
