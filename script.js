10725a999f86da0430630d49f1c091a73ea5b88f
vania patricia <vapatty@gmail.com>
2020-08-06 09:43:38 -0300

    mudança

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const remove = document.querySelector('#remover-finalizados');
 const clear = document.querySelector('#apaga-tudo');
 
 function addItemList() {
-    if (document.querySelector('#texto-tarefa').value !== "") {
+    if (document.querySelector('#texto-tarefa').value !== ' ') {
     let newItem = document.createElement('li');
     newItem.innerHTML = document.querySelector('#texto-tarefa').value;
     document.querySelector('#lista-tarefas').appendChild(newItem);
-    document.querySelector('#texto-tarefa').value = ' ';
+    document.querySelector('#texto-tarefa').value = '';
     } else {
         console.log('vazio');
     }
