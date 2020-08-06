5cde4976fa80d47e49bdf13caa22cbb8eba71e7c
RenatoCostaDev <renatotrybe@gmail.com>
2020-08-06 14:38:46 -0300

    14- refatorando js

--- script.js
+++ script.js
@@ -1,46 +1,104 @@
-window.onload = function () {
+const btnAddTask = document.querySelector('#criar-tarefa');
+const btnEraseAllTask = document.querySelector('#apaga-tudo');
+const btnEraseAllTaskFineshed = document.querySelector('#remover-finalizados');
+const btnSaveAllTasks = document.querySelector('#salvar-tarefas');
+const btnRemoveSelected = document.querySelector('#remover-selecionado');
+const btnSetUp = document.querySelector('#mover-cima');
+const btnSetDown = document.querySelector('#mover-baixo');
 
-  const botaoCriarTarefa = document.getElementById('criar-tarefa');
-  const botaoApagarTudo = document.getElementById('apaga-tudo');
-
-  botaoCriarTarefa.addEventListener('click', addTask);
-  botaoApagarTudo.addEventListener('click', removeTask);
+// Criar 'ol' no html para inserir a lista
+const createList = () => {
+  const taskListDiv = document.querySelector('.taskList');
+  const listElement = document.createElement('ol');
+  listElement.id = 'lista-tarefas';
+  taskListDiv.appendChild(listElement);
 }
-//adiciona tarefa
-addTask = () => {
-  const ol = document.getElementById('lista-tarefas');
-  event.preventDefault();
-  if (document.getElementById('texto-tarefa').value != '') {
-    const item = document.getElementById('texto-tarefa');
-    const li = createItemEl(item.value);
-    ol.appendChild(li);
-    item.value = '';
-  }
-}
+createList();
 
-createItemEl = (itemValue) => {
-  const li = document.createElement('li');
-  li.className = ('task');
-  li.appendChild(document.createTextNode(itemValue));
-  return li;
+// Acrescentado tarefas a lista
+const addTask = () => {
+  const taskList = document.querySelector('#lista-tarefas');
+  const taskTxt = document.querySelector('#texto-tarefa');
+  const itemLi = document.createElement('li');
+  itemLi.innerHTML = taskTxt.value;
+  taskTxt.value = '';
+  taskList.appendChild(itemLi);
 }
+btnAddTask.addEventListener('click', addTask);
 
-//apaga todas as tarefas
-removeTask = () => {
-  if (document.getElementById('lista-tarefas').innerHTML.value != ''){    
-    document.querySelectorAll('.task').forEach(e => e.remove());
-  }
-}
-
-//marca 1 elemento
-const taskGray = document.querySelector('#lista-tarefas');
-taskGray.addEventListener('click', function (event) {
-  const oldSelected = document.querySelector('.selected');
-  const selected = event.target;
-  if (oldSelected) {
-    oldSelected.classList.remove('selected');
-    selected.classList.add('selected');
-  } else {
-    selected.classList.add('selected');
+// Apagando todas as tarefas
+btnEraseAllTask.addEventListener('click', function () {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
   }
 });
+
+// Mudando para cinza a tarefa em um click
+const taskList = document.querySelector('#lista-tarefas');
+taskList.addEventListener('click', function (event) {
+  const ifTaskSelected = document.querySelector('.selected');
+  const newSelected = event.target;
+  if (ifTaskSelected) {
+    ifTaskSelected.classList.remove('selected');
+    newSelected.classList.add('selected');
+  } else {
+    newSelected.classList.add('selected');
+  }
+});
+
+// Riscando tarefa com duplo click
+taskList.addEventListener('dblclick', function () {
+  const listItem = event.target;
+  listItem.classList.toggle('completed');
+});
+
+// Remover todos as tarefas finalizadas
+const removeTaskFineshed = () => {
+  const fineshedTasks = document.querySelectorAll('.completed');
+  if (fineshedTasks) {
+    for (let index = 0; index < fineshedTasks.length; index += 1) {
+      fineshedTasks[index].remove();
+    }
+  }
+};
+btnEraseAllTaskFineshed.addEventListener('click', removeTaskFineshed);
+
+// Salvando tarefas no local storage
+const saveTaskLocalStorage = () => {
+  window.localStorage.setItem('taskList', taskList.innerHTML);
+};
+btnSaveAllTasks.addEventListener('click', saveTaskLocalStorage);
+
+// Recuperando tarefas salvas no local Stage
+function recoverTaskList() {
+  const taskListStorage = window.localStorage.getItem('taskList');
+  taskList.innerHTML = taskListStorage;
+}
+recoverTaskList();
+
+// Remover tarefa selecionada/fundo cinza 
+const removeSelected =  () => {
+  const itemSelected = document.querySelector('.selected');
+  if (itemSelected) {
+    itemSelected.remove();
+  }
+};
+btnRemoveSelected.addEventListener('click', removeSelected);
+
+// Mover para cima e para baixo a tarefa usando setas
+btnSetUp.addEventListener('click', function () {
+  const selected = document.querySelector('.selected');
+  if ((selected) && (selected.previousSibling)) {
+    taskList.insertBefore(selected, selected.previousSibling);
+  }
+});
+btnRemoveSelected.addEventListener('click', removeSelected);
+
+
+btnSetDown.addEventListener('click', function () {
+  const selected = document.querySelector('.selected');
+  if ((selected) && (selected.nextSibling)) {
+    taskList.insertBefore(selected, selected.nextSibling.nextSibling);
+  }
+});
+btnRemoveSelected.addEventListener('click', removeSelected);
