bc51397bc2b267c3891999047fff95feda7e93d2
gussta <saideles.silveira@gmail.com>
2020-08-06 09:34:17 -0300

    js para botao/lista

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const btnCriarTarefa = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
 
+function criarTarefa () {
+  let lista = document.getElementById('texto-tarefa').value;
+  let tarefa = document.createElement('li');
+  listaTarefas.appendChild(tarefa);
+  tarefa.className = 'item';
+  tarefa.innerHTML = lista;  
+  document.getElementById('texto-tarefa').value = "";
+}
+
+btnCriarTarefa.addEventListener ('click', criarTarefa);
