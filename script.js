2ca93f72a38bb09ff20ee4c45b99369634978b36
ilanribeiro <ilanribeiro@gmail.com>
2020-08-06 01:23:37 -0300

    build funtion to select task and other function to remove selected task

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 
 // CONSTANTS
-const taskInput = document.querySelector('#texto-tarefa');
-const addTaskButton = document.querySelector('#criar-tarefa');
-const listItemContainer = document.querySelector('#lista-tarefas');
+const taskInput = document.querySelector('input#texto-tarefa');
+const addTaskButton = document.querySelector('button#criar-tarefa');
+const listItemContainer = document.querySelector('ol#lista-tarefas');
+const removeDoneButton = document.querySelector('button#remover-finalizados');
+const removeSelectedButton = document.querySelector('button#remover-selecionado');
 
 
 // BUILD TASKS LIST
 function buildList() {
-  const listItem = document.createElement('li');
-  listItemContainer.className = 'task';
-  listItemContainer.innerHTML = taskInput.value;
+  let listItem = document.createElement('li');
+  listItem.className = 'task';
+  listItem.innerHTML = taskInput.value;
   listItemContainer.appendChild(listItem);
   taskInput.value = '';
 }
+addTaskButton.addEventListener('click', buildList);
 
+// REMOVE SELECTED ITEM
+function removeSelected() {
+  let selectedClassItem = document.querySelector('.selected');
+  selectedClassItem.remove()
+}
+removeSelectedButton.addEventListener('click', removeSelected);
+
+// SELECT ITEMS
+function selectTask(event) {
+  let clickedTaskItem = event.target;
+  let selectedTaskItem = document.querySelector('.selected');
+  if (selectedTaskItem !== null) {
+    selectedTaskItem.classList.remove('selected');
+  } else {
+    clickedTaskItem.classList.add('selected');
+  };
+}
+listItemContainer.addEventListener('click', selectTask);
 
 
 
 
 window.onload = function () {
-  addTaskButton.addEventListener('click', buildList);
+
 
 }
