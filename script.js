ef6cb15031de55cf0b91acfbc3c24d790b350d6e
gussta <saideles.silveira@gmail.com>
2020-08-06 09:58:27 -0300

    js clicar selecionado cinza

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const btnCriarTarefa = document.getElementById('criar-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 
-function criarTarefa () {
-  let lista = document.getElementById('texto-tarefa').value;
-  let tarefa = document.createElement('li');
+function criarTarefa() {
+  const lista = document.getElementById('texto-tarefa').value;
+  const tarefa = document.createElement('li');
   listaTarefas.appendChild(tarefa);
   tarefa.className = 'item';
-  tarefa.innerHTML = lista;  
-  document.getElementById('texto-tarefa').value = "";
+  tarefa.innerHTML = lista;
+  document.getElementById('texto-tarefa').value = '';
 }
 
-btnCriarTarefa.addEventListener ('click', criarTarefa);
+btnCriarTarefa.addEventListener('click', criarTarefa);
+
+listaTarefas.addEventListener('click', function(event) {
+  const tiraSelecionado = document.querySelector('.selecionado');
+  if (tiraSelecionado !== null) {
+    tiraSelecionado.classList.remove('selecionado');
+  }
+  event.target.classList.add('selecionado');
+});
