f617ea7bd29acd908886389638228aff91f298fa
lucasvr39 <lucasveras1994@gmail.com>
2020-08-11 10:59:58 -0300

    fix button spelling and add save event to bonus req

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnNewTask = document.getElementById('criar-tarefa');
 const btnDeleteSelected = document.getElementById('remover-selecionado');
 const btnDeleteCompleted = document.getElementById('remover-finalizados');
 const btnDeleteAll = document.getElementById('apaga-tudo');
+const saveTaskList = document.getElementById('salvar-tarefas');
 
 // create list item, get input value and push list value
 
@@ -44,6 +45,8 @@ function addCompletedTask(event) {
   }
 }
 
+// add events to page buttons
+
 btnNewTask.addEventListener('click', pushListItem);
 
 btnDeleteSelected.addEventListener('click', function () {
@@ -61,16 +64,38 @@ btnDeleteCompleted.addEventListener('click', function () {
 });
 
 btnDeleteAll.addEventListener('click', function () {
-  const allList = document.querySelectorAll('#lista-tarefas');
-  for (let i = 0; i < allList.length; i += 1) {
-    allList[i].remove();
+  const allList = document.querySelector('#lista-tarefas');
+  while (allList.firstChild) {
+    allList.removeChild(allList.lastChild);
+  }
+  localStorage.clear();
+});
+
+document.querySelector('#lista-tarefas').addEventListener('click', handleSelectedItem);
+
+document.querySelector('#lista-tarefas').addEventListener('dblclick', addCompletedTask);
+
+// call event to store list items
+
+saveTaskList.addEventListener('click', () => {
+  const tasks = document.getElementsByTagName('li');
+  if (localStorage !== null) {
+    localStorage.clear();
+  }
+  for (let i = 0; i < tasks.length; i += 1) {
+    localStorage.setItem(i, tasks[i].innerHTML);
+    localStorage.setItem(tasks[i].innerHTML, tasks[i].className);
   }
 });
 
-document
-  .querySelector('#lista-tarefas')
-  .addEventListener('click', handleSelectedItem);
+// load stored items after page load
 
-document
-  .querySelector('#lista-tarefas')
-  .addEventListener('dblclick', addCompletedTask);
+window.onload = () => {
+  const parentList = document.getElementById('lista-tarefas');
+  for (let key = 0; key < localStorage.length / 2; key += 1) {
+    const loadListItem = document.createElement('li');
+    loadListItem.innerHTML = localStorage.getItem(key);
+    loadListItem.className = localStorage.getItem(localStorage[key]);
+    parentList.appendChild(loadListItem);
+  }
+};
