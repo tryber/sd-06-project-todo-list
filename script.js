10db6e1095127c8bf92653ded4fad03893faeeb9
v-icto-r <victor130496@gmail.com>
2020-09-02 20:41:31 -0300

    testando

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-let ElementOl =  document.createElement('ol');
-ElementOl.id = "lista-tarefas";
-document.body.appendChild(ElementOl);
-console.log(ElementOl);
-
-let button = document.getElementById("criar-tarefa");
-let activity = document.getElementById("texto-tarefa");
-
-
-
-button.addEventListener("click", function() {
-
-    let elementLi = document.createElement('Li');   
-
-    elementLi.innerHTML = activity.value;
-    ElementOl.appendChild(elementLi);
-
-    activity.value = "";
+function criarLista() {
+    const tarefas = document.querySelector('.tarefas');
+    const listaItem = document.createElement('ol');
+    listaItem.id = 'lista-tarefas';
+    tarefas.appendChild(listaItem);
+}
+criarLista();
+const btnAdd = document.querySelector('#criar-tarefa');
+btnAdd.addEventListener('click', function(){
+    const lista = document.querySelector('#lista-tarefas');
+    const textoItem = document.querySelector('#texto-tarefa');
+    const item = document.createElement('li');
+    item.innerHTML = textoItem.value;
+    textoItem.value = '';
+    lista.appendChild(item);
 });
 
-ElementOl.addEventListener('click', function(event) {
-    let listItem  = event.target;
-    listItem.style.backgroundColor = "gray";
+// Mudar cor de somente 1 elemento selecionado
+const lista = document.querySelector('#lista-tarefas');
+lista.addEventListener('click', function (event) {
+  const elementoSelecionado = document.querySelector('.selected');
+  const selected = event.target;
+  if (elementoSelecionado) {
+    elementoSelecionado.classList.remove('selected');
+    selected.classList.add('selected');
+  } else {
+    selected.classList.add('selected');
+  }
+});
+
+// Riscar as tarefas completas
+lista.addEventListener('dblclick', function () {
+  const item = event.target;
+  item.classList.toggle('completed');
+});
+
+// Apagar todos os ítens da lista
+const btnApagaTudo = document.querySelector('#apaga-tudo');
+btnApagaTudo.addEventListener('click', function () {
+  while (lista.firstChild) {
+    lista.removeChild(lista.firstChild);
+  }
+});
+
+// Remover os ítens finalizados
+const btnApagaFinalizados = document.querySelector('#remover-finalizados');
+btnApagaFinalizados.addEventListener('click', function () {
+  const itens = document.querySelectorAll('.completed');
+  if (itens) {
+    for (let index = 0; index < itens.length; index += 1) {
+      itens[index].remove();
+    }
+  }
 });
